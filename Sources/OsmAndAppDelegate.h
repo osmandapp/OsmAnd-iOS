8af7a27b65d2d38384159c6e62a07f8697e4117a
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-07-15 13:15:05 +0300

    Initial iOS project & workspace

---
+++ Sources/OsmAndAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OsmAndAppDelegate.h
+//  OsmAnd
+//
+//  Created by Alexey Pelykh on 7/15/13.
+//  Copyright (c) 2013 OsmAnd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OsmAndAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

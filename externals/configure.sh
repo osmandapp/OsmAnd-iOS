80437efd234376ca0963f61932c14c66686700a2
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-08-19 21:09:15 +0300

    Add JASidePanels external

---
+++ externals/configure.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SRCLOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+OSMAND_EXTERNALS_SET=($*)
+if [ -z "$OSMAND_EXTERNALS_SET" ]; then
+	OSMAND_EXTERNALS_SET=*
+fi
+
+for external in ${OSMAND_EXTERNALS_SET[@]/#/$SRCLOC/} ; do
+	if [ -d "$external" ]; then
+		if [ -e "$external/configure.sh" ]; then
+			$external/configure.sh
+		fi
+	fi
+done

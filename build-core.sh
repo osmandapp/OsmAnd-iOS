f2f375a50d047a9140bb0eaca3314724c529a4e0
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-09-22 12:55:04 +0300

    Fix build script

--- build-core.sh
+++ build-core.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SRCLOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT="$SRCLOC/.."
 
 if [[ -z "$OSMAND_BUILD_CPU_CORES_NUM" ]]; then
 	OSMAND_BUILD_CPU_CORES_NUM=`sysctl hw.ncpu | awk '{print $2}'`
@@ -8,11 +9,11 @@ fi
 
 # Prepare core dependencies
 DEPS=expat gdal giflib glm glsl-optimizer jpeg libpng protobuf qtbase-ios skia zlib
-"$SRCLOC/core/externals/configure.sh" $DEPS
-"$SRCLOC/core/externals/build.sh" $DEPS
+"$ROOT/core/externals/configure.sh" $DEPS
+"$ROOT/core/externals/build.sh" $DEPS
 
 # Build core library with different flavours
-("$SRCLOC/build/simulator-ios.sh" debug && (cd "$SRCLOC/baked/simulator-ios-clang-debug.makefile" && make -j$OSMAND_BUILD_CPU_CORES_NUM OsmAndCore_static))
-("$SRCLOC/build/simulator-ios.sh" release && (cd "$SRCLOC/baked/simulator-ios-clang-release.makefile" && make -j$OSMAND_BUILD_CPU_CORES_NUM OsmAndCore_static))
-("$SRCLOC/build/device-ios.sh" debug && (cd "$SRCLOC/baked/device-ios-clang-debug.makefile" && make -j$OSMAND_BUILD_CPU_CORES_NUM OsmAndCore_static))
-("$SRCLOC/build/device-ios.sh" release && (cd "$SRCLOC/baked/device-ios-clang-release.makefile" && make -j$OSMAND_BUILD_CPU_CORES_NUM OsmAndCore_static))
+("$ROOT/build/simulator-ios.sh" debug && (cd "$ROOT/baked/simulator-ios-clang-debug.makefile" && make -j$OSMAND_BUILD_CPU_CORES_NUM OsmAndCore_static))
+("$ROOT/build/simulator-ios.sh" release && (cd "$ROOT/baked/simulator-ios-clang-release.makefile" && make -j$OSMAND_BUILD_CPU_CORES_NUM OsmAndCore_static))
+("$ROOT/build/device-ios.sh" debug && (cd "$ROOT/baked/device-ios-clang-debug.makefile" && make -j$OSMAND_BUILD_CPU_CORES_NUM OsmAndCore_static))
+("$ROOT/build/device-ios.sh" release && (cd "$ROOT/baked/device-ios-clang-release.makefile" && make -j$OSMAND_BUILD_CPU_CORES_NUM OsmAndCore_static))

534acf40f9b2b6acf4d7886c11d2e6315853077a
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-09-22 13:18:44 +0300

    Fix

--- build-core.sh
+++ build-core.sh
@@ -8,9 +8,10 @@ if [[ -z "$OSMAND_BUILD_CPU_CORES_NUM" ]]; then
 fi
 
 # Prepare core dependencies
-DEPS=qtbase-ios expat gdal giflib glm glsl-optimizer jpeg libpng protobuf skia zlib
-"$ROOT/core/externals/configure.sh" $DEPS
-"$ROOT/core/externals/build.sh" $DEPS
+echo "Configuring dependencies..."
+"$ROOT/core/externals/configure.sh" qtbase-ios expat gdal giflib glm glsl-optimizer jpeg libpng protobuf skia zlib
+echo "Building dependencies..."
+"$ROOT/core/externals/build.sh"
 
 # Build core library with different flavours
 ("$ROOT/build/simulator-ios.sh" debug && (cd "$ROOT/baked/simulator-ios-clang-debug.makefile" && make -j$OSMAND_BUILD_CPU_CORES_NUM OsmAndCore_static))

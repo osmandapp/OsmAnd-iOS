fd16d7c27784bec84517aa53a414feccc4a55328
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-09-29 17:10:00 +0300

    Update dependencies

--- build-core.sh
+++ build-core.sh
@@ -9,7 +9,7 @@ fi
 
 # Prepare core dependencies
 echo "Configuring dependencies..."
-"$ROOT/core/externals/configure.sh" qtbase-ios expat harfbuzz freetype giflib jpeg zlib libpng protobuf skia gdal glsl-optimizer glm
+"$ROOT/core/externals/configure.sh" qtbase-ios expat giflib jpeg zlib libpng protobuf skia gdal glsl-optimizer glm
 echo "Building dependencies..."
 "$ROOT/core/externals/build.sh"
 

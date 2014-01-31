d0d95638d34aa847e5ed2ca51aba3919440aef48
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-01-31 22:26:05 +0200

    Script for cleaning TestFlight

---
+++ cleanup-testflight.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SRCLOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PYTHON3=`which python3`
+if [ ! -f "$PYTHON3" ]; then
+	PYTHON3=`which python3.1`
+fi
+if [ ! -f "$PYTHON3" ]; then
+	PYTHON3=`which python3.2`
+fi
+if [ ! -f "$PYTHON3" ]; then
+	PYTHON3=`which python3.3`
+fi
+if [ ! -f "$PYTHON3" ]; then
+	PYTHON3=`which python3.4`
+fi
+if [ ! -f "$PYTHON3" ]; then
+	echo "Python3 not found"
+	exit
+fi
+
+"$PYTHON3" "$SRCLOC/cleanup-testflight.py" "$@"

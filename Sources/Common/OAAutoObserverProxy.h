b800b3e9b52807ad3efb615a59465e9cce5002ad
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-02-25 13:05:02 +0200

    Implement auto-observer proxy

--- Sources/Common/OAAutoObserverProxy.h
+++ Sources/Common/OAAutoObserverProxy.h
@@ -8,6 +8,23 @@
 
 #import <Foundation/Foundation.h>
 
-@interface OAAutoObserverProxy : NSObject
+#import "OAObservableProtocol.h"
+#import "OAObserverProtocol.h"
+
+@interface OAAutoObserverProxy : NSObject <OAObserverProtocol>
+
+- (id)initWith:(id<OAObserverProtocol>)owner_;
+- (id)initWith:(id<OAObserverProtocol>)owner_ withHandler:(SEL)selector_;
+
+@property(weak, readonly) id<OAObserverProtocol> owner;
+@property(weak, readonly) SEL handler;
+
+@property(weak, readonly) id<OAObservableProtocol> observable;
+- (void)observe:(id<OAObservableProtocol>)observable;
+
+- (void)handleObservedEvent;
+- (void)handleObservedEventFrom:(id<OAObservableProtocol>)observer;
+- (void)handleObservedEventFrom:(id<OAObservableProtocol>)observer withKey:(id)key;
+- (void)handleObservedEventFrom:(id<OAObservableProtocol>)observer withKey:(id)key andValue:(id)value;
 
 @end
--- Sources/Common/OAAutoObserverProxy.m
+++ Sources/Common/OAAutoObserverProxy.m
@@ -9,5 +9,150 @@
 #import "OAAutoObserverProxy.h"
 
 @implementation OAAutoObserverProxy
+{
+}
+
+@synthesize owner = _owner;
+@synthesize handler = _handler;
+
+- (id)initWith:(id<OAObserverProtocol>)owner_
+{
+    self = [super init];
+    if (self) {
+        [self ctor:owner_
+           handler:nil];
+    }
+    return self;
+}
+
+- (id)initWith:(id<OAObserverProtocol>)owner_ withHandler:(SEL)handler_
+{
+    self = [super init];
+    if (self) {
+        [self ctor:owner_
+           handler:handler_];
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [self dtor];
+}
+
+- (void)ctor:(id<OAObserverProtocol>)owner_ handler:(SEL)handler_
+{
+    _owner = owner_;
+    _handler = handler_;
+    _observable = nil;
+}
+
+- (void)dtor
+{
+    if(_observable != nil)
+    {
+        [_observable unregisterObserver:self];
+        _observable = nil;
+    }
+}
+
+@synthesize observable = _observable;
+
+- (void)observe:(id<OAObservableProtocol>)observable
+{
+    if(_observable != nil)
+    {
+        [_observable unregisterObserver:self];
+        _observable = nil;
+    }
+
+    _observable = observable;
+    [_observable registerObserver:self];
+}
+
+- (void)handleObservedEvent
+{
+    [self handleObservedEventFrom:nil];
+}
+
+- (void)handleObservedEventFrom:(id<OAObservableProtocol>)observer
+{
+    [self handleObservedEventFrom:observer
+                          withKey:nil];
+}
+
+- (void)handleObservedEventFrom:(id<OAObservableProtocol>)observer withKey:(id)key
+{
+    [self handleObservedEventFrom:observer
+                          withKey:key
+                         andValue:nil];
+}
+
+- (void)handleObservedEventFrom:(id<OAObservableProtocol>)observer withKey:(id)key andValue:(id)value
+{
+    if(_handler != nil)
+    {
+        NSMethodSignature* handlerSignature = [_owner methodSignatureForSelector:_handler];
+        NSUInteger handlerArgsCount = [handlerSignature numberOfArguments] - 2; // Subtract "self" and "cmd_"
+        NSInvocation* invocation = [NSInvocation invocationWithMethodSignature:handlerSignature];
+        [invocation setTarget:_owner];
+        [invocation setSelector:_handler];
+
+        if(handlerArgsCount == 3)
+        {
+            [invocation setArgument:observer
+                            atIndex:2+0];
+            [invocation setArgument:key
+                            atIndex:2+1];
+            [invocation setArgument:value
+                            atIndex:2+2];
+            [invocation invoke];
+            return;
+        }
+        
+        if(handlerArgsCount == 2)
+        {
+            [invocation setArgument:observer
+                            atIndex:2+0];
+            [invocation setArgument:key
+                            atIndex:2+1];
+            [invocation invoke];
+            return;
+        }
+        
+        if(handlerArgsCount == 1)
+        {
+            [invocation setArgument:observer
+                            atIndex:2+0];
+            [invocation invoke];
+            return;
+        }
+        
+        [invocation invoke];
+    }
+    
+    if([_owner respondsToSelector:@selector(handleObservedEventFrom:withKey:andValue:)])
+    {
+        [_owner handleObservedEventFrom:self
+                                withKey:key
+                               andValue:value];
+        return;
+    }
+    
+    if([_owner respondsToSelector:@selector(handleObservedEventFrom:withKey:)])
+    {
+        [_owner handleObservedEventFrom:self
+                                withKey:key];
+        return;
+    }
+    
+    if([_owner respondsToSelector:@selector(handleObservedEventFrom:)])
+    {
+        [_owner handleObservedEventFrom:self];
+        return;
+    }
+    
+    [_owner handleObservedEvent];
+}
 
 @end

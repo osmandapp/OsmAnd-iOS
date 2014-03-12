790240f328aeb5922a625d4490614c9d032e96a5
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-03-12 21:15:58 +0200

    Get file modification scripts

--- download-shipped-resources.sh
+++ download-shipped-resources.sh
@@ -9,7 +9,22 @@ fi
 SRCLOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT="$SRCLOC/.."
 
+if [[ "$(uname -a)" =~ Linux ]]; then
+	GET_FILE_MODIFICATION="stat -c %Y"
+elif [[ "$(uname -a)" =~ Darwin ]]; then
+	GET_FILE_MODIFICATION="stat -f %m"
+elif [[ "$(uname -a)" =~ Cygwin ]]; then
+	GET_FILE_MODIFICATION="stat -c %Y"
+elif [[ "$(uname -a)" =~ MINGW ]]; then
+	GET_FILE_MODIFICATION="stat -c %Y"
+else
+	echo "'$(uname -a)' is not recognized"
+	exit 1
+fi
+
 # Download world mini-basemap
 mkdir -p "$SRCLOC/ShippedResources"
 (cd "$SRCLOC/ShippedResources" && \
-	curl -z WorldMiniBasemap.obf -L http://builder.osmand.net:81/basemap/World_basemap_mini_2.obf -o WorldMiniBasemap.obf)
+	curl -R -z WorldMiniBasemap.obf -L http://builder.osmand.net:81/basemap/World_basemap_mini_2.obf -o WorldMiniBasemap.obf && \
+	$GET_FILE_MODIFICATION "$SRCLOC/ShippedResources/WorldMiniBasemap.obf" > "$SRCLOC/ShippedResources/WorldMiniBasemap.obf.stamp")
+echo "Shipping 'WorldMiniBasemap.obf' with version '$(cat $SRCLOC/ShippedResources/WorldMiniBasemap.obf.stamp)'"

95be5991b2fc1c8579872f1c2d1e4b8d5540313f
and01 <and01@users.noreply.github.com>
2016-02-06 17:37:26 +0100

    missing documentation

--- Sources/OASmartNaviWatchSession.h
+++ Sources/OASmartNaviWatchSession.h
@@ -5,6 +5,12 @@
 //  Created by egloff on 18/12/15.
 //  Copyright © 2015 OsmAnd. All rights reserved.
 //
+/**
+ *  This singleton class is responsible for every data connection to the
+ *  Apple Watch. It registers sessions as well as provides information such
+ *  as whether or not the plugin is activated.
+ */
+//
 
 #import <Foundation/Foundation.h>
 
@@ -13,19 +19,100 @@
 #import "OASmartNaviWatchNavigationController.h"
 
 @interface OASmartNaviWatchSession : NSObject <WCSessionDelegate> {
-    //properties
+
     NSObject *observer;
     OASmartNaviWatchNavigationController *navigationController;
+    CLLocation *currentLocation;
+    BOOL navigationUpdateNeeded;
+    BOOL newNavigationDataAvailable;
+    
 }
 
+/**
+ *  singleton instance
+ *
+ *  @return self
+ */
 + (id)sharedInstance;
 
+/**
+ *  whether or not the SmartNaviWatch plugin has been activated
+ *
+ *  @return true if activated
+ */
 -(BOOL)checkIfPluginEnabled;
+
+/**
+ *  activates the plugin and activates session
+ */
 -(void)activatePlugin;
+
+/**
+ *  deactivates the plugin and its session
+ */
+-(void)deactivatePlugin;
+
+/**
+ *  send rendered map images of the current location to the watch
+ *
+ *  @param imageData NSArray of UIImage images
+ *  @param location  the location around which the map is rendered
+ */
 -(void)sendImageData:(NSArray*)imageData forLocation:(CLLocation*)location;
+
+/**
+ *  registers an observer such as the map panel for updates
+ *
+ *  @param observerToRegister observer to be registered
+ */
 -(void)registerObserverForUpdates:(NSObject*)observerToRegister;
 
-+ (NSData *)imageDataWithImage:(UIImage *)image scaledToSize:(CGSize)newSize;
+/**
+ *  Call this method when new location data has been acquired.
+ *  The following criterias are relevant when it comes to the provided
+ *  fix:
+ *  speed > 1
+ *  horizontal accuracy <= 50
+ *  every 20 meters from the last provided valid fix, new distances and
+ *  bearing on the current navigation route is calculated
+ *
+ *  @param newLocation the CLLocation fix
+ */
+-(void)updateSignificantLocationChange:(CLLocation*)newLocation;
+
+/**
+ *  takes a UIImage and scales it down to the given size
+ *  further it compresses the images with jpg and converts
+ *  into NSData
+ *
+ *  @param image   the image to be scaled and compressed
+ *  @param newSize the target size of the scale down
+ *
+ *  @return NSData of the compressed and scaled image
+ */
++ (NSData *)compressedImageDataWithImage:(UIImage *)image scaledToSize:(CGSize)newSize;
+
+/**
+ *  gathers all the current information such as current image data, location
+ *  and navigation instructions
+ */
+-(void)initiateUpdate;
+
+/**
+ *  sets the active route of the OsmAnd navigation mode if any
+ *  calculates new distances and bearings
+ *  initiatesUpdate if forced
+ *
+ *  @param forced initiatesUpdate if true
+ */
+-(void)setActiveRouteWithForceRefresh:(BOOL)forced;
+
+/**
+ *  checks whether or not the watch extension is installed on the watch
+ *
+ *  @return true if installed
+ */
+-(BOOL)isAppInstalledOnWatch;
 
 @end
 
--- Sources/OASmartNaviWatchSession.m
+++ Sources/OASmartNaviWatchSession.m
@@ -14,6 +14,7 @@
 #import "OAMapPanelViewController.h"
 #import "OARootViewController.h"
 #import "OAMapViewController.h"
+#import "OASmartNaviWatchConstants.h"
 
 @implementation OASmartNaviWatchSession
 
@@ -40,11 +41,176 @@
     observer = observerToRegister;
 }
 
+
+-(void)updateSignificantLocationChange:(CLLocation*)newLocation {
+    if ([self checkIfPluginEnabled]) {
+        
+        //only take into account measurement if quality is sufficient enough
+        if (newLocation.speed < 1 && (newLocation.horizontalAccuracy == 0.0 || newLocation.horizontalAccuracy > 50)) {
+            //do nothing
+            if (currentLocation == nil) {
+                currentLocation = newLocation;
+            }
+        } else {
+        if (currentLocation == nil) currentLocation = newLocation;
+            double significantDistance = [currentLocation distanceFromLocation:newLocation];
+            
+            if (significantDistance >= 20) {
+                currentLocation = newLocation;
+                
+                if ([navigationController calculateClosestWaypointIndexFromLocation:newLocation]) {
+                    [self initiateUpdate];
+                }
+                
+
+            }
+           
+        }
+        
+        
+    }
+}
+
+-(void)sendImageData:(NSArray *)imageData forLocation:(CLLocation*)location {
+    WCSession* session = [WCSession defaultSession];
+    
+    if (!session.watchAppInstalled) {
+        
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Connection error" message:@"Please install app on your Apple Watch." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alertView show];
+        return;
+        
+    }
+    
+    if (!session.isPaired) {
+        
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Connection error" message:@"Please pair your Apple Watch with your iPhone." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alertView show];
+        return;
+        
+    }
+    
+    //scale picture to device resolution in order to save data
+    NSMutableDictionary *dataDict = [[NSMutableDictionary alloc] init];
+    
+    for (int i=0; i<imageData.count; ++i) {
+        NSData *data = [OASmartNaviWatchSession compressedImageDataWithImage:(UIImage*)[imageData objectAtIndex:i] scaledToSize:CGSizeMake(140, 140)];
+        if (data.length > 2200) {
+            [dataDict setObject:data forKey:[NSString stringWithFormat:@"image%d",i]];
+        } else {
+            [dataDict setObject:@"1" forKey:OA_SMARTNAVIWATCH_KEY_LOCATION_ERROR_IMAGE_DATA];
+        }
+    }
+    
+    //request location info
+    OAMapPanelViewController *mapPanel = [OARootViewController instance].mapPanel;
+    NSString *address = [mapPanel findRoadNameByLat:location.coordinate.latitude lon:location.coordinate.longitude];
+        
+    if (address != nil) {
+        [dataDict setObject:address forKey:OA_SMARTNAVIWATCH_KEY_LOCATION_INFO];
+        
+    }
+    
+    //check if no active route
+    if (!newNavigationDataAvailable) {
+        [self setActiveRouteWithForceRefresh:NO];
+    } else {
+        newNavigationDataAvailable = NO;
+    }
+    
+    //get routing data
+    if (navigationUpdateNeeded) {
+        NSDictionary *routingData = [navigationController getActiveRouteInfoForCurrentLocation:location];
+        [dataDict setObject:routingData forKey:OA_SMARTNAVIWATCH_KEY_NAVIGATION_UPDATE];
+    }
+    
+    [session sendMessage:dataDict
+            replyHandler:^(NSDictionary *reply) {
+                //handle reply from iPhone app here
+            }
+            errorHandler:^(NSError *error) {
+                //TODO code == 7007
+                // open Watch App in order to get navigation updates
+                NSString *message;
+                switch (error.code) {
+                    case 7007: {
+                        message = @"Please open the OsmAnd Watch App.";
+                    }
+                        
+
+                        break;
+                        
+                    default:
+                        break;
+                }
+                
+                if (![message isEqualToString:@""]) {
+//                    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Error", @"Error") message:message delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+//                    [alertView show];
+                }
+
+            }
+     ];
+    
+
+    }
+
+-(void)setActiveRouteWithForceRefresh:(BOOL)forced {
+    
+    if (forced) {
+        newNavigationDataAvailable = YES;
+    }
+    
+    if (forced || [navigationController hasActiveRoute:currentLocation]) {
+        [navigationController setActiveRouteForLocation:currentLocation];
+        navigationController.currentIndexForRouting = -2;
+        navigationUpdateNeeded = YES;
+        if (currentLocation != nil) {
+            [navigationController calculateClosestWaypointIndexFromLocation:currentLocation];
+            if (forced) {
+                [self initiateUpdate];
+            }
+        }
+    } else {
+        navigationUpdateNeeded = NO;
+    }
+    
+}
+
+-(void)initiateUpdate {
+    [(OAMapViewController*)observer smartNaviWatchRequestLocationUpdate];
+}
+
+
+
+#pragma mark WCSessionDelegate
+
+-(void)session:(WCSession *)session didReceiveMessage:(NSDictionary<NSString *,id> *)message {
+    
+    if ([message objectForKey:OA_SMARTNAVIWATCH_KEY_LOCATION_REQUEST]) {
+        [self initiateUpdate];
+    }
+    
+}
+
+-(void)sessionReachabilityDidChange:(WCSession *)session {
+    //handle reachability changes
+}
+
+#pragma helper methods
+
 -(void)activatePlugin {
     if ([self checkIfPluginEnabled] && [WCSession isSupported]) {
         WCSession* session = [WCSession defaultSession];
         session.delegate = self;
         [session activateSession];
+    }
+}
+
+-(void)deactivatePlugin {
+    if ([WCSession isSupported]) {
+        WCSession* session = [WCSession defaultSession];
+        session.delegate = nil;
     }
 }
 
@@ -57,88 +223,15 @@
     return ![[OAIAPHelper sharedInstance] isProductDisabled:kInAppId_Addon_SmartNaviWatch];
 }
 
--(void)sendImageData:(NSArray *)imageData forLocation:(CLLocation*)location {
-    WCSession* session = [WCSession defaultSession];
-    
-    if (session.watchAppInstalled) {
-        //TODO show message
-    }
-    
-    //scale picture to device resolution in order to save data
-    
-        if (session.isPaired) {
-            
-            NSMutableDictionary *dataDict = [[NSMutableDictionary alloc] init];
++ (NSData *)compressedImageDataWithImage:(UIImage *)image scaledToSize:(CGSize)newSize {
 
-            for (int i=0; i<imageData.count; ++i) {
-                NSData *data = [OASmartNaviWatchSession imageDataWithImage:(UIImage*)[imageData objectAtIndex:i] scaledToSize:CGSizeMake(140, 140)];
-                [dataDict setObject:data forKey:[NSString stringWithFormat:@"image%d",i]];
-            }
-            
-            //request location info
-            OAMapPanelViewController *mapPanel = [OARootViewController instance].mapPanel;
-            NSString *address = [mapPanel findRoadNameByLat:location.coordinate.latitude lon:location.coordinate.longitude];
-
-            if (address != nil) {
-                [dataDict setObject:address forKey:@"locationInfo"];
-
-            }
-            
-            //TODO move this to some other method
-
-            
-            
-
-            //get routing data
-            NSDictionary *routingData = [navigationController getActiveRouteInfoForCurrentLocation:location.coordinate];
-            
-//            [dataDict setObject:routingData forKey:@"routingInfo"];
-            
-            [session sendMessage:dataDict
-                    replyHandler:^(NSDictionary *reply) {
-                        //handle reply from iPhone app here
-                    }
-                    errorHandler:^(NSError *error) {
-                        //catch any errors here
-                    }
-             ];
-        }
-
-    }
-    
-
-
-
-
-
-#pragma mark WCSessionDelegate
-
--(void)session:(WCSession *)session didReceiveMessage:(NSDictionary<NSString *,id> *)message {
-    
-    if ([message objectForKey:@"locationRequest"]) {
-        //TODO render location image
-//        [self sendImageData:nil];
-//        [(OATargetPointView*)observer smartNaviWatchRequestLocationUpdate];
-        [(OAMapViewController*)observer smartNaviWatchRequestLocationUpdate];
-    }
-    
-}
-
--(void)sessionReachabilityDidChange:(WCSession *)session {
-    //TODO
-}
-
-#pragma helper methods
-+ (NSData *)imageDataWithImage:(UIImage *)image scaledToSize:(CGSize)newSize {
-    //UIGraphicsBeginImageContext(newSize);
-    // In next line, pass 0.0 to use the current device's pixel scaling factor (and thus account for Retina resolution).
-    // Pass 1.0 to force exact pixel size.
     UIGraphicsBeginImageContextWithOptions(newSize, NO, 0.0);
     [image drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
     UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
-    
-    NSData *compressedImageData= UIImageJPEGRepresentation(newImage,0.5 /*compressionQuality*/);
+
+    //use jpg compression in order to create smaller paylords, 40% compression quality
+    NSData *compressedImageData= UIImageJPEGRepresentation(newImage,0.4);
 
     return compressedImageData;
 }

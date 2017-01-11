04fcc6b8a5da555c1781b63a9137f003390cfc50
Alexey Kulish <alexey.kulish@osmand.net>
2017-01-11 10:10:30 +0300

    Added revision sha for poi ui filters code

--- Sources/POI/OAPOIFiltersHelper.h
+++ Sources/POI/OAPOIFiltersHelper.h
@@ -5,6 +5,7 @@
 //  Created by Alexey Kulish on 21/12/2016.
 //  Copyright © 2016 OsmAnd. All rights reserved.
 //
+//  revision 878491110c391829cc1f42eace8dc582cb35e08e
 
 #import <Foundation/Foundation.h>
 
@@ -31,6 +32,7 @@
 - (void) removeSelectedPoiFilter:(OAPOIUIFilter *)filter;
 - (BOOL) isShowingAnyPoi;
 - (void) clearSelectedPoiFilters;
+- (void) hidePoiFilters;
 - (NSString *) getFiltersName:(NSSet<OAPOIUIFilter *> *)filters;
 - (NSString *) getSelectedPoiFiltersName;
 - (BOOL) isPoiFilterSelected:(OAPOIUIFilter *)filter;

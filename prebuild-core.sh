8af140ef7a259123074fd73e3c38d58307635c33
Victor Shcherb <victor.shcherb@gmail.com>
2017-06-07 17:58:57 +0200

    Make version to compile only debug (uncomment who needs more:)

--- prebuild-core.sh
+++ prebuild-core.sh
@@ -10,15 +10,19 @@ SRCLOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT="$SRCLOC/.."
 
 # Workaround for http://public.kitware.com/Bug/view.php?id=14297
-(cd "$ROOT/baked/fat-ios-clang.xcode" && \
-	xcodebuild -project OsmAnd_projects.xcodeproj -target ZERO_CHECK -sdk iphoneos -configuration Debug && \
-	xcodebuild -project OsmAnd_projects.xcodeproj -target ZERO_CHECK -sdk iphoneos -configuration Release && \
-	xcodebuild -project OsmAnd_projects.xcodeproj -target ZERO_CHECK -sdk iphonesimulator -configuration Debug && \
-	xcodebuild -project OsmAnd_projects.xcodeproj -target ZERO_CHECK -sdk iphonesimulator -configuration Release)
+(
+	cd "$ROOT/baked/fat-ios-clang.xcode" && \
+#	xcodebuild -project OsmAnd_projects.xcodeproj -target ZERO_CHECK -sdk iphoneos -configuration Debug && \
+#	xcodebuild -project OsmAnd_projects.xcodeproj -target ZERO_CHECK -sdk iphoneos -configuration Release && \
+	xcodebuild -project OsmAnd_projects.xcodeproj -target ZERO_CHECK -sdk iphonesimulator -configuration Debug
+#	xcodebuild -project OsmAnd_projects.xcodeproj -target ZERO_CHECK -sdk iphonesimulator -configuration Release \
+)
 
 # Build core for all archs
-(cd "$ROOT/baked/fat-ios-clang.xcode" && \
-	xcodebuild -project OsmAnd_projects.xcodeproj -target OsmAndCore_static_standalone -sdk iphoneos -configuration Debug && \
-	xcodebuild -project OsmAnd_projects.xcodeproj -target OsmAndCore_static_standalone -sdk iphoneos -configuration Release && \
-	xcodebuild -project OsmAnd_projects.xcodeproj -target OsmAndCore_static_standalone -sdk iphonesimulator -configuration Debug && \
-	xcodebuild -project OsmAnd_projects.xcodeproj -target OsmAndCore_static_standalone -sdk iphonesimulator -configuration Release)
+(
+	cd "$ROOT/baked/fat-ios-clang.xcode" && \
+#	xcodebuild -project OsmAnd_projects.xcodeproj -target OsmAndCore_static_standalone -sdk iphoneos -configuration Debug && \
+#	xcodebuild -project OsmAnd_projects.xcodeproj -target OsmAndCore_static_standalone -sdk iphoneos -configuration Release && \
+	xcodebuild -project OsmAnd_projects.xcodeproj -target OsmAndCore_static_standalone -sdk iphonesimulator -configuration Debug
+#	xcodebuild -project OsmAnd_projects.xcodeproj -target OsmAndCore_static_standalone -sdk iphonesimulator -configuration Release \
+)

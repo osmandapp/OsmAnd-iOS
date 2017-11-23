5cd495fe48696773e20521926f206a3608369e20
Alexey Kulish <alexey.kulish@osmand.net>
2017-11-23 19:39:09 +0300

    LocationProvider done

---
+++ Sources/Services/OALocationSimulation.h
@@ -0,0 +1,15 @@
+//
+//  OALocationSimulation.h
+//  OsmAnd
+//
+//  Created by Alexey Kulish on 23/11/2017.
+//  Copyright © 2017 OsmAnd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OALocationSimulation : NSObject
+
+- (BOOL) isRouteAnimating;
+
+@end

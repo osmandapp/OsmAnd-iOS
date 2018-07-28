f5f1573b74227b6bd650e4163dcecd744932f176
crimean <alexey.kulish@osmand.net>
2018-07-28 21:38:07 +0300

    Added transport route context menu

---
+++ Sources/Controllers/2.0/OATransportRouteController.h
@@ -0,0 +1,13 @@
+//
+//  OATransportRouteController.h
+//  OsmAnd
+//
+//  Created by Alexey on 28/07/2018.
+//  Copyright © 2018 OsmAnd. All rights reserved.
+//
+
+#import "OATargetInfoViewController.h"
+
+@interface OATransportRouteController : OATargetInfoViewController
+
+@end

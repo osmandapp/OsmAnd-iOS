e89dfb2de78bd54e73d145fac38218d7fe154aad
PaulStets <stetsenko.pavlo@gmail.com>
2018-09-12 13:27:55 +0300

    Music is now silenced during the voice prompts

--- Sources/Voice/OATTSCommandPlayerImpl.h
+++ Sources/Voice/OATTSCommandPlayerImpl.h
@@ -5,12 +5,15 @@
 //  Created by Paul on 7/10/18.
 //  Copyright © 2018 OsmAnd. All rights reserved.
 //
+#import <AVFoundation/AVFoundation.h>
 #include "OAAbstractCommandPlayer.h"
 #include "OAVoiceRouter.h"
 
-@interface OATTSCommandPlayerImpl : OAAbstractCommandPlayer
+@interface OATTSCommandPlayerImpl : OAAbstractCommandPlayer<AVSpeechSynthesizerDelegate>
 
 - (instancetype) initWithVoiceRouter:(OAVoiceRouter *) voiceRouter voiceProvider:(NSString *)provider;
 - (void)playCommands:(OACommandBuilder *)builder;
+
+- (void)speechSynthesizer:(AVSpeechSynthesizer *)synthesizer didFinishSpeechUtterance:(AVSpeechUtterance *)utterance;
 
 @end

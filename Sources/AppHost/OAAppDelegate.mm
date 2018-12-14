0fc66a690fd1967a630d4e54a3eb5732492b517a
PaulStets <stetsenko.pavlo@gmail.com>
2018-12-14 11:43:58 +0200

    Revert last commit

--- Sources/AppHost/OAAppDelegate.mm
+++ Sources/AppHost/OAAppDelegate.mm
@@ -245,13 +245,11 @@
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        if (_checkLiveTimer)
-        {
-            [_checkLiveTimer invalidate];
-            _checkLiveTimer = nil;
-        }
-    });
+    if (_checkLiveTimer)
+    {
+        [_checkLiveTimer invalidate];
+        _checkLiveTimer = nil;
+    }
     if (_appInitDone)
         [_app onApplicationDidEnterBackground];
 }

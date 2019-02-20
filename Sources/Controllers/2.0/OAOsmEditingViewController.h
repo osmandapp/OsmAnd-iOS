73b19868fcba12a8a793915c65de320a232e79af
PaulStets <stetsenko.pavlo@gmail.com>
2019-02-20 11:13:56 +0200

    Start working on simple edtiting

---
+++ Sources/Controllers/2.0/OAOsmEditingViewController.h
@@ -0,0 +1,17 @@
+//
+//  OAOsmEditingViewController.h
+//  OsmAnd
+//
+//  Created by Paul on 2/20/19.
+//  Copyright © 2019 OsmAnd. All rights reserved.
+//
+
+#import "OACompoundViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OAOsmEditingViewController : OACompoundViewController
+
+@end
+
+NS_ASSUME_NONNULL_END

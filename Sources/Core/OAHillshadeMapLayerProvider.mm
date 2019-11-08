ace37956758e6487905c0a64bc974e0c3a8bb98c
PaulStets <stetsenko.pavlo@gmail.com>
2019-11-08 11:53:10 +0200

    Removed unnecessary code

--- Sources/Core/OAHillshadeMapLayerProvider.mm
+++ Sources/Core/OAHillshadeMapLayerProvider.mm
@@ -15,9 +15,6 @@ OAHillshadeMapLayerProvider::OAHillshadeMapLayerProvider()
 
 OAHillshadeMapLayerProvider::~OAHillshadeMapLayerProvider()
 {
-    _threadPool->clear();
-    _threadPool->waitForDone();
-    delete _threadPool;
 }
 
 OsmAnd::AlphaChannelPresence OAHillshadeMapLayerProvider::getAlphaChannelPresence() const

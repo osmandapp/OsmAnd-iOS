937d06091e5bb8a578d9e5aefd31977a9c95237f
PaulStets <stetsenko.pavlo@gmail.com>
2020-01-10 14:50:00 +0200

    Finished initial route details

--- Sources/Controllers/2.0/OARouteDetailsViewController.h
+++ Sources/Controllers/2.0/OARouteDetailsViewController.h
@@ -11,14 +11,9 @@
 @interface OARouteDetailsViewController : OATargetMenuViewController <UITableViewDataSource, UITableViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UIButton *doneButton;
-@property (weak, nonatomic) IBOutlet UILabel *routeInfoLabel;
-@property (weak, nonatomic) IBOutlet UILabel *elevationLabel;
-@property (weak, nonatomic) IBOutlet UILabel *descentLabel;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (weak, nonatomic) IBOutlet UIButton *cancelButton;
 @property (weak, nonatomic) IBOutlet UIButton *startButton;
-@property (weak, nonatomic) IBOutlet UIImageView *eleUpImageView;
-@property (weak, nonatomic) IBOutlet UIImageView *eleDownImageView;
 @property (weak, nonatomic) IBOutlet UIView *bottomToolBarDividerView;
 
 @end

a068e361ff8840ec4dfb8ace694363174eaeefd9
PaulStets <stetsenko.pavlo@gmail.com>
2020-02-13 17:00:43 +0200

    Fixed incorrect textView layout on rotation

--- Sources/Controllers/Resources/OAOnlineTilesEditingViewController.mm
+++ Sources/Controllers/Resources/OAOnlineTilesEditingViewController.mm
@@ -202,6 +202,15 @@
     _possibleZoomValues = @[@"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", @"11", @"12", @"13", @"14", @"15", @"16", @"17", @"18", @"19", @"20", @"21", @"22"];
 }
 
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator
+{
+    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
+    [coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext>  _Nonnull context) {
+        [_tableView beginUpdates];
+        [_tableView endUpdates];
+    } completion:nil];
+}
+
 - (void) setupView
 {
     [self applySafeAreaMargins];

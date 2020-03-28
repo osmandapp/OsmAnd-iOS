5b4c0172b6e1255fb345d7907766ed89e22d8008
Anna Bibyk <annbibik@gmail.com>
2020-03-28 17:16:17 +0200

    SettingsHelper added

--- Sources/Helpers/OASettingsHelper.h
+++ Sources/Helpers/OASettingsHelper.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "OsmAndAppProtocol.h"
 
 typedef enum : NSUInteger {
     EOAGlobal = 0,
@@ -31,11 +32,43 @@ typedef enum : NSUInteger {
 @property (nonatomic, assign) EOASettingsItemType type;
 @property (nonatomic, assign) BOOL shouldReplace;
 
--(instancetype)initWithType:(EOASettingsItemType)type;
--(instancetype)initWithType:(EOASettingsItemType)type json:(NSDictionary*)json;
--(EOASettingsItemType)getType;
--(void)readFromJSON:(NSDictionary*)json;
--(void)writeToJSON:(NSDictionary*)json;
+- (instancetype) initWithType:(EOASettingsItemType)type;
+- (instancetype) initWithType:(EOASettingsItemType)type json:(NSDictionary*)json;
+- (EOASettingsItemType) getType;
+- (BOOL) shouldReadOnCollecting;
+- (void) setShouldReplace:(BOOL)shouldReplace;
+- (EOASettingsItemType) parseItemType:(NSDictionary*)json;
+- (void) readFromJSON:(NSDictionary*)json;
+- (void) writeToJSON:(NSDictionary*)json;
+- (NSString *)toJSON;
+- (NSUInteger) hash;
+- (BOOL) isEqual:(id)object;
+
+@end
+
+#pragma mark - SettingsItemReader
+
+@interface OASettingsItemReader<ObjectType : OASettingsItem *> : NSObject
+
+- (instancetype) initWithItem:(ObjectType)item;
+- (void) readFromStream:(NSInputStream*)inputStream;
+
+@end
+
+#pragma mark - SettingsItemWriter
+
+@interface OASettingsItemWriter<ObjectType : OASettingsItem *> : NSObject
+
+- (instancetype) initWithItem:(ObjectType)item;
+- (BOOL) writeToStream:(NSOutputStream*)outputStream;
+
+@end
+
+#pragma mark - StreamSettingsItemReader
+
+@interface StreamSettingsItemReader : OASettingsItemReader<OASettingsItem *>
+
+- (instancetype)initWithItem:(OASettingsItem *)item;
 
 @end
 
@@ -43,15 +76,67 @@ typedef enum : NSUInteger {
 
 @interface OAStreamSettingsItem : OASettingsItem
 
-@property (nonatomic, assign) NSInputStream* inputStream;
-@property (nonatomic, assign) NSString* name;
+@property (nonatomic, retain, readonly) NSString* name;
+
+- (instancetype) initWithType:(EOASettingsItemType)type name:(NSString*)name;
+- (instancetype) initWithType:(EOASettingsItemType)type json:(NSDictionary*)json;
+- (instancetype) initWithType:(EOASettingsItemType)type inputStream:(NSInputStream*)inputStream name:(NSString*)name;
+- (NSInputStream *) getInputStream;
+- (void) setInputStream:(NSInputStream *)inputStream;
+- (NSString *) getName;
+- (NSString *) getPublicName;
+- (void) readFromJSON:(NSDictionary *)json;
+- (OASettingsItemWriter*) getWriter;
+
+
+@end
+
+#pragma mark - OADataSettingsItemReader
+
+@interface OADataSettingsItemReader: StreamSettingsItemReader
+
+- (instancetype)initWithItem:(OASettingsItem *)item;
 
 @end
 
 #pragma mark - DataSettingsItem
 
-@interface DataSettingsItem : OAStreamSettingsItem
+@interface OADataSettingsItem : OAStreamSettingsItem
+
+@property (nonatomic, retain) NSData *data;
+
+- (instancetype) initWithName:(NSString *)name;
+- (instancetype) initWithJson:(NSDictionary *)json;
+- (instancetype) initWithData:(NSData *)data name:(NSString *)name;
+- (NSString *) getFileName;
+- (NSData *) getData;
+- (OASettingsItemReader *) getReader;
+- (OASettingsItemWriter *) getWriter;
+
+@end
+
+#pragma mark - OAFileSettingsItemReader
+
+@interface OAFileSettingsItemReader: StreamSettingsItemReader
+
+- (instancetype)initWithItem:(OASettingsItem *)item;
+
+@end
 
 
+#pragma mark - OAFileSettingsItem
+
+@interface OAFileSettingsItem : OAStreamSettingsItem
+
+@property (nonatomic, retain) NSString *filePath;
+
+- (instancetype) initWithFile:(NSString *)filePath;
+- (instancetype) initWithJSON:(NSDictionary*)json;
+- (NSString *) getFileName;
+- (NSString *) getFile;
+- (BOOL) exists;
+- (NSString *) renameFile:(NSString*)file;
+- (OASettingsItemReader *) getReader;
+- (OASettingsItemWriter *) getWriter;
 
 @end
--- Sources/Helpers/OASettingsHelper.m
+++ Sources/Helpers/OASettingsHelper.m
@@ -12,11 +12,13 @@
 
 @end
 
+static const NSInteger _buffer = 1024;
+
 #pragma mark - OASettingsItem
 
 @implementation OASettingsItem
 
--(instancetype)initWithType:(EOASettingsItemType)type
+- (instancetype) initWithType:(EOASettingsItemType)type
 {
     self = [super init];
     if (self) {
@@ -25,7 +27,7 @@
     return self;
 }
  
--(instancetype)initWithType:(EOASettingsItemType)type json:(NSDictionary*)json
+- (instancetype) initWithType:(EOASettingsItemType)type json:(NSDictionary*)json
 {
     self = [super init];
     if (self) {
@@ -35,69 +37,82 @@
     return self;
 }
 
--(EOASettingsItemType)getType
+- (EOASettingsItemType) getType
 {
     return _type;
 }
 
--(NSString*)getName
+- (NSString *) getName
 {
     return nil;
 }
 
--(NSString*)getPublicName //:(Contex)ctx
+- (NSString *) getPublicName
 {
     return nil;
 }
 
--(NSString*)getFileName
+- (NSString *) getFileName
 {
     return nil;
 }
 
--(BOOL)shouldReadOnCollecting
+- (BOOL) shouldReadOnCollecting
 {
     return NO;
 }
 
--(void)setShouldReplace:(BOOL)shouldReplace
+- (void) setShouldReplace:(BOOL)shouldReplace
 {
     _shouldReplace = shouldReplace;
 }
 
-/*
-static SettingsItemType parseItemType(@NonNull JSONObject json) throws IllegalArgumentException, JSONException {
-    return SettingsItemType.valueOf(json.getString("type"));
-}
- */
-
--(EOASettingsItemType)parseItemType:(NSDictionary*)json
+- (EOASettingsItemType) parseItemType:(NSDictionary*)json
 {
-    //return [EOASettingsItemType valueForKey:[json objectForKey:@"type"]];
+    NSString *str = [json objectForKey:@"type"];
+    if ([str isEqualToString:@"GLOBAL"])
+        return EOAGlobal;
+    if ([str isEqualToString:@"PROFILE"])
+        return EOAProfile;
+    if ([str isEqualToString:@"PLUGIN"])
+        return EOAPlugin;
+    if ([str isEqualToString:@"DATA"])
+        return EOAData;
+    if ([str isEqualToString:@"FILE"])
+        return EOAFile;
+    if ([str isEqualToString:@"QUICK_ACTION"])
+        return EOAQuickAction;
+    if ([str isEqualToString:@"POI_UI_FILTERS"])
+        return EOAPoiUIFilters;
+    if ([str isEqualToString:@"MAP_SOURCES"])
+        return EOAMapSources;
+    if ([str isEqualToString:@"AVOID_ROADS"])
+        return EOAAvoidRoads;
+    return nil;
 }
 
--(BOOL)exists
+- (BOOL) exists
 {
     return NO;
 }
 
--(void)apply
+- (void) apply
 {
     // non implemented
 }
 
--(void)readFromJSON:(NSDictionary*)json
+- (void) readFromJSON:(NSDictionary*)json
 {
 }
 
--(void)writeToJSON:(NSDictionary*)json
+- (void) writeToJSON:(NSDictionary*)json
 {
     [json setValue:[NSNumber numberWithInteger:_type] forKey:[self getName]];
     [json setValue:[self getName] forKey:@"name"];
     
 }
 
--(NSString *)toJSON
+- (NSString *)toJSON
 {
     NSDictionary *JSONDic=[[NSDictionary alloc] init];
     NSError *error;
@@ -108,25 +123,17 @@ static SettingsItemType parseItemType(@NonNull JSONObject json) throws IllegalAr
     return [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
 }
 
-/*
- @NonNull
- abstract SettingsItemReader getReader();
+- (OASettingsItemReader *) getReader
+{
+    return nil;
+}
 
- @NonNull
- abstract SettingsItemWriter getWriter();
- */
+- (OASettingsItemWriter *) getWriter
+{
+    return nil;
+}
 
-//-(SettingsItemReader*)getReader
-//{
-//    return nil;
-//}
-//
-//-(SettingsItemWriter*)getWriter
-//{
-//    return nil;
-//}
-
-- (NSUInteger)hash
+- (NSUInteger) hash
 {
     NSInteger result = _type;
     NSString *name = [self getName];
@@ -134,7 +141,7 @@ static SettingsItemType parseItemType(@NonNull JSONObject json) throws IllegalAr
     return result;
 }
 
-- (BOOL)isEqual:(id)object
+- (BOOL) isEqual:(id)object
 {
     if (self == object)
         return YES;
@@ -156,24 +163,94 @@ static SettingsItemType parseItemType(@NonNull JSONObject json) throws IllegalAr
 
 @end
 
+#pragma mark - OASettingsItemReader
+
+@interface OASettingsItemReader<ObjectType : OASettingsItem *>()
+
+@property (nonatomic, assign) ObjectType item;
+
+@end
+
+@implementation OASettingsItemReader
+
+- (instancetype) initWithItem:(id)item
+{
+    _item = item;
+    return self;
+}
+
+- (void) readFromStream:(NSInputStream*)inputStream
+{
+    return;
+}
+
+@end
+
+#pragma mark - OSSettingsItemWriter
+
+@interface OASettingsItemWriter<ObjectType : OASettingsItem *>()
+
+@property (nonatomic, assign) ObjectType item;
+
+@end
+
+@implementation OASettingsItemWriter
+
+- (id) getItem
+{
+    return _item;
+}
+
+- (instancetype) initWithItem:(id)item
+{
+    _item = item;
+    return self;
+}
+
+- (BOOL) writeToStream:(NSOutputStream*)outputStream
+{
+    return NO;
+}
+
+@end
+
+#pragma mark - StreamSettingsItemReader
+
+@implementation StreamSettingsItemReader
+
+- (instancetype)initWithItem:(OASettingsItem *)item
+{
+    self = [super initWithItem:item];
+    return self;
+}
+
+@end
+
 #pragma mark - OAStreamSettingsItem
+
+@interface OAStreamSettingsItem()
+
+@property (nonatomic, retain) NSInputStream* inputStream;
+@property (nonatomic, retain) NSString* name;
+
+@end
 
 @implementation OAStreamSettingsItem
 
--(instancetype)initWithType:(EOASettingsItemType)type name:(NSString*)name
+- (instancetype) initWithType:(EOASettingsItemType)type name:(NSString*)name
 {
     [super setType:type];
     _name = name;
     return self;
 }
 
--(instancetype)initWithType:(EOASettingsItemType)type json:(NSDictionary*)json
+- (instancetype) initWithType:(EOASettingsItemType)type json:(NSDictionary*)json
 {
     self = [super initWithType:type json:json];
     return self;
 }
 
--(instancetype)initWithType:(EOASettingsItemType)type inputStream:(NSInputStream*)inputStream name:(NSString*)name
+- (instancetype) initWithType:(EOASettingsItemType)type inputStream:(NSInputStream*)inputStream name:(NSString*)name
 {
     [super setType:type];
     _name = name;
@@ -181,45 +258,237 @@ static SettingsItemType parseItemType(@NonNull JSONObject json) throws IllegalAr
     return self;
 }
 
--(NSInputStream*)getInputStream
+- (NSInputStream *) getInputStream
 {
     return _inputStream;
 }
 
--(void)setInputStream:(NSInputStream*)inputStream
+- (void) setInputStream:(NSInputStream *)inputStream
 {
     _inputStream = inputStream;
 }
 
--(NSString*)getName
+- (NSString *) getName
 {
     return _name;
 }
 
--(NSString*)getPublicName //:(Contex)ctx
+- (NSString *) getPublicName
 {
     return [self getName];
 }
 
--(void)readFromJSON:(NSDictionary*)json
+- (void) readFromJSON:(NSDictionary *)json
 {
     [super readFromJSON:json];
     _name = [[NSString alloc] initWithData:[json objectForKey:@"name"] encoding:NSUTF8StringEncoding];
 }
 
- /*
-@NonNull
-@Override
-public SettingsItemWriter getWriter() {
-    return new StreamSettingsItemWriter(this);
+-(OASettingsItemWriter*)getWriter
+{
+    OASettingsItemWriter *itemWriter = [[OASettingsItemWriter alloc] initWithItem:self];
+    return itemWriter;
 }
- */
 
-//-(SettingsItemWriter*)getWriter
-//{
-//    return nil;
-//}
+@end
+
+#pragma mark - OADataSettingsItemReader
+
+@interface OADataSettingsItemReader()
+
+@property (nonatomic, retain) OADataSettingsItem *dataSettingsItem;
+
+@end
+
+@implementation OADataSettingsItemReader
+
+- (instancetype)initWithItem:(OADataSettingsItem *)item
+{
+    self = [super initWithItem:item];
+    _dataSettingsItem = item;
+    return self;
+}
+
+
+- (void)readFromStream:(NSInputStream *)inputStream
+{
+    NSOutputStream *buffer = [[NSOutputStream alloc] init];
+    uint8_t data[_buffer];
+    NSInteger nRead;
+    [buffer open];
+    while ([inputStream hasBytesAvailable]) {
+        nRead = [inputStream read:data maxLength:sizeof(data)];
+        if (nRead > 0) {
+            [buffer write:data maxLength:nRead];
+        }
+    }
+    _dataSettingsItem.data = [buffer propertyForKey:NSStreamDataWrittenToMemoryStreamKey];
+    [buffer close];
+}
 
 @end
 
 
+#pragma mark - OAFileSettingsItemReader
+
+@interface OAFileSettingsItemReader()
+
+@property (nonatomic, retain) OAFileSettingsItem *fileSettingsItem;
+
+
+@end
+
+@implementation OAFileSettingsItemReader
+
+- (instancetype)initWithItem:(OAFileSettingsItem *)item
+{
+    self = [super initWithItem:item];
+    _fileSettingsItem = item;
+    return self;
+}
+
+- (void)readFromStream:(NSInputStream *)inputStream
+{
+    NSOutputStream *output;
+    NSString *filePath = _fileSettingsItem.filePath;
+    if (![_fileSettingsItem exists] || [_fileSettingsItem shouldReplace])
+        output = [NSOutputStream outputStreamToFileAtPath:filePath append:NO];
+    else
+        output = [NSOutputStream outputStreamToFileAtPath:[_fileSettingsItem renameFile:filePath] append:NO];
+    uint8_t buffer[_buffer];
+    NSInteger count;
+    [output open];
+    @try {
+        while ([inputStream hasBytesAvailable]) {
+            count = [inputStream read:buffer maxLength:count];
+            if (count > 0) {
+                [output write:buffer maxLength:count];
+            }
+        }
+    } @finally {
+        [output close];
+    }
+}
+
+@end
+
+
+#pragma mark - OADataSettingsItem
+
+@implementation OADataSettingsItem
+
+- (instancetype) initWithName:(NSString *)name
+{
+    self = [super initWithType:EOAData name:name];
+    return self;
+}
+
+- (instancetype) initWithJson:(NSDictionary *)json
+{
+    self = [super initWithType:EOAData json:json];
+    return self;
+}
+
+- (instancetype) initWithData:(NSData *)data name:(NSString *)name
+{
+    self = [super initWithType:EOAData name:name];
+    _data = data;
+    return self;
+}
+
+- (NSString *) getFileName
+{
+    return [[self getName] stringByAppendingString:@".dat"];
+}
+
+- (NSData *) getData {
+    return _data;
+}
+
+- (OASettingsItemReader *) getReader
+{
+    OADataSettingsItemReader *reader = [[OADataSettingsItemReader alloc] initWithItem:self];
+    [reader readFromStream: super.inputStream];
+    return reader;
+}
+
+- (OASettingsItemWriter *) getWriter
+{
+    NSInputStream *inputStream = [[NSInputStream alloc] initWithData:_data];
+    [self setInputStream:inputStream];
+    return [super getWriter];
+}
+
+@end
+
+
+#pragma mark - OAFileSettingsItem
+
+@implementation OAFileSettingsItem
+
+- (instancetype) initWithFile:(NSString *)filePath
+{
+    self = [super initWithType:EOAFile name:filePath];
+    _filePath = filePath;
+    return self;
+}
+
+- (instancetype) initWithJSON:(NSDictionary*)json
+{
+    self = [super initWithType:EOAFile json:json];
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    _filePath = [fileManager currentDirectoryPath];
+    
+    return self;
+}
+
+- (NSString *) getFileName
+{
+    return [super getName];
+}
+
+- (NSString *) getFile
+{
+    return _filePath;
+}
+
+- (BOOL) exists
+{
+    NSFileManager *filemaneger;
+
+    filemaneger = [NSFileManager defaultManager];
+
+    if ([filemaneger fileExistsAtPath: _filePath] == YES)
+        return YES;
+    else
+        return NO;
+}
+
+- (NSString *) renameFile:(NSString*)filePath
+{
+    NSFileManager *filemaneger = [NSFileManager defaultManager];
+    NSError *error = nil;
+    [filemaneger moveItemAtPath:_filePath toPath: filePath error: &error];
+    return _filePath;
+}
+
+- (OASettingsItemReader *) getReader
+{
+    OAFileSettingsItemReader *reader = [[OAFileSettingsItemReader alloc] initWithItem:self];
+    [reader readFromStream: super.inputStream];
+    return reader;
+}
+
+- (OASettingsItemWriter *) getWriter
+{
+    NSInputStream *inputStream = [NSInputStream inputStreamWithFileAtPath:_filePath];
+    @try {
+        [self setInputStream:inputStream];
+    } @catch (NSException *exception) {
+        NSLog(@"Failed to set input stream from file: %@", _filePath);
+    }
+    return [super getWriter];
+}
+
+@end

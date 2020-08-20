7561efebee9e9c9fbe8793e44a3cb1864ff0fa15
Anna Bibyk <annbibik@gmail.com>
2020-08-20 18:20:28 +0300

    Introduce Global Settings 48

--- Sources/Controllers/2.0/Settings/OAGlobalSettingsViewController.m
+++ Sources/Controllers/2.0/Settings/OAGlobalSettingsViewController.m
@@ -163,6 +163,7 @@
             NSArray *nib = [[NSBundle mainBundle] loadNibNamed:identifierCell owner:self options:nil];
             cell = (OASettingsTableViewCell *)[nib objectAtIndex:0];
             cell.descriptionView.font = [UIFont systemFontOfSize:17.0];
+            cell.descriptionView.numberOfLines = 1;
             cell.iconView.image = [[UIImage imageNamed:@"ic_custom_arrow_right"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
             cell.iconView.tintColor = UIColorFromRGB(color_tint_gray);
         }
@@ -204,6 +205,8 @@
             cell.separatorInset = UIEdgeInsetsMake(0.0, 62.0, 0.0, 0.0);
             [cell.overflowButton setImage:[[UIImage imageNamed:@"ic_checkmark_default"]  imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate] forState:UIControlStateNormal];
             cell.overflowButton.tintColor = UIColorFromRGB(color_primary_purple);
+            cell.textView.numberOfLines = 3;
+            cell.textView.lineBreakMode = NSLineBreakByTruncatingTail;
         }
         OAApplicationMode *am = _profileList[indexPath.row];
         UIImage *img = am.getIcon;

1fb703104dab52f60b9c7a1a2469bddae26690b4
nnngrach <nnngrach@gmail.com>
2020-08-21 00:36:37 +0300

    Made margins like in Figma

---
+++ Sources/Controllers/Cells/OAMultyLabelCell.h
@@ -0,0 +1,15 @@
+//
+//  OAMultyLabelCell.h
+//  OsmAnd
+//
+//  Created by nnngrach on 21.08.2020.
+//  Copyright © 2020 OsmAnd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OAMultyLabelCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *textView;
+
+@end
---
+++ Sources/Controllers/Cells/OAMultyLabelCell.m
@@ -0,0 +1,15 @@
+//
+//  OAMultyLabelCell.m
+//  OsmAnd
+//
+//  Created by nnngrach on 21.08.2020.
+//  Copyright © 2020 OsmAnd. All rights reserved.
+//
+
+#import "OAMultyLabelCell.h"
+
+@implementation OAMultyLabelCell
+
+ 
+@end
+

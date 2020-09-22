a62c5535d489ba2795dc5ce65783480829a9e0df
Anna Bibyk <annbibik@gmail.com>
2020-09-22 13:46:51 +0300

    bottomsheet header cell changed

---
+++ Sources/OABottomSheetHeaderButtonCell.m
@@ -0,0 +1,25 @@
+//
+//  OABottomSheetHeaderButtonCell.m
+//  OsmAnd Maps
+//
+//  Created by Anna Bibyk on 22.09.2020.
+//  Copyright © 2020 OsmAnd. All rights reserved.
+//
+
+#import "OABottomSheetHeaderButtonCell.h"
+
+@implementation OABottomSheetHeaderButtonCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+
+@end

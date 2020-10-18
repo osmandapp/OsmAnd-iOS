fc5831205982345cd00f9a18c363d106aa9c41cc
nnngrach <nnngrach@gmail.com>
2020-10-19 01:08:48 +0300

    OAItemsExistViewController cells setup

--- Sources/Controllers/2.0/Settings/OAItemExistViewControllers.m
+++ Sources/Controllers/2.0/Settings/OAItemExistViewControllers.m
@@ -11,8 +11,20 @@
 #import "Localization.h"
 #import "OAColors.h"
 
+#import "OAApplicationMode.h"
+
+#import "OAQuickActionRegistry.h"
+#import "OAQuickAction.h"
+#import "OAQuickActionType.h"
+
+#import "OAMenuSimpleCell.h"
+#import "OAIconTitleButtonCell.h"
+
 #define kSidePadding 16
 #define kTopPadding 6
+#define kMenuSimpleCell @"OAMenuSimpleCell"
+#define kMenuSimpleCellNoIcon @"OAMenuSimpleCellNoIcon"
+#define kIconTitleButtonCell @"OAIconTitleButtonCell"
 
 @interface OAItemExistViewControllers () <UITableViewDelegate, UITableViewDataSource>
 
@@ -20,7 +32,10 @@
 
 @implementation OAItemExistViewControllers
 {
-    NSArray<NSDictionary *> *_data;
+    NSMutableArray<NSMutableArray<NSDictionary *> *> *_data;
+    NSArray<OAApplicationMode *> * _profileList;
+    NSArray<OAQuickActionType *> *_quickActionsList;
+
     
     CGFloat _heightForHeader;
 }
@@ -30,9 +45,71 @@
     self = [super init];
     if (self)
     {
+        [self commonInit];
     }
     return self;
 }
+
+- (void) commonInit
+{
+    [self generateData];
+}
+
+- (void) generateData
+{
+    //TODO: for now here is generating fake data, just for demo
+    
+    _data = [NSMutableArray new];
+    
+    _profileList = [NSArray arrayWithObject:OAApplicationMode.CAR];
+    
+    NSArray<OAQuickActionType *> *allQuickActions = [[OAQuickActionRegistry sharedInstance] produceTypeActionsListWithHeaders];
+    _quickActionsList = [allQuickActions subarrayWithRange:NSMakeRange(1,2)];
+    
+
+    if (_profileList.count > 0)
+    {
+        //TODO: add header strings to LocalizableStrings
+        
+        NSMutableArray<NSDictionary *> *profileItems = [NSMutableArray new];
+        [profileItems addObject: @{
+            @"type": @"header",
+            @"label": @"Profiles",
+            @"description": @"Listed profiles, already exist in OsmAnd."
+        }];
+        for (OAApplicationMode *profile in _profileList)
+        {
+            [profileItems addObject: @{
+                @"type": @"profile",
+                @"label": profile.toHumanString,
+                @"description": profile.getProfileDescription,
+                @"icon": profile.getIcon,
+                @"iconColor": UIColorFromRGB(profile.getIconColor)
+            }];
+        }
+        [_data addObject:profileItems];
+    }
+    
+    if (_quickActionsList.count > 0)
+    {
+        NSMutableArray<NSDictionary *> *quickActionsItems = [NSMutableArray new];
+        [quickActionsItems addObject: @{
+            @"type": @"header",
+            @"label": @"Quick actions",
+            @"description": @"Listed quick actions, already exist in OsmAnd."
+        }];
+        for (OAQuickActionType *action in _quickActionsList)
+        {
+            [quickActionsItems addObject: @{
+                @"type": @"quickAction",
+                @"label": action.name,
+                @"iconName": action.iconName,
+                @"secondaryIconName": action.hasSecondaryIcon ? [action createNew].getSecondaryIconName : @""
+            }];
+        }
+        [_data addObject:quickActionsItems];
+    }
+};
 
 - (void) applyLocalization
 {
@@ -65,45 +142,149 @@
     [self setToButton: self.primaryBottomButton firstLabelText:OALocalizedString(@"replace_all") firstLabelFont:[UIFont systemFontOfSize:15 weight:UIFontWeightSemibold] firstLabelColor:[UIColor whiteColor] secondLabelText:OALocalizedString(@"replace_all_desc") secondLabelFont:[UIFont systemFontOfSize:13] secondLabelColor:[[UIColor whiteColor] colorWithAlphaComponent:0.5]];
 }
 
-- (NSArray<NSString *> *) getTestData
-{
-    return @[@"Driving", @"Add favorite", @"Add POI"];
-}
-
 - (UIView *) tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
 {
-    UIView *vw = [[UIView alloc] initWithFrame:CGRectMake(0, 0.0, tableView.bounds.size.width - OAUtilities.getLeftMargin * 2, _heightForHeader)];
-    CGFloat textWidth = self.tableView.bounds.size.width - (kSidePadding + OAUtilities.getLeftMargin) * 2;
-    UILabel *description = [[UILabel alloc] initWithFrame:CGRectMake(kSidePadding + OAUtilities.getLeftMargin, 6.0, textWidth, _heightForHeader)];
-    UIFont *labelFont = [UIFont systemFontOfSize:15.0];
-    description.font = labelFont;
-    [description setTextColor: UIColorFromRGB(color_text_footer)];
-    NSMutableParagraphStyle *style = [[NSMutableParagraphStyle alloc] init];
-    [style setLineSpacing:6];
-    description.attributedText = [[NSAttributedString alloc] initWithString:OALocalizedString(@"import_duplicates_description") attributes:@{NSParagraphStyleAttributeName : style}];
-    description.numberOfLines = 0;
-    [vw addSubview:description];
-    return vw;
+    if (section == 0)
+    {
+        UIView *vw = [[UIView alloc] initWithFrame:CGRectMake(0, 0.0, tableView.bounds.size.width - OAUtilities.getLeftMargin * 2, _heightForHeader)];
+        CGFloat textWidth = self.tableView.bounds.size.width - (kSidePadding + OAUtilities.getLeftMargin) * 2;
+        UILabel *description = [[UILabel alloc] initWithFrame:CGRectMake(kSidePadding + OAUtilities.getLeftMargin, 6.0, textWidth, _heightForHeader)];
+        UIFont *labelFont = [UIFont systemFontOfSize:15.0];
+        description.font = labelFont;
+        [description setTextColor: UIColorFromRGB(color_text_footer)];
+        NSMutableParagraphStyle *style = [[NSMutableParagraphStyle alloc] init];
+        [style setLineSpacing:6];
+        description.attributedText = [[NSAttributedString alloc] initWithString:OALocalizedString(@"import_duplicates_description") attributes:@{NSParagraphStyleAttributeName : style}];
+        description.numberOfLines = 0;
+        [vw addSubview:description];
+        return vw;
+    }
+    else
+    {
+        return nil;
+    }
 }
 
 - (CGFloat) tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
 {
-    _heightForHeader = [self heightForLabel:OALocalizedString(@"import_duplicates_description")];
-    return _heightForHeader + kSidePadding + kTopPadding;
+    if (section == 0)
+    {
+        _heightForHeader = [self heightForLabel:OALocalizedString(@"import_duplicates_description")];
+        return _heightForHeader + kSidePadding + kTopPadding;
+    }
+    else
+    {
+        return UITableViewAutomaticDimension;
+    }
 }
 
-- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [self getTestData].count;
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSDictionary *item = _data[indexPath.section][indexPath.row];
+    
+    if ([item[@"type"] isEqualToString:@"header"])
+    {
+        return 56.;
+    }
+    else if ([item[@"type"] isEqualToString:@"profile"])
+    {
+        return 60.;
+    }
+    else if ([item[@"type"] isEqualToString:@"quickAction"])
+    {
+        return 44.;
+    }
+    else
+    {
+        return 48.;
+    }
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return _data.count;
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return _data[section].count;
 }
 
 - (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath
 {
-    NSString *text = [self getTestData][indexPath.row];
-    UITableViewCell *cell = [[UITableViewCell alloc] init];
-    [cell.textLabel setText:text];
-    return cell;
+    NSDictionary *item = _data[indexPath.section][indexPath.row];
+    
+    if ([item[@"type"] isEqualToString:@"header"])
+    {
+        NSString* const identifierCell = kMenuSimpleCellNoIcon;
+        OAMenuSimpleCell* cell;
+        cell = (OAMenuSimpleCell *)[tableView dequeueReusableCellWithIdentifier:identifierCell];
+        if (cell == nil)
+        {
+            NSArray *nib = [[NSBundle mainBundle] loadNibNamed:kMenuSimpleCellNoIcon owner:self options:nil];
+            cell = (OAMenuSimpleCell *)[nib objectAtIndex:0];
+            cell.separatorInset = UIEdgeInsetsMake(0.0, 20.0, 0.0, 0.0);
+        }
+        cell.textView.text = item[@"label"];
+        cell.descriptionView.text = item[@"description"];
+        return cell;
+    }
+    else if ([item[@"type"] isEqualToString:@"profile"])
+    {
+        NSString* const identifierCell = kMenuSimpleCell;
+        OAMenuSimpleCell* cell;
+        cell = (OAMenuSimpleCell *)[tableView dequeueReusableCellWithIdentifier:identifierCell];
+        if (cell == nil)
+        {
+            NSArray *nib = [[NSBundle mainBundle] loadNibNamed:kMenuSimpleCell owner:self options:nil];
+            cell = (OAMenuSimpleCell *)[nib objectAtIndex:0];
+            cell.separatorInset = UIEdgeInsetsMake(0.0, 62., 0.0, 0.0);
+        }
+        cell.textView.text = item[@"label"];
+        cell.descriptionView.text = item[@"description"];
+        cell.imgView.image = [item[@"icon"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+        //cell.imgView.tintColor = item[@"iconColor"];
+        cell.imgView.tintColor = UIColorFromRGB(color_chart_orange);
+        return cell;
+    }
+    else if ([item[@"type"] isEqualToString:@"quickAction"])
+    {
+        NSString* const identifierCell = kIconTitleButtonCell;
+        OAIconTitleButtonCell* cell = [tableView dequeueReusableCellWithIdentifier:identifierCell];
+        if (cell == nil)
+        {
+            NSArray *nib = [[NSBundle mainBundle] loadNibNamed:@"OAIconTitleButtonCell" owner:self options:nil];
+            cell = (OAIconTitleButtonCell *)[nib objectAtIndex:0];
+        }
+        if (cell)
+        {
+            cell.separatorInset = UIEdgeInsetsMake(0., 62., 0., 0.);
+            cell.titleView.text = item[@"label"];
+            cell.iconView.image = [UIImage imageNamed:item[@"iconName"]];
+            if (cell.iconView.subviews.count > 0)
+                [[cell.iconView subviews] makeObjectsPerformSelector:@selector(removeFromSuperview)];
+            
+            NSString* secondaryIconName = item[@"secondaryIconName"];
+            if (secondaryIconName.length > 0)
+            {
+                CGRect frame = CGRectMake(0., 0., cell.iconView.frame.size.width, cell.iconView.frame.size.height);
+                UIImage *imgBackground = [[UIImage imageNamed:@"ic_custom_compound_action_background"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+                UIImageView *background = [[UIImageView alloc] initWithImage:imgBackground];
+                [background setTintColor:UIColor.whiteColor];
+                [cell.iconView addSubview:background];
+                UIImage *img = [UIImage imageNamed:item[@"secondaryIconName"]];
+                UIImageView *view = [[UIImageView alloc] initWithImage:img];
+                view.frame = frame;
+                [cell.iconView addSubview:view];
+            }
+            [cell setButtonText:nil];
+            cell.buttonView.tag = indexPath.section << 10 | indexPath.row;
+            [cell.buttonView setImage:[[UIImage imageNamed:@"ic_custom_plus"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal] forState:UIControlStateNormal];
+            [cell.buttonView addTarget:self action:@selector(addAction:) forControlEvents:UIControlEventTouchUpInside];
+            cell.buttonView.imageEdgeInsets = UIEdgeInsetsMake(0., cell.buttonView.frame.size.width - 30, 0, 0);
+        }
+        return cell;
+    }
 }
-
-
 
 @end

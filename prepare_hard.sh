54c46f34548f5207716992212a7d0a299f9fad14
nnngrach <nnngrach@gmail.com>
2021-01-20 15:29:07 +0300

    Update prepare_hard.sh

--- prepare_hard.sh
+++ prepare_hard.sh
@@ -5,16 +5,13 @@ echo "iOS project reset and prepare - START."
 echo "    Updating all repositories:"
 cd ios && git pull origin master && cd ../android && git pull origin master && cd ../resources && git pull origin master && cd ../core && git pull origin master && cd ../core-legacy && git pull origin legacy_core && cd ../help && git pull origin master
 
-
 cd ..
 echo "    Deledeing build folders:"
 sudo rm -R baked
 sudo rm -R binaries
-rm -rf ~/Library/Developer/Xcode/DerivedData/*
-
+sudo rm -R ~/Library/Developer/Xcode/DerivedData/*
 
 echo "    Redownloading dependencyes:"
 ./ios/prepare.sh
 
 echo "iOS project reset and prepare - DONE."
-

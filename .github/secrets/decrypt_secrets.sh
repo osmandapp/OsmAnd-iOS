afd8bb3e06c3245e56e36d2b354ef8885e2bd6bd
Victor Shcherb <victor.shcherb@gmail.com>
2021-04-15 19:51:09 +0200

    Update secrets

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -9,11 +9,11 @@ mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./.github/secrets/Github_Build.mobileprovision.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/Github_Build.mobileprovision.mobileprovision
 
 
-security create-keychain -p "" build.keychain
-security import ./.github/secrets/distribution.cer -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+security create-keychain -p "$PASSWORD_KEYCHAIN_EXPORT" build.keychain
+security import ./.github/secrets/distribution.cer -t agg -k ~/Library/Keychains/build.keychain -P "$PASSWORD_KEYCHAIN_EXPORT" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
-security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+security unlock-keychain -p "$PASSWORD_KEYCHAIN_EXPORT" ~/Library/Keychains/build.keychain
 
 security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain

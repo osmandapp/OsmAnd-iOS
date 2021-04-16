7383f659ea762f0d2a55cbfa341bf89d35f2cc4f
Victor Shcherb <victor.shcherb@gmail.com>
2021-04-16 12:05:17 +0200

    Refactor scripts

--- prepare.sh
+++ prepare.sh
@@ -7,6 +7,12 @@ SRCLOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Bake or update core projects for XCode
 OSMAND_BUILD_TOOL=xcode "$SRCLOC/../build/fat-ios.sh"
+if [ ! -z "$BUILT_QT_FILES_ZIPFILE" ]; then
+	TMP_DIR=$(basename $BUILT_QT_FILES_ZIPFILE).dir
+	mkdir -p $TMP_DIR
+	cp -r $SRCLOC/../core/externals/qtbase-ios/upstream.patched* $TMP_DIR/
+	zip -r  "$BUILT_QT_FILES_ZIPFILE" "$TMP_DIR/*"
+fi
 
 # Download all shipped resources
 "$SRCLOC/Scripts/download-shipped-resources.sh"

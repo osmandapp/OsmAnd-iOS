cde50218b1ca5ac5ac8ab4c571dac6be44f48030
Victor Shcherb <victor.shcherb@gmail.com>
2021-04-16 15:53:03 +0200

    Finish implementation of ios prebuilt

--- prepare.sh
+++ prepare.sh
@@ -8,7 +8,10 @@ SRCLOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [ "$DOWNLOAD_PREBUILT_QT_FILES" = true ] ; then
 	BUILT_QT_FILES_ZIPFILE=${BUILT_QT_FILES_ZIPFILE:-qt-ios-prebuilt.zip}
 	wget https://builder.osmand.net/binaries/ios/qt-ios-prebuilt.zip -O "$BUILT_QT_FILES_ZIPFILE"
-	unzip -o -d $SRCLOC/../core/externals/qtbase-ios/ "$BUILT_QT_FILES_ZIPFILE"
+	TMPDIR=$(basename $BUILT_QT_FILES_ZIPFILE).dir
+	unzip -o -d $TMPDIR "$BUILT_QT_FILES_ZIPFILE"
+	mv $TMPDIR/upstream.patched* $SRCLOC/../core/externals/qtbase-ios/
+	rm -rf $TMPDIR
 fi
 
 # Bake or update core projects for XCode
@@ -16,12 +19,9 @@ OSMAND_BUILD_TOOL=xcode "$SRCLOC/../build/fat-ios.sh"
 
 # Package built qt files as zip file
 if [ ! -z "$BUILT_QT_FILES_ZIPFILE" ] && [ ! -f "$BUILT_QT_FILES_ZIPFILE" ]; then
-	TMP_DIR=$(basename $BUILT_QT_FILES_ZIPFILE).dir
-	mkdir -p $TMP_DIR
-	cp -r $SRCLOC/../core/externals/qtbase-ios/upstream.patched* $TMP_DIR/
-	( cd "$TMP_DIR"/ && zip -r "$TMP_DIR.zip" . )
-	mv $TMP_DIR/$TMP_DIR.zip $BUILT_QT_FILES_ZIPFILE
-	rm -rf $TMP_DIR/
+	BNAME=$(basename $BUILT_QT_FILES_ZIPFILE)
+	( cd $SRCLOC/../core/externals/qtbase-ios/ && zip --symlinks -r "$BNAME" . )
+	mv $SRCLOC/../core/externals/qtbase-ios/$BNAME $BUILT_QT_FILES_ZIPFILE
 fi
 
 # Download all shipped resources

44f2477cf9b8e562a3da9a6cdee3862023b7c3d4
alisa911 <alisa.91150@gmail.com>
2021-04-21 19:17:49 +0300

    add revision

--- Sources/Search/OASearchCoreFactory.h
+++ Sources/Search/OASearchCoreFactory.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 OsmAnd. All rights reserved.
 //
 //  OsmAnd-java/src/net/osmand/search/core/SearchCoreFactory.java
-//  git revision 9251a6bb4af017065cb7d2861baeae1df7f7b130
+//  git revision 336856975717f9997790770e30b6b394eecf548b
 
 #import <Foundation/Foundation.h>
 #import "OASearchCoreAPI.h"

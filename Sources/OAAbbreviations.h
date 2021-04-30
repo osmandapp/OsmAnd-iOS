61cd9965a83007d4c17a97e183e6b317a27cca44
alisa911 <alisa.91150@gmail.com>
2021-04-30 18:51:44 +0300

    merge java 10889

--- Sources/OAAbbreviations.h
+++ Sources/OAAbbreviations.h
@@ -5,6 +5,8 @@
 //  Created by plotva on 30.04.2021.
 //  Copyright © 2021 OsmAnd. All rights reserved.
 //
+//  OsmAnd-java/src/main/java/net/osmand/binary/Abbreviations.java
+//  git revision /54e26c5a6195beb371c210746c5d89674016b9f7
 
 #ifndef OAAbbreviations_h
 #define OAAbbreviations_h

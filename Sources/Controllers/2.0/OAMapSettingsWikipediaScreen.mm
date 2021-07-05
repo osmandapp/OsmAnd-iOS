bff3a9e5f99ef08f48d3c494eac44b1f9bb02f84
Skalii Dmitrii <skaliy.d@gmail.com>
2021-07-05 17:33:46 +0300

    add new controller for wikipedia layer;

---
+++ Sources/Controllers/2.0/OAMapSettingsWikipediaScreen.mm
@@ -0,0 +1,42 @@
+//
+//  OAMapSettingsWikipediaScreen.m
+//  OsmAnd
+//
+//  Created by Skalii on 01.07.2021.
+//  Copyright (c) 2021 OsmAnd. All rights reserved.
+//
+
+#import "OAMapSettingsWikipediaScreen.h"
+#import "OAMapSettingsViewController.h"
+
+@implementation OAMapSettingsWikipediaScreen
+{
+
+}
+
+- (id) initWithTable:(UITableView *)tableView viewController:(OAMapSettingsViewController *)viewController
+{
+    self = [super init];
+    if (self) {
+
+    }
+    return self;
+}
+
+- (void) initData
+{
+}
+
+- (void) setupView
+{
+}
+
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+}
+
+- (UITableViewCell*) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+}
+
+@end

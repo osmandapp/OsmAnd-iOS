0bf92ad9f424bc895a3a24083b7072af390301e7
alisa911 <alisa.91150@gmail.com>
2021-10-27 20:31:28 +0300

    add Fix #12481

--- OsmAnd MapsTests/Router/OARouteResultPreparationTest.mm
+++ OsmAnd MapsTests/Router/OARouteResultPreparationTest.mm
@@ -94,17 +94,23 @@
         {
             if (prevSegment >= 0)
             {
-                NSString *lanes = [self getLanesString:routeSegments[prevSegment]];
-                NSString *turn = [NSString stringWithUTF8String:routeSegments[prevSegment]->turnType->toXmlString().c_str()];
+                auto segment = routeSegments[prevSegment];
+                NSString *lanes = [self getLanesString:segment];
+                NSString *turn = [NSString stringWithUTF8String:segment->turnType->toXmlString().c_str()];
                 NSString *turnLanes = [NSString stringWithFormat:@"%@:%@", turn, lanes];
+                NSString *name = [NSString stringWithUTF8String:segment->description.c_str()];
+                bool skipToSpeak = segment->turnType->isSkipToSpeak();
+                long segmentId = segment->object->id >> 6;
                 
-                NSString *name = [NSString stringWithUTF8String:routeSegments[prevSegment]->description.c_str()];
-                
-                long segmentId = routeSegments[prevSegment]->object->id >> 6;
                 NSString *expectedResult = expectedResults[@(segmentId).stringValue];
+                
                 if (expectedResult != nil && ![expectedResult isKindOfClass:NSNull.class])
                 {
-                    if(![expectedResult isEqualToString:turnLanes] &&
+                    if ([@"skipToSpeak" isEqualToString:expectedResult])
+                    {
+                        XCTAssertTrue(skipToSpeak);
+                        NSLog(@"Test case %@ failed", testCase[@"testName"]);
+                    } else if (![expectedResult isEqualToString:turnLanes] &&
                        ![expectedResult isEqualToString:lanes] &&
                        ![expectedResult isEqualToString:turn])
                     {

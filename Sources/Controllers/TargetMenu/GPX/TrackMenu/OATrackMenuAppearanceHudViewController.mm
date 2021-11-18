d7463d8667400ba07ba5688d8cdbabb805c8930a
Skalii Dmitrii <skaliy.d@gmail.com>
2021-11-18 11:48:18 +0200

    fix ui for colorizing by attributes in track appearance

--- Sources/Controllers/TargetMenu/GPX/TrackMenu/OATrackMenuAppearanceHudViewController.mm
+++ Sources/Controllers/TargetMenu/GPX/TrackMenu/OATrackMenuAppearanceHudViewController.mm
@@ -355,6 +355,14 @@
                 kCellTitle: OALocalizedString(@"route_line_color_elevation_description")
             }];
         }
+        else if ([_selectedItem.coloringType isRouteInfoAttribute])
+        {
+            gridOrDescriptionCell = [OAGPXTableCellData withData:@{
+                    kCellKey: @"color_attribute_description",
+                    kCellType: [OATextLineViewCell getCellIdentifier],
+                    kCellTitle: OALocalizedString(@"slope_grey_color_descr")
+            }];
+        }
         return gridOrDescriptionCell;
     };
     __block OAGPXTableCellData *gridOrDescriptionCell = generateGridOrDescriptionCell();

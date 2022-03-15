deb9baa4d19980d4e1dbecda24c953f98d0e978b
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-15 09:18:17 +0800

    Delete Scripts directory

--- Scripts/delete_cmake_settings_lines.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-TOOLCHAIN=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain
-CLANG_VERSION=$(ls $TOOLCHAIN/usr/lib/clang/)
-sudo patch $TOOLCHAIN/usr/lib/clang/$CLANG_VERSION/include/ia32intrin.h ../.github/workflows/__bsfd.patch 

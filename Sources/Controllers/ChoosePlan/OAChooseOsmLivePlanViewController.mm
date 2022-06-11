780f3cfff9753324197871340a4624aebd41f8c7
Skalii <skaliidmitrii@gmail.com>
2022-06-11 20:47:38 +0300

    fix OsmAnd Live feature list

--- Sources/Controllers/ChoosePlan/OAChooseOsmLivePlanViewController.mm
+++ Sources/Controllers/ChoosePlan/OAChooseOsmLivePlanViewController.mm
@@ -65,7 +65,6 @@
                              [[OAFeature alloc] initWithFeature:EOAFeatureUnlimitedDownloads],
                              [[OAFeature alloc] initWithFeature:EOAFeatureWikipediaOffline],
                              [[OAFeature alloc] initWithFeature:EOAFeatureContourLinesHillshadeMaps],
-                             [[OAFeature alloc] initWithFeature:EOAFeatureWeather],
                              [[OAFeature alloc] initWithFeature:EOAFeatureSeaDepthMaps]];
     
     _iapHelper = [OAIAPHelper sharedInstance];

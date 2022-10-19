f15a18ad3f9e03f82aae28952756dd34f0c15ccc
nnngrach <nnngrach@gmail.com>
2022-10-19 23:53:19 +0500

    fps_widget

--- Sources/Plugins/Development/OAOsmandDevelopmentPlugin.m
+++ Sources/Plugins/Development/OAOsmandDevelopmentPlugin.m
@@ -8,10 +8,32 @@
 
 #import "OAOsmandDevelopmentPlugin.h"
 #import "OAProducts.h"
+#import "OsmAndApp.h"
+#import "OAAppSettings.h"
+#import "Localization.h"
+#import "OAMapInfoController.h"
+#import "OATextInfoWidget.h"
+#import "OAFPSTextInfoWidget.h"
 
 #define PLUGIN_ID kInAppId_Addon_OsmandDevelopment
 
 @implementation OAOsmandDevelopmentPlugin
+{
+    OsmAndAppInstance _app;
+    OAAppSettings *_settings;
+    OAFPSTextInfoWidget *_fpsWidgetControl;
+}
+
+- (instancetype) init
+{
+    self = [super init];
+    if (self)
+    {
+        _app = [OsmAndApp instance];
+        _settings = [OAAppSettings sharedManager];
+    }
+    return self;
+}
 
 - (NSString *) getId
 {
@@ -21,6 +43,43 @@
 - (BOOL)isEnableByDefault
 {
     return NO;
+}
+
+- (void) registerLayers
+{
+    [self registerWidget];
+}
+
+- (void) registerWidget
+{
+    OAMapInfoController *mapInfoController = [self getMapInfoController];
+    if (mapInfoController)
+    {
+        _fpsWidgetControl = [[OAFPSTextInfoWidget alloc] init];
+        [mapInfoController registerSideWidget:_fpsWidgetControl imageId:@"ic_custom_timer" message:OALocalizedString(@"map_widget_rendering_fps") key:PLUGIN_ID left:false priorityOrder:99];
+        [mapInfoController recreateControls];
+    }
+}
+
+- (void) updateLayers
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if ([self isEnabled])
+        {
+            if (!_fpsWidgetControl)
+                [self registerWidget];
+        }
+        else
+        {
+            if (_fpsWidgetControl)
+            {
+                OAMapInfoController *mapInfoController = [self getMapInfoController];
+                [mapInfoController removeSideWidget:_fpsWidgetControl];
+                [mapInfoController recreateControls];
+                _fpsWidgetControl = nil;
+            }
+        }
+    });
 }
 
 @end

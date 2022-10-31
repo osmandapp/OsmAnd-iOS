de48e4f770d9f75131a6c1fb98a43908c1fc83a6
Dmitry Svetlichny <svetlichny1988@icloud.com>
2022-10-31 12:54:25 +0200

    fix

--- Sources/Controllers/Settings/ImportExport/OAPluginInstalledViewController.mm
+++ Sources/Controllers/Settings/ImportExport/OAPluginInstalledViewController.mm
@@ -289,6 +289,7 @@ typedef NS_ENUM(NSInteger, EOAPluginSectionType) {
         if (product)
         {
             [_iapHelper disableProduct:_pluginId];
+            if ([product.productIdentifier isEqual: kInAppId_Addon_Mapillary])
             [_app.data setMapillary:NO];
         }
         else

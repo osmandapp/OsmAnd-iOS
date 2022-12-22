8aba5ce941f70dce2aec0704ff910e981a98a124
Skalii <skaliidmitrii@gmail.com>
2022-12-22 23:18:26 +0200

    add OATextMultilineTableViewCell;
    remove OATextMultiViewCell;

--- Sources/Controllers/TargetMenu/OATargetInfoViewController.mm
+++ Sources/Controllers/TargetMenu/OATargetInfoViewController.mm
@@ -41,7 +41,7 @@
 #import "OAButtonRightIconCell.h"
 #import "OAMapillaryOsmTagHelper.h"
 #import "OACollapsableWaypointsView.h"
-#import "OATextMultiViewCell.h"
+#import "OATextMultilineTableViewCell.h"
 #import "OAEditDescriptionViewController.h"
 
 #include <OsmAndCore/Utilities.h>
@@ -749,14 +749,15 @@
     }
     else if ([info.typeName isEqualToString:kDescriptionRowType])
     {
-        OATextMultiViewCell* cell;
-        cell = (OATextMultiViewCell *)[tableView dequeueReusableCellWithIdentifier:[OATextMultiViewCell getCellIdentifier]];
+        OATextMultilineTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:[OATextMultilineTableViewCell getCellIdentifier]];
         if (cell == nil)
         {
-            NSArray *nib = [[NSBundle mainBundle] loadNibNamed:[OATextMultiViewCell getCellIdentifier] owner:self options:nil];
-            cell = (OATextMultiViewCell *)[nib objectAtIndex:0];
+            NSArray *nib = [[NSBundle mainBundle] loadNibNamed:[OATextMultilineTableViewCell getCellIdentifier] owner:self options:nil];
+            cell = (OATextMultilineTableViewCell *) nib[0];
+            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+            [cell leftIconVisibility:NO];
+            [cell clearButtonVisibility:NO];
         }
-        
         NSString *label = info.text;
         if (label.length == 0)
         {
@@ -764,30 +765,18 @@
             cell.textView.textContainerInset = UIEdgeInsetsMake(11,11,0,0);
             cell.textView.text = info.textPrefix;
             cell.textView.textColor = [UIColor lightGrayColor];
-            cell.iconView.hidden = NO;
         }
         else
         {
             cell.textView.font = [UIFont systemFontOfSize:14.0];
             cell.textView.textColor = [UIColor blackColor];
             cell.textView.text = label;
-            cell.iconView.hidden = NO;
             
             CGSize s = [OAUtilities calculateTextBounds:info.text width:self.tableView.bounds.size.width - 38.0 font:[UIFont systemFontOfSize:14.0]];
             CGFloat h = MIN(188.0, s.height + 10.0);
-            h = MAX(44.0, h);
+            h = MAX(48.0, h);
             info.height = h;
-            
-            BOOL descSingleLine = (s.height < 24.0);
-            if (descSingleLine)
-                cell.textView.textContainerInset = UIEdgeInsetsMake(12,11,0,35);
-            else if (info.height > 44.0)
-                cell.textView.textContainerInset = UIEdgeInsetsMake(5,11,0,35);
-            else
-                cell.textView.textContainerInset = UIEdgeInsetsMake(3,11,0,35);
         }
-        cell.textView.backgroundColor = UIColorFromRGB(0xffffff);
-        cell.backgroundColor = UIColorFromRGB(0xffffff);
         return cell;
     }
     

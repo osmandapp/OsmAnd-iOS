c11f9f1cf16cf9985f76c2d330eeda3b80f4b7f5
Dmitry Svetlichny <svetlichny1988@icloud.com>
2023-05-24 16:11:25 +0300

    Refactoring OAOnlineTilesEditingViewController OAOnlineTilesSettingsViewController

--- Sources/Controllers/Resources/OAOnlineTilesEditingViewController.h
+++ Sources/Controllers/Resources/OAOnlineTilesEditingViewController.h
@@ -6,7 +6,7 @@
 //  Copyright © 2020 OsmAnd. All rights reserved.
 //
 
-#import "OACompoundViewController.h"
+#import "OABaseNavbarViewController.h"
 #import "OAResourcesBaseViewController.h"
 
 typedef NS_ENUM(NSInteger, EOASourceFormat)
@@ -23,12 +23,7 @@ typedef NS_ENUM(NSInteger, EOASourceFormat)
 
 @class OAResourcesBaseViewController;
 
-@interface OAOnlineTilesEditingViewController : OACompoundViewController<UITableViewDelegate, UITableViewDataSource>
-@property (weak, nonatomic) IBOutlet UITableView *tableView;
-@property (weak, nonatomic) IBOutlet UILabel *titleView;
-@property (weak, nonatomic) IBOutlet UIButton *saveButton;
-@property (weak, nonatomic) IBOutlet UIButton *backButton;
-@property (weak, nonatomic) IBOutlet UIView *navBarView;
+@interface OAOnlineTilesEditingViewController : OABaseNavbarViewController
 
 @property (nonatomic) id<OATilesEditingViewControllerDelegate> delegate;
 

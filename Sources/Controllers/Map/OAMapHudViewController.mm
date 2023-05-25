5918999dca41cddd1fad076b316a20c94f8a6023
PaulStets <stetsenko.pavlo@gmail.com>
2023-05-25 09:54:37 +0200

    Minor fix

--- Sources/Controllers/Map/OAMapHudViewController.mm
+++ Sources/Controllers/Map/OAMapHudViewController.mm
@@ -1206,7 +1206,7 @@
             CLLocation *currentLocation = _app.locationServices.lastKnownLocation;
             if (currentLocation)
             {
-                if (!_previousLocation || (_previousLocation && [currentLocation distanceFromLocation:_previousLocation] > kDistanceMeters))
+                if (!_previousLocation || [currentLocation distanceFromLocation:_previousLocation] > kDistanceMeters)
                 {
                     NSString *positionAddress;
                     _previousLocation = currentLocation;

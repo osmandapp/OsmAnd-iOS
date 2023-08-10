0ae2001b6faab590d5c393734ff1ca1cf123da82
Aleksandr Panchenko <>
2023-08-10 17:56:35 +0300

    update_only_over_wi_fi key new translation

--- Sources/Controllers/Resources/OAWeatherAutoUpdateSettingsViewController/OAWeatherAutoUpdateSettingsViewController.mm
+++ Sources/Controllers/Resources/OAWeatherAutoUpdateSettingsViewController/OAWeatherAutoUpdateSettingsViewController.mm
@@ -1,9 +1,9 @@
 //
-//  OAWeatherFrequencySettingsViewController.mm
-//  OsmAnd
+//  OAWeatherFrequencySettingsViewController.swift
+//  OsmAnd Maps
 //
-//  Created by Skalii on 11.08.2022.
-//  Copyright (c) 2022 OsmAnd. All rights reserved.
+//  Created by Oleksandr Panchenko on 10.08.2023.
+//  Copyright © 2023 OsmAnd. All rights reserved.
 //
 
 #import "OAWeatherAutoUpdateSettingsViewController.h"

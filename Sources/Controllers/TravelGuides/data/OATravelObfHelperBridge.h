79d15ba02fee7cf8db1e46c5f596b86bc6b7f607
nnngrach <nnngrach@gmail.com>
2023-08-10 21:05:36 +0500

    TravelObfHelperUpdated

--- Sources/Controllers/TravelGuides/data/OATravelObfHelperBridge.h
+++ Sources/Controllers/TravelGuides/data/OATravelObfHelperBridge.h
@@ -6,8 +6,13 @@
 //  Copyright © 2023 OsmAnd. All rights reserved.
 //
 
+@class OAWptPt, OAPOI;
+
+
 @interface OATravelObfHelperBridge : NSObject
 
 + (void) foo:(double)lat lon:(double)lon;
+
++ (OAWptPt *) createWptPt:(OAPOI *)amenity lang:(NSString *)lang;
 
 @end
--- Sources/Controllers/TravelGuides/data/OATravelObfHelperBridge.mm
+++ Sources/Controllers/TravelGuides/data/OATravelObfHelperBridge.mm
@@ -7,8 +7,10 @@
 //
 
 #import "OATravelObfHelperBridge.h"
+#import "OAGPXDocumentPrimitives.h"
 #import "OAPOIHelper.h"
 #import "OsmAndApp.h"
+#import "OAPOI.h"
 
 #include <OsmAndCore/Utilities.h>
 
@@ -24,6 +26,31 @@
     NSArray<OAPOI *> *points1 = [OAPOIHelper findTravelGuides:@"route_article" location:locI radius:100000];
     //NSArray<OAPOI *> *points2 = [OAPOIHelper findTravelGuides:@"route_track" location:locI radius:100000];
     int a = 0;
+}
+
++ (OAWptPt *) createWptPt:(OAPOI *)amenity lang:(NSString *)lang
+{
+    OAWptPt *wptPt = [[OAWptPt alloc] init];
+    wptPt.name = amenity.name;
+    wptPt.position = CLLocationCoordinate2DMake(amenity.latitude, amenity.longitude);
+    wptPt.desc = amenity.desc;
+    
+    if ([amenity getSite])
+        wptPt.links = @[ [amenity getSite] ];
+    
+    NSString *color = [amenity getColor];
+    OAGPXColor *gpxColor = [OAGPXColor getColorFromName:color];
+    if (gpxColor)
+        [wptPt setColor:gpxColor.color];
+    
+    if (amenity.iconName)
+        [wptPt setIcon:amenity.iconName];
+
+    NSString *category = [amenity getTagSuffix:@"category_"];
+    if (category)
+        wptPt.category = [OAUtilities capitalizeFirstLetter:category];
+
+    return wptPt;
 }
 
 @end

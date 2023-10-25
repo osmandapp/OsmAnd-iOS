3de06130afba45db87d181feab4a622058572f20
Max Kojin <nnngrach@gmail.com>
2023-10-25 22:48:56 +0500

    Add dart theme to Help web wiews

--- Sources/Controllers/TargetMenu/Wiki/OAWikiWebViewController.mm
+++ Sources/Controllers/TargetMenu/Wiki/OAWikiWebViewController.mm
@@ -541,7 +541,6 @@
         return nil;
     
     NSString *nightModeClass = [ThemeManager shared].isLightTheme ? @"" : @" nightmode";
-    NSString *head = @"<header><meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no'></header><head></head><div class=\"main%@\" >%@</div>";
     return [NSString stringWithFormat:@"<html><head> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" /> <meta http-equiv=\"cleartype\" content=\"on\" />  </head> <div class=\"main%@\">%@ </body></html>", nightModeClass, content];
 }
 

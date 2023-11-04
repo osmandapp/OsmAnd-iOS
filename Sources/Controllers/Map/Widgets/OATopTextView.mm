3ff721facd67e94f799420132e492a693a9a0016
Dmitry Svetlichny <svetlichny1988@icloud.com>
2023-11-04 17:44:00 +0200

    Exit number Fixed

--- Sources/Controllers/Map/Widgets/OATopTextView.mm
+++ Sources/Controllers/Map/Widgets/OATopTextView.mm
@@ -59,6 +59,8 @@
 @property (weak, nonatomic) IBOutlet UIButton *waypointButtonMore;
 @property (weak, nonatomic) IBOutlet UIButton *waypointButtonRemove;
 
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *exitRefTextContainerWidthConstraint;
+
 @end
 
 @implementation OATopTextView
@@ -216,10 +218,10 @@
     if (showExit)
     {
         CGSize size = [OAUtilities calculateTextBounds:_exitRefText.text width:w font:[UIFont systemFontOfSize:17 weight:UIFontWeightSemibold]];
-        CGRect textFrame = CGRectMake(3., 3., size.width, h - 10);
-        _exitRefText.frame = textFrame;
+        _exitRefText.adjustsFontSizeToFitWidth = YES;
+        _exitRefText.baselineAdjustment = UIBaselineAdjustmentAlignCenters;
         exitRefFrame.size = CGSizeMake(size.width + 6., h - 4);
-        _exitRefTextContainer.frame = exitRefFrame;
+        self.exitRefTextContainerWidthConstraint.constant = size.width;
     }
     
     CGFloat margin = _turnView.subviews.count > 0 ? 4 + _turnView.bounds.size.width + 2 : 2;

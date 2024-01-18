304d47952c9c8abe8fd57f472ac18281385bc525
Dmitry Svetlichny <svetlichny1988@icloud.com>
2024-01-18 10:44:28 +0200

    Add MapSettingsGpxViewController

---
+++ Sources/Controllers/DashboardOnMap/MapSettings/MapSettingsGpxViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MapSettingsGpxViewController.swift
+//  OsmAnd Maps
+//
+//  Created by ДМИТРИЙ СВЕТЛИЧНЫЙ on 09.01.2024.
+//  Copyright © 2024 OsmAnd. All rights reserved.
+//
+
+import Foundation

b72e868398cad0ce01adbff089f89330733dd4e4
Skalii <skaliidmitrii@gmail.com>
2024-02-13 18:42:48 +0200

    remove unnecessary code;

--- Sources/Controllers/Map/OAMapViewTrackingUtilities.h
+++ Sources/Controllers/Map/OAMapViewTrackingUtilities.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
 
-@class OAMapViewController, OAObservable;
+@class OAMapViewController;
 
 @interface OAMapViewTrackingUtilities : NSObject
 

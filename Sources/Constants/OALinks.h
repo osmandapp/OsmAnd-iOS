acd621cd12e7ef888bec283f83a440ef1fb6ac52
Victor Shcherb <victor.shcherb@gmail.com>
2024-04-16 14:12:15 +0300

    Prepare 47 release

--- Sources/Constants/OALinks.h
+++ Sources/Constants/OALinks.h
@@ -18,8 +18,8 @@
 #define kOsmAndGoPathPrefix @"/go"
 #define kOsmAndMapPathPrefix @"/map"
 
-//#define kDocsLatestVersion @"https://docs.osmand.net/blog/osmand-ios-4-3-released"
-#define kDocsLatestVersion  @"https://docs.osmand.net/docs/versions/future-ios"
+#define kDocsLatestVersion @"https://docs.osmand.net/blog/osmand-ios-4-7-released"
+//#define kDocsLatestVersion  @"https://docs.osmand.net/docs/versions/future-ios"
 #define kBlogGuideline  @"https://osmand.net/blog/guideline-pt"
 
 #define kSupportEmail @"support@osmand.net"

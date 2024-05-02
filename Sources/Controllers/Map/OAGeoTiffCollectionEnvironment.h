79af883e710b8ef6a6083bcf636b4429ec77e8f9
Skalii <skaliidmitrii@gmail.com>
2024-05-02 13:52:30 +0300

    remove sync getHeightsForPoints;

---
+++ Sources/Controllers/Map/OAGeoTiffCollectionEnvironment.h
@@ -0,0 +1,13 @@
+//
+//  OAGeoTiffCollectionEnvironment.h
+//  OsmAnd
+//
+//  Created by Дмитро Скалій on 02.05.2024.
+//  Copyright © 2024 OsmAnd. All rights reserved.
+//
+
+#ifndef OAGeoTiffCollectionEnvironment_h
+#define OAGeoTiffCollectionEnvironment_h
+
+
+#endif /* OAGeoTiffCollectionEnvironment_h */
---
+++ Sources/Controllers/Map/OAGeoTiffCollectionEnvironment.mm
@@ -0,0 +1,9 @@
+//
+//  OAGeoTiffCollectionEnvironment.m
+//  OsmAnd Maps
+//
+//  Created by Дмитро Скалій on 02.05.2024.
+//  Copyright © 2024 OsmAnd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>

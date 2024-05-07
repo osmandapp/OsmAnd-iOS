e97965c12f14ede20491b93183e5c98622efc81c
Max Kojin <nnngrach@gmail.com>
2024-05-07 22:30:07 +0500

    remove v1 cell download helper

--- Sources/OsmAnd Maps-Bridging-Header.h
+++ Sources/OsmAnd Maps-Bridging-Header.h
@@ -21,7 +21,6 @@
 #import "OAMapViewTrackingUtilities.h"
 #import "OAUtilities.h"
 #import "OAQuickActionRegistry.h"
-#import "OADownloadingCellHelper.h"
 #import "OADownloadingCellResourceHelper.h"
 #import "OAWikiArticleHelper.h"
 #import "OAGPXDatabase.h"

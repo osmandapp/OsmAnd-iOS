729db6881e21cf3b2060f4c5b3d67c618c6ed756
Max Kojin <nnngrach@gmail.com>
2024-05-13 19:52:02 +0500

    always show "add wpt" button in context menu

--- Sources/Controllers/BottomSheet/MoreOptionsBottomSheet/OAMoreOptionsBottomSheetViewController.mm
+++ Sources/Controllers/BottomSheet/MoreOptionsBottomSheet/OAMoreOptionsBottomSheetViewController.mm
@@ -130,7 +130,6 @@
         for (OAFunctionalAddon *addon in _iapHelper.functionalAddons)
         {
             if ([addon.addonId isEqualToString:kId_Addon_TrackRecording_Edit_Waypoint]
-                && _iapHelper.trackRecording.isActive
                 && (_targetPoint.type == OATargetWpt) && [_targetPoint.targetObj isKindOfClass:[OAGpxWptItem class]]) {
                 [arr addObject:@{ @"title" : addon.titleShort,
                                   @"key" : @"addon_edit_waypoint",
@@ -138,8 +137,7 @@
                                   @"type" : [OASimpleTableViewCell getCellIdentifier] } ];
             }
             else if ([addon.addonId isEqualToString:kId_Addon_TrackRecording_Add_Waypoint]
-                && (_targetPoint.type != OATargetWpt && _targetPoint.type != OATargetGPX)
-                && _iapHelper.trackRecording.isActive) {
+                && (_targetPoint.type != OATargetWpt && _targetPoint.type != OATargetGPX)) {
                 [arr addObject:@{ @"title" : addon.titleShort,
                                   @"key" : @"addon_add_waypoint",
                                   @"img" : addon.imageName,

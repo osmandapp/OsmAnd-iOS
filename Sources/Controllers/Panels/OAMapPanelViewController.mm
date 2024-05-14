1aaf9ea921526a5af42893b5c64e824b65d49c23
Max Kojin <nnngrach@gmail.com>
2024-05-14 20:13:03 +0500

    change map tilt on zoom in/out

--- Sources/Controllers/Panels/OAMapPanelViewController.mm
+++ Sources/Controllers/Panels/OAMapPanelViewController.mm
@@ -1834,12 +1834,12 @@ typedef enum
 
 - (void) targetZoomIn
 {
-    [_mapViewController animatedZoomIn];
+    [_mapViewController zoomIn];
 }
 
 - (void) targetZoomOut
 {
-    [_mapViewController animatedZoomOut];
+    [_mapViewController zoomOut];
     [_mapViewController calculateMapRuler];
 }
 

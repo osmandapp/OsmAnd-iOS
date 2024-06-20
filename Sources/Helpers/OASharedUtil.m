866a0f41897b6e3cc0519c51998e2f24383e5ce3
alex-osm <alex.morozov.osm@gmail.com>
2024-06-20 16:13:30 +0300

    Fix gpx database for ios target

--- Sources/Helpers/OASharedUtil.m
+++ Sources/Helpers/OASharedUtil.m
@@ -18,6 +18,52 @@
     [OASPlatformUtil.shared initializeAppDir:documentsPath
                                       gpxDir:gpxPath
                                xmlFactoryApi:[[OAXmlFactory alloc] init]];
+
+    //[self.class testGpxReadWrite:@"" outputFile:@""];
+    //[self.class testGpxDatabase:@""];
+}
+
+// Temporary test code
++ (void) testGpxReadWrite:(NSString *)inputFile outputFile:(NSString *)outputFile
+{
+    NSLog(@"GPX TEST - READ");
+
+    OASKFile *file = [[OASKFile alloc] initWithFilePath:inputFile];
+    OASGpxFile *gpxFile = [OASGpxUtilities.shared loadGpxFileFile:file];
+
+    NSLog(@"GPX TEST - WRITE");
+
+    file = [[OASKFile alloc] initWithFilePath:outputFile];
+    [OASGpxUtilities.shared writeGpxFileFile:file gpxFile:gpxFile];
+
+    NSLog(@"GPX TEST - DONE");
+}
+
++ (void) testGpxDatabase:(NSString *)gpxFileName
+{
+    NSLog(@"GPX DB TEST - BEGIN");
+
+    OASGpxDatabase *db = [[OASGpxDatabase alloc] init];
+
+    OASKFile *file = [[OASKFile alloc] initWithFilePath:gpxFileName];
+    OASGpxDataItem *item = [[OASGpxDataItem alloc] initWithFile:file];
+
+    NSLog(@"GPX DB TEST - ADD");
+    BOOL res = [db addItem:item];
+
+    NSLog(@"GPX DB TEST - GET");
+    OASGpxDataItem *read = [db getGpxDataItemFile:file];
+
+    NSLog(@"GPX DB TEST - UPDATE");
+    res = [db updateDataItemItem:item];
+
+    NSLog(@"GPX DB TEST - REMOVE");
+    res = [db removeFile:file];
+
+    NSLog(@"GPX DB TEST - GET");
+    read = [db getGpxDataItemFile:file];
+
+    NSLog(@"GPX DB TEST - DONE");
 }
 
 @end

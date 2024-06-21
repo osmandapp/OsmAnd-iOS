3bb9137d7620f6fc873538b567093d88ee764954
Dmitry Svetlichny <svetlichny1988@icloud.com>
2024-06-21 18:16:26 +0300

    Add keysToSections to NameTagsDetailsViewController

--- Sources/Controllers/TargetMenu/NameTagsDetailsViewController.swift
+++ Sources/Controllers/TargetMenu/NameTagsDetailsViewController.swift
@@ -16,9 +16,10 @@ import UIKit
 @objcMembers
 final class NameTagsDetailsViewController: OABaseNavbarViewController {
     private var detailsType: DetailsType
-    private var tags: [String: [String: Any]]
+    private var tags: [NSDictionary]
+    private let keysToSections: [String: String] = ["reg_name": "route_name_regional", "loc_name": "route_name_local", "name:": "shared_string_name", "nat_name": "route_name_national", "int_name": "route_name_international", "short_name": "route_name_short", "official_name": "route_name_official", "old_name": "route_name_old", "alt_name": "route_name_alt"]
     
-    init(tags: [String: [String: Any]], detailsType: DetailsType) {
+    init(tags: [NSDictionary], detailsType: DetailsType) {
         self.tags = tags
         self.detailsType = detailsType
         super.init(nibName: "OABaseNavbarViewController", bundle: nil)
@@ -43,92 +44,38 @@ final class NameTagsDetailsViewController: OABaseNavbarViewController {
     
     override func generateData() {
         tableData.clearAllData()
-        
-        if let regionalName = tags["reg_name"] {
-            let regSection = tableData.createNewSection()
-            regSection.headerText = localizedString("route_name_regional")
-            let row = regSection.createNewRow()
-            row.cellType = OASimpleTableViewCell.getIdentifier()
-            row.key = "reg_name"
-            row.title = regionalName["value"] as? String ?? ""
-            row.descr = regionalName["localizedTitle"] as? String ?? ""
-        }
-        if let localName = tags["loc_name"] {
-            let localSection = tableData.createNewSection()
-            localSection.headerText = localizedString("route_name_local")
-            let row = localSection.createNewRow()
-            row.cellType = OASimpleTableViewCell.getIdentifier()
-            row.key = "loc_name"
-            row.title = localName["value"] as? String ?? ""
-            row.descr = localName["localizedTitle"] as? String ?? ""
-        }
-        let filteredTags = tags.filter { $0.key.hasPrefix("name:") }
-        if !filteredTags.isEmpty {
-            let section = tableData.createNewSection()
-            section.headerText = localizedString("shared_string_name")
-            filteredTags.forEach { _, valueDict in
-                let title = valueDict["value"] as? String ?? ""
-                let localizedTitle = valueDict["localizedTitle"] as? String ?? ""
-                let row = section.createNewRow()
-                row.cellType = OASimpleTableViewCell.getIdentifier()
-                row.key = "country_loc_name"
-                row.title = title
-                row.descr = localizedTitle
+        for (key, header) in keysToSections {
+            if key == "name:" {
+                processNameTagsSection(header: header)
+            } else {
+                processTagsSectionForKey(key: key, header: header)
             }
         }
-        if let natName = tags["nat_name"] {
-            let natSection = tableData.createNewSection()
-            natSection.headerText = localizedString("route_name_national")
-            let row = natSection.createNewRow()
-            row.cellType = OASimpleTableViewCell.getIdentifier()
-            row.key = "nat_name"
-            row.title = natName["value"] as? String ?? ""
-            row.descr = natName["localizedTitle"] as? String ?? ""
+    }
+    
+    private func processNameTagsSection(header: String) {
+        let nameTags = tags.filter { ($0["key"] as? String)?.hasPrefix("name:") ?? false }
+        if !nameTags.isEmpty {
+            createSectionWithTags(filteredTags: nameTags, header: header, key: "country_loc_name")
         }
-        if let intName = tags["int_name"] {
-            let intSection = tableData.createNewSection()
-            intSection.headerText = localizedString("route_name_international")
-            let row = intSection.createNewRow()
-            row.cellType = OASimpleTableViewCell.getIdentifier()
-            row.key = "int_name"
-            row.title = intName["value"] as? String ?? ""
-            row.descr = intName["localizedTitle"] as? String ?? ""
+    }
+    
+    private func processTagsSectionForKey(key: String, header: String) {
+        let filteredTags = tags.filter { $0["key"] as? String == key }
+        if !filteredTags.isEmpty {
+            createSectionWithTags(filteredTags: filteredTags, header: header, key: key)
         }
-        if let shortName = tags["short_name"] {
-            let shortSection = tableData.createNewSection()
-            shortSection.headerText = localizedString("route_name_short")
-            let row = shortSection.createNewRow()
+    }
+    
+    private func createSectionWithTags(filteredTags: [NSDictionary], header: String, key: String) {
+        let section = tableData.createNewSection()
+        section.headerText = localizedString(header)
+        for tagDict in filteredTags {
+            let row = section.createNewRow()
             row.cellType = OASimpleTableViewCell.getIdentifier()
-            row.key = "short_name"
-            row.title = shortName["value"] as? String ?? ""
-            row.descr = shortName["localizedTitle"] as? String ?? ""
-        }
-        if let officialName = tags["official_name"] {
-            let officialSection = tableData.createNewSection()
-            officialSection.headerText = localizedString("route_name_official")
-            let row = officialSection.createNewRow()
-            row.cellType = OASimpleTableViewCell.getIdentifier()
-            row.key = "official_name"
-            row.title = officialName["value"] as? String ?? ""
-            row.descr = officialName["localizedTitle"] as? String ?? ""
-        }
-        if let oldName = tags["old_name"] {
-            let oldSection = tableData.createNewSection()
-            oldSection.headerText = localizedString("route_name_old")
-            let row = oldSection.createNewRow()
-            row.cellType = OASimpleTableViewCell.getIdentifier()
-            row.key = "old_name"
-            row.title = oldName["value"] as? String ?? ""
-            row.descr = oldName["localizedTitle"] as? String ?? ""
-        }
-        if let altName = tags["alt_name"] {
-            let altSection = tableData.createNewSection()
-            altSection.headerText = localizedString("route_name_alt")
-            let row = altSection.createNewRow()
-            row.cellType = OASimpleTableViewCell.getIdentifier()
-            row.key = "alt_name"
-            row.title = altName["value"] as? String ?? ""
-            row.descr = altName["localizedTitle"] as? String ?? ""
+            row.key = key
+            row.title = tagDict["value"] as? String ?? ""
+            row.descr = tagDict["localizedTitle"] as? String ?? ""
         }
     }
     

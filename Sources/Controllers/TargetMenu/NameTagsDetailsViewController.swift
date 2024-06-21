24dd6fbf8f80e4f53e440cee68275db90d40a893
Dmitry Svetlichny <svetlichny1988@icloud.com>
2024-06-21 13:48:25 +0300

    Add row.descr to NameTagsDetailsViewController

--- Sources/Controllers/TargetMenu/NameTagsDetailsViewController.swift
+++ Sources/Controllers/TargetMenu/NameTagsDetailsViewController.swift
@@ -16,9 +16,9 @@ import UIKit
 @objcMembers
 final class NameTagsDetailsViewController: OABaseNavbarViewController {
     private var detailsType: DetailsType
-    private var tags: [String: String]
+    private var tags: [String: [String: Any]]
     
-    init(tags: [String: String], detailsType: DetailsType) {
+    init(tags: [String: [String: Any]], detailsType: DetailsType) {
         self.tags = tags
         self.detailsType = detailsType
         super.init(nibName: "OABaseNavbarViewController", bundle: nil)
@@ -50,7 +50,8 @@ final class NameTagsDetailsViewController: OABaseNavbarViewController {
             let row = regSection.createNewRow()
             row.cellType = OASimpleTableViewCell.getIdentifier()
             row.key = "reg_name"
-            row.title = regionalName
+            row.title = regionalName["value"] as? String ?? ""
+            row.descr = regionalName["localizedTitle"] as? String ?? ""
         }
         if let localName = tags["loc_name"] {
             let localSection = tableData.createNewSection()
@@ -58,20 +59,21 @@ final class NameTagsDetailsViewController: OABaseNavbarViewController {
             let row = localSection.createNewRow()
             row.cellType = OASimpleTableViewCell.getIdentifier()
             row.key = "loc_name"
-            row.title = localName
+            row.title = localName["value"] as? String ?? ""
+            row.descr = localName["localizedTitle"] as? String ?? ""
         }
         let filteredTags = tags.filter { $0.key.hasPrefix("name:") }
         if !filteredTags.isEmpty {
             let section = tableData.createNewSection()
             section.headerText = localizedString("shared_string_name")
-            filteredTags.forEach { key, value in
-                let countryCode = key.split(separator: ":").dropFirst().first.map(String.init) ?? "Unknown"
-                let countryName = Locale.current.localizedString(forRegionCode: countryCode) ?? "Unknown"
+            filteredTags.forEach { _, valueDict in
+                let title = valueDict["value"] as? String ?? ""
+                let localizedTitle = valueDict["localizedTitle"] as? String ?? ""
                 let row = section.createNewRow()
                 row.cellType = OASimpleTableViewCell.getIdentifier()
                 row.key = "country_loc_name"
-                row.title = value
-                row.descr = countryName
+                row.title = title
+                row.descr = localizedTitle
             }
         }
         if let natName = tags["nat_name"] {
@@ -80,7 +82,8 @@ final class NameTagsDetailsViewController: OABaseNavbarViewController {
             let row = natSection.createNewRow()
             row.cellType = OASimpleTableViewCell.getIdentifier()
             row.key = "nat_name"
-            row.title = natName
+            row.title = natName["value"] as? String ?? ""
+            row.descr = natName["localizedTitle"] as? String ?? ""
         }
         if let intName = tags["int_name"] {
             let intSection = tableData.createNewSection()
@@ -88,7 +91,8 @@ final class NameTagsDetailsViewController: OABaseNavbarViewController {
             let row = intSection.createNewRow()
             row.cellType = OASimpleTableViewCell.getIdentifier()
             row.key = "int_name"
-            row.title = intName
+            row.title = intName["value"] as? String ?? ""
+            row.descr = intName["localizedTitle"] as? String ?? ""
         }
         if let shortName = tags["short_name"] {
             let shortSection = tableData.createNewSection()
@@ -96,7 +100,8 @@ final class NameTagsDetailsViewController: OABaseNavbarViewController {
             let row = shortSection.createNewRow()
             row.cellType = OASimpleTableViewCell.getIdentifier()
             row.key = "short_name"
-            row.title = shortName
+            row.title = shortName["value"] as? String ?? ""
+            row.descr = shortName["localizedTitle"] as? String ?? ""
         }
         if let officialName = tags["official_name"] {
             let officialSection = tableData.createNewSection()
@@ -104,7 +109,8 @@ final class NameTagsDetailsViewController: OABaseNavbarViewController {
             let row = officialSection.createNewRow()
             row.cellType = OASimpleTableViewCell.getIdentifier()
             row.key = "official_name"
-            row.title = officialName
+            row.title = officialName["value"] as? String ?? ""
+            row.descr = officialName["localizedTitle"] as? String ?? ""
         }
         if let oldName = tags["old_name"] {
             let oldSection = tableData.createNewSection()
@@ -112,15 +118,17 @@ final class NameTagsDetailsViewController: OABaseNavbarViewController {
             let row = oldSection.createNewRow()
             row.cellType = OASimpleTableViewCell.getIdentifier()
             row.key = "old_name"
-            row.title = oldName
+            row.title = oldName["value"] as? String ?? ""
+            row.descr = oldName["localizedTitle"] as? String ?? ""
         }
         if let altName = tags["alt_name"] {
             let altSection = tableData.createNewSection()
             altSection.headerText = localizedString("route_name_alt")
             let row = altSection.createNewRow()
             row.cellType = OASimpleTableViewCell.getIdentifier()
-            row.key = "old_name"
-            row.title = altName
+            row.key = "alt_name"
+            row.title = altName["value"] as? String ?? ""
+            row.descr = altName["localizedTitle"] as? String ?? ""
         }
     }
     
@@ -130,7 +138,7 @@ final class NameTagsDetailsViewController: OABaseNavbarViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: OASimpleTableViewCell.getIdentifier(), for: indexPath) as! OASimpleTableViewCell
         cell.selectionStyle = .none
         cell.leftIconVisibility(false)
-        cell.descriptionVisibility(item.key == "country_loc_name")
+        cell.descriptionVisibility(!(item.descr?.isEmpty ?? true))
         cell.titleLabel.text = item.title
         cell.descriptionLabel.text = item.descr
         return cell

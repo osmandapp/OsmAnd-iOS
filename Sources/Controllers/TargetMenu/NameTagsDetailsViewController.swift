a596f58c669833a51df3fc2ae36b7f5d825c5455
Dmitry Svetlichny <svetlichny1988@icloud.com>
2024-06-24 21:42:44 +0300

    Add Loc Strings

--- Sources/Controllers/TargetMenu/NameTagsDetailsViewController.swift
+++ Sources/Controllers/TargetMenu/NameTagsDetailsViewController.swift
@@ -8,20 +8,14 @@
 
 import UIKit
 
-@objc enum DetailsType: Int {
-    case routeNameTags, poiNameTags
-}
-
 @objc(OANameTagsDetailsViewController)
 @objcMembers
 final class NameTagsDetailsViewController: OABaseNavbarViewController {
-    private var detailsType: DetailsType
     private var tags: [NSDictionary]
-    private let keysToSections: [String: String] = ["reg_name": "route_name_regional", "loc_name": "route_name_local", "name:": "shared_string_name", "nat_name": "route_name_national", "int_name": "route_name_international", "short_name": "route_name_short", "official_name": "route_name_official", "old_name": "route_name_old", "alt_name": "route_name_alt"]
+    private let keysToSections: [String: String] = ["reg_name": "route_name_regional", "loc_name": "route_name_local", "name": "shared_string_name", "nat_name": "route_name_national", "int_name": "route_name_international", "short_name": "route_name_short", "official_name": "route_name_official", "old_name": "route_name_old", "alt_name": "route_name_alt"]
     
-    init(tags: [NSDictionary], detailsType: DetailsType) {
+    init(tags: [NSDictionary]) {
         self.tags = tags
-        self.detailsType = detailsType
         super.init(nibName: "OABaseNavbarViewController", bundle: nil)
         initTableData()
     }
@@ -45,37 +39,22 @@ final class NameTagsDetailsViewController: OABaseNavbarViewController {
     override func generateData() {
         tableData.clearAllData()
         for (key, header) in keysToSections {
-            if key == "name:" {
-                processNameTagsSection(header: header)
-            } else {
-                processTagsSectionForKey(key: key, header: header)
+            let filteredTags = tags.filter { tag in
+                guard let tagKey = tag["key"] as? String else { return false }
+                return tagKey.hasPrefix(key)
             }
-        }
-    }
-    
-    private func processNameTagsSection(header: String) {
-        let nameTags = tags.filter { ($0["key"] as? String)?.hasPrefix("name:") ?? false }
-        if !nameTags.isEmpty {
-            createSectionWithTags(filteredTags: nameTags, header: header, key: "country_loc_name")
-        }
-    }
-    
-    private func processTagsSectionForKey(key: String, header: String) {
-        let filteredTags = tags.filter { $0["key"] as? String == key }
-        if !filteredTags.isEmpty {
-            createSectionWithTags(filteredTags: filteredTags, header: header, key: key)
-        }
-    }
-    
-    private func createSectionWithTags(filteredTags: [NSDictionary], header: String, key: String) {
-        let section = tableData.createNewSection()
-        section.headerText = localizedString(header)
-        for tagDict in filteredTags {
-            let row = section.createNewRow()
-            row.cellType = OASimpleTableViewCell.getIdentifier()
-            row.key = key
-            row.title = tagDict["value"] as? String ?? ""
-            row.descr = tagDict["localizedTitle"] as? String ?? ""
+            
+            if !filteredTags.isEmpty {
+                let section = tableData.createNewSection()
+                section.headerText = localizedString(header)
+                for tagDict in filteredTags {
+                    let row = section.createNewRow()
+                    row.cellType = OASimpleTableViewCell.getIdentifier()
+                    row.key = key
+                    row.title = tagDict["value"] as? String ?? ""
+                    row.descr = tagDict["localizedTitle"] as? String ?? ""
+                }
+            }
         }
     }
     

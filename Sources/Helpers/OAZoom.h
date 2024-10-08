5b7fd1796489bf14354631c32ebad86b6157d22e
Max Kojin <nnngrach@gmail.com>
2024-10-08 16:47:41 +0500

    Revert "add minimal zoom formula"
    This reverts commit a0587c1b89724c9c37a00e41bec4b5a37498604f.

--- Sources/Helpers/OAZoom.h
+++ Sources/Helpers/OAZoom.h
@@ -25,7 +25,6 @@
 
 - (void) calculateAnimatedZoom:(int)currentBaseZoom deltaZoom:(float)deltaZoom;
 
-- (void) checkZoomBounds;
 + (OAZoom *) checkZoomBoundsWithZoom:(float)zoom minZoom:(int)minZoom maxZoom:(int)maxZoom;
 + (OAZoom *) checkZoomBoundsWithBaseZoom:(int)baseZoom floatZoomPart:(float)floatZoomPart minZoom:(int)minZoom maxZoom:(int)maxZoom;
 
--- Sources/Helpers/OAZoom.m
+++ Sources/Helpers/OAZoom.m
@@ -13,7 +13,7 @@
     int _baseZoom;
     float _zoomFloatPart;
     float _zoomAnimation;
-    float _minZoom;
+    int _minZoom;
     int _maxZoom;
 }
 
@@ -24,7 +24,7 @@
     {
         _baseZoom = baseZoom;
         _zoomFloatPart = zoomFloatPart;
-        _minZoom = log(ceil([OAUtilities calculateScreenHeight] / 256));
+        _minZoom = minZoom;
         _maxZoom = maxZoom;
     }
     return self;
@@ -64,18 +64,11 @@
 
 - (void) zoomIn
 {
-    if (_zoomFloatPart > 0)
-        _zoomFloatPart = 0;
     [self changeZoom:1];
-    [self checkZoomBounds];
 }
 - (void) zoomOut
 {
-    if (_zoomFloatPart > 0)
-        _zoomFloatPart = 0;
-    else
-        [self changeZoom:-1];
-    [self checkZoomBounds];
+    [self changeZoom:-1];
 }
 
 - (void) changeZoom:(int)step
@@ -142,8 +135,8 @@
     }
     else if (_baseZoom < _minZoom)
     {
-        _baseZoom = (int) _minZoom;
-        _zoomFloatPart = _minZoom - _baseZoom;
+        _baseZoom = _minZoom;
+        _zoomFloatPart = 0;
     }
 }
 
@@ -184,6 +177,7 @@
     int startIntZoom = ((int) startZoom);
     float startZoomFloatPart = startZoom - startIntZoom;
     double startDistanceIntZoom = startDistance * [self.class floatPartToVisual:startZoomFloatPart];
+    double log2 = log(startDistanceIntZoom / endDistance) / log(2);
     int intZoomDelta = ((int) log(2));
     double startDistanceIntZoomed = intZoomDelta >= 0
         ? startDistanceIntZoom / (1 << intZoomDelta)

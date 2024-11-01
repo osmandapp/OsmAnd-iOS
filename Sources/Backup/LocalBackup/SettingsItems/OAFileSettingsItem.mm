4a0ab74396f64a48f724e57e51b6a0f138159a51
Max Kojin <nnngrach@gmail.com>
2024-11-01 17:32:17 +0500

    review fix

--- Sources/Backup/LocalBackup/SettingsItems/OAFileSettingsItem.mm
+++ Sources/Backup/LocalBackup/SettingsItems/OAFileSettingsItem.mm
@@ -179,25 +179,25 @@
             }
             case EOASettingsItemFileSubtypeGpx:
             {
-                if ([name hasSuffix:@".gpx"])
+                if ([name hasSuffix:GPX_FILE_EXT])
                     return subtype;
                 break;
             }
             case EOASettingsItemFileSubtypeTravel:
             {
-                if ([name hasSuffix:@".sqlite"] && [name.lowercaseString containsString:@"travel"])
+                if ([name hasSuffix:BINARY_WIKIVOYAGE_MAP_INDEX_EXT] && [name.lowercaseString containsString:@"travel"])
                     return subtype;
                 break;
             }
             case EOASettingsItemFileSubtypeTilesMap:
             {
-                if ([name hasSuffix:@".sqlitedb"] || name.pathExtension.length == 0)
+                if ([name hasSuffix:SQLITE_EXT] || name.pathExtension.length == 0)
                     return subtype;
                 break;
             }
             case EOASettingsItemFileSubtypeRoutingConfig:
             {
-                if ([name hasSuffix:@".xml"] && ![name hasSuffix:RENDERER_INDEX_EXT])
+                if ([name hasSuffix:ROUTING_FILE_EXT] && ![name hasSuffix:RENDERER_INDEX_EXT])
                     return subtype;
                 break;
             }

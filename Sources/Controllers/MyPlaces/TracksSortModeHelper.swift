eb58dff6164a235288d2b9458e27edd78f445232
Dmitry Svetlichny <svetlichny1988@icloud.com>
2024-11-09 10:58:04 +0200

    Add TracksSortModeHelper

---
+++ Sources/Controllers/MyPlaces/TracksSortModeHelper.swift
@@ -0,0 +1,203 @@
+//
+//  TracksSortModeHelper.swift
+//  OsmAnd Maps
+//
+//  Created by Dmitry Svetlichny on 09.11.2024.
+//  Copyright © 2024 OsmAnd. All rights reserved.
+//
+
+import UIKit
+import OsmAndShared
+
+@objc enum TracksSortMode: Int {
+    case nearest
+    case lastModified
+    case nameAZ
+    case nameZA
+    case newestDateFirst
+    case oldestDateFirst
+    case longestDistanceFirst
+    case shortestDistanceFirst
+    case longestDurationFirst
+    case shorterDurationFirst
+    
+    static var allCases: [TracksSortMode] {
+        return [.nearest, .lastModified, .nameAZ, .nameZA, .newestDateFirst, .oldestDateFirst, .longestDistanceFirst, .shortestDistanceFirst, .longestDurationFirst, .shorterDurationFirst]
+    }
+    
+    var title: String {
+        switch self {
+        case .nearest: return localizedString("shared_string_nearest")
+        case .lastModified: return localizedString("sort_last_modified")
+        case .nameAZ: return localizedString("track_sort_az")
+        case .nameZA: return localizedString("track_sort_za")
+        case .newestDateFirst: return localizedString("newest_date_first")
+        case .oldestDateFirst: return localizedString("oldest_date_first")
+        case .longestDistanceFirst: return localizedString("longest_distance_first")
+        case .shortestDistanceFirst: return localizedString("shortest_distance_first")
+        case .longestDurationFirst: return localizedString("longest_duration_first")
+        case .shorterDurationFirst: return localizedString("shorter_duration_first")
+        }
+    }
+    
+    var image: UIImage? {
+        switch self {
+        case .nearest: return .icCustomNearby
+        case .lastModified: return .icCustomLastModified
+        case .nameAZ: return .icCustomSortNameAscending
+        case .nameZA: return .icCustomSortNameDescending
+        case .newestDateFirst: return .icCustomSortDateNewest
+        case .oldestDateFirst: return .icCustomSortDateOldest
+        case .longestDistanceFirst: return .icCustomSortLongToShort
+        case .shortestDistanceFirst: return .icCustomSortShortToLong
+        case .longestDurationFirst: return .icCustomSortDurationLongToShort
+        case .shorterDurationFirst: return .icCustomSortDurationShortToLong
+        }
+    }
+    
+    static func getByTitle(_ title: String) -> TracksSortMode {
+        return TracksSortMode.allCases.first(where: { $0.title == title }) ?? .lastModified
+    }
+}
+
+@objc class TracksSortModeHelper: NSObject {
+    @objc static var defaultSortModeTitle: String {
+        return TracksSortMode.lastModified.title
+    }
+    
+    @objc static func title(for mode: TracksSortMode) -> String {
+        return mode.title
+    }
+    
+    static func sortTracksWithMode(_ tracks: [GpxDataItem], mode: TracksSortMode) -> [GpxDataItem] {
+        switch mode {
+        case .nearest:
+            return tracks.sorted { TracksSortModeHelper.distanceToGPX(gpx: $0) < TracksSortModeHelper.distanceToGPX(gpx: $1) }
+        case .lastModified:
+            return tracks.sorted { $0.lastModifiedTime > $1.lastModifiedTime }
+        case .nameAZ:
+            return tracks.sorted { $0.gpxFileName.localizedCaseInsensitiveCompare($1.gpxFileName) == .orderedAscending }
+        case .nameZA:
+            return tracks.sorted { $0.gpxFileName.localizedCaseInsensitiveCompare($1.gpxFileName) == .orderedDescending }
+        case .newestDateFirst:
+            return tracks.sorted { $0.creationDate > $1.creationDate }
+        case .oldestDateFirst:
+            return tracks.sorted { $0.creationDate < $1.creationDate }
+        case .longestDistanceFirst:
+            return tracks.sorted { $0.totalDistance > $1.totalDistance }
+        case .shortestDistanceFirst:
+            return tracks.sorted { $0.totalDistance < $1.totalDistance }
+        case .longestDurationFirst:
+            return tracks.sorted { $0.timeSpan > $1.timeSpan }
+        case .shorterDurationFirst:
+            return tracks.sorted { $0.timeSpan < $1.timeSpan }
+        }
+    }
+    
+    static func getTrackDescription(track: GpxDataItem, sortMode: TracksSortMode, includeFolderInfo: Bool = false) -> NSAttributedString {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        let date = dateFormatter.string(from: track.lastModifiedTime)
+        let creationDate = dateFormatter.string(from: track.creationDate)
+        let distance = OAOsmAndFormatter.getFormattedDistance(track.totalDistance) ?? localizedString("shared_string_not_available")
+        let time = OAOsmAndFormatter.getFormattedTimeInterval(TimeInterval(track.timeSpan / 1000), shortFormat: true) ?? localizedString("shared_string_not_available")
+        let waypointCount = "\(track.wptPoints)"
+        let fullString = NSMutableAttributedString()
+        let defaultAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.preferredFont(forTextStyle: .footnote), .foregroundColor: UIColor.textColorSecondary]
+        let detailsText = "\(distance) • \(time) • \(waypointCount)"
+        let detailsString = NSAttributedString(string: detailsText, attributes: defaultAttributes)
+        switch sortMode {
+        case .nearest:
+            let distanceToTrack: String
+            let calculatedDistance = distanceToGPX(gpx: track)
+            if calculatedDistance != CGFloat.greatestFiniteMagnitude {
+                distanceToTrack = OAOsmAndFormatter.getFormattedDistance(Float(calculatedDistance))
+            } else {
+                distanceToTrack = localizedString("shared_string_not_available")
+            }
+            
+            var directionAngle: CGFloat = 0.0
+            if let analysis = track.getAnalysis(), let start = analysis.getLatLonStart() {
+                directionAngle = OADistanceAndDirectionsUpdater.getDirectionAngle(from: OsmAndApp.swiftInstance().locationServices?.lastKnownLocation, toDestinationLatitude: start.latitude, destinationLongitude: start.longitude)
+            }
+            
+            let cityName = track.nearestCity ?? localizedString("shared_string_not_available")
+            if let locationAttributedString = createImageAttributedString(named: "location.north.fill", tintColor: UIColor.iconColorActive, defaultAttributes: defaultAttributes, rotate: true, rotationAngle: directionAngle) {
+                fullString.append(locationAttributedString)
+                fullString.append(NSAttributedString(string: " "))
+            }
+            
+            let directionString = distanceToTrack + ", "
+            let directionAttributedString = NSAttributedString(string: directionString, attributes: [.font: UIFont.preferredFont(forTextStyle: .footnote), .foregroundColor: UIColor.iconColorActive])
+            let cityString = NSAttributedString(string: "\(cityName) | ", attributes: defaultAttributes)
+            fullString.append(directionAttributedString)
+            fullString.append(cityString)
+            fullString.append(detailsString)
+        case .lastModified:
+            let dateString = NSAttributedString(string: "\(date) | ", attributes: defaultAttributes)
+            fullString.append(dateString)
+            fullString.append(detailsString)
+        case .nameAZ, .nameZA:
+            if includeFolderInfo {
+                let folderName: String
+                if let capitalizedFolderName = OAUtilities.capitalizeFirstLetter(track.gpxFolderName), !capitalizedFolderName.isEmpty {
+                    folderName = capitalizedFolderName
+                } else {
+                    folderName = localizedString("shared_string_gpx_tracks")
+                }
+                
+                fullString.append(detailsString)
+                fullString.append(NSAttributedString(string: " | ", attributes: defaultAttributes))
+                if let folderAttributedString = createImageAttributedString(named: "folder", tintColor: UIColor.textColorSecondary, defaultAttributes: defaultAttributes, rotate: false) {
+                    fullString.append(folderAttributedString)
+                    fullString.append(NSAttributedString(string: " \(folderName)", attributes: defaultAttributes))
+                }
+            } else {
+                fullString.append(detailsString)
+            }
+        case .newestDateFirst, .oldestDateFirst:
+            let dateString = NSAttributedString(string: "\(creationDate) | ", attributes: defaultAttributes)
+            fullString.append(dateString)
+            fullString.append(detailsString)
+        case .longestDistanceFirst, .shortestDistanceFirst:
+            fullString.append(detailsString)
+        case .longestDurationFirst, .shorterDurationFirst:
+            let durationFirstDetailsString = NSAttributedString(string: "\(time) • \(distance) • \(waypointCount)", attributes: defaultAttributes)
+            fullString.append(durationFirstDetailsString)
+        }
+        
+        return fullString
+    }
+    
+    static func distanceToGPX(gpx: GpxDataItem) -> CGFloat {
+        guard let currentLocation = OsmAndApp.swiftInstance().locationServices?.lastKnownLocation else {
+            return CGFloat.greatestFiniteMagnitude
+        }
+        
+        if let analysis = gpx.getAnalysis(), let start = analysis.getLatLonStart(), CLLocationCoordinate2DIsValid(CLLocationCoordinate2DMake(start.latitude, start.longitude)) {
+            return OADistanceAndDirectionsUpdater.getDistanceFrom(currentLocation, toDestinationLatitude: start.latitude, destinationLongitude: start.longitude)
+        }
+        
+        return CGFloat.greatestFiniteMagnitude
+    }
+    
+    static func createImageAttributedString(named imageName: String, tintColor: UIColor, defaultAttributes: [NSAttributedString.Key: Any], rotate: Bool = false, rotationAngle: CGFloat = 0) -> NSAttributedString? {
+        guard let image = UIImage(systemName: imageName)?.withTintColor(tintColor, renderingMode: .alwaysTemplate) else { return nil }
+        let attachment = NSTextAttachment()
+        var finalImage = image
+        if rotate {
+            finalImage = image.rotateWithDiagonalSize(radians: rotationAngle) ?? image
+        }
+        
+        attachment.image = finalImage
+        if let font = defaultAttributes[.font] as? UIFont {
+            let fontHeight = font.capHeight
+            let scaleFactor: CGFloat = 1.2
+            let adjustedHeight = fontHeight * scaleFactor
+            let adjustedYPosition = (fontHeight - adjustedHeight) / 2
+            attachment.bounds = CGRect(x: 0, y: adjustedYPosition, width: adjustedHeight + 2, height: rotate ? adjustedHeight + 2 : adjustedHeight)
+        }
+        
+        return NSAttributedString(attachment: attachment)
+    }
+}

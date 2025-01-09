92f49791b13e76504160a0196d636f7097298236
Dmitry Svetlichny <svetlichny1988@icloud.com>
2025-01-09 10:42:24 +0200

    DetailedTrackGuidanceViewController add settings

--- Sources/Controllers/Settings/NavigationSettings/DetailedTrackGuidanceViewController.swift
+++ Sources/Controllers/Settings/NavigationSettings/DetailedTrackGuidanceViewController.swift
@@ -12,9 +12,25 @@ import UIKit
 @objcMembers
 final class DetailedTrackGuidanceViewController: OABaseSettingsViewController {
     private static let imgRowKey = "imgRowKey"
+    private static let askRowKey = "askRowKey"
+    private static let alwaysRowKey = "alwaysRowKey"
+    private static let thresholdSliderRowKey = "thresholdSliderRowKey"
+    
+    private var changedTrackGuidance: EOATrackApproximationType?
+    private var distanceThreshold: Int = 50
     
     override func registerCells() {
         addCell(OAImageHeaderCell.reuseIdentifier)
+        addCell(OASimpleTableViewCell.reuseIdentifier)
+        addCell(OATitleSliderRoundCell.reuseIdentifier)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        changedTrackGuidance = EOATrackApproximationType(rawValue: Int(OAAppSettings.sharedManager().detailedTrackGuidance.get(appMode)))
+        distanceThreshold = Int(OAAppSettings.sharedManager().gpxApproximationDistance.get(self.appMode))
+        generateData()
+        tableView.reloadData()
     }
     
     override func getTitle() -> String? {
@@ -46,6 +62,24 @@ final class DetailedTrackGuidanceViewController: OABaseSettingsViewController {
         imgRow.cellType = OAImageHeaderCell.reuseIdentifier
         imgRow.key = Self.imgRowKey
         imgRow.iconName = "img_detailed_track_guidance"
+        let detailedTrackSettingsSection = tableData.createNewSection()
+        let askRow = detailedTrackSettingsSection.createNewRow()
+        askRow.cellType = OASimpleTableViewCell.reuseIdentifier
+        askRow.key = Self.askRowKey
+        askRow.title = localizedString("ask_every_time")
+        askRow.iconName = "ic_checkmark_default"
+        askRow.setObj(changedTrackGuidance == .manual, forKey: "selected")
+        let alwaysRow = detailedTrackSettingsSection.createNewRow()
+        alwaysRow.cellType = OASimpleTableViewCell.reuseIdentifier
+        alwaysRow.key = Self.alwaysRowKey
+        alwaysRow.title = localizedString("shared_string_always")
+        alwaysRow.iconName = "ic_checkmark_default"
+        alwaysRow.setObj(changedTrackGuidance == .automatic, forKey: "selected")
+        let thresholdSection = tableData.createNewSection()
+        let thresholdSliderRow = thresholdSection.createNewRow()
+        thresholdSliderRow.cellType = OATitleSliderRoundCell.reuseIdentifier
+        thresholdSliderRow.key = Self.thresholdSliderRowKey
+        thresholdSliderRow.title = localizedString("threshold_distance")
     }
     
     override func getRow(_ indexPath: IndexPath?) -> UITableViewCell? {
@@ -57,8 +91,54 @@ final class DetailedTrackGuidanceViewController: OABaseSettingsViewController {
             cell.backgroundImageView.image = UIImage(named: item.iconName ?? "")
             cell.backgroundImageView.layer.cornerRadius = 4
             return cell
+        } else if item.cellType == OASimpleTableViewCell.reuseIdentifier {
+            let cell = tableView.dequeueReusableCell(withIdentifier: OASimpleTableViewCell.reuseIdentifier) as! OASimpleTableViewCell
+            cell.descriptionVisibility(false)
+            cell.titleLabel.text = item.title
+            cell.leftIconView.image = item.obj(forKey: "selected") as? Bool ?? false ? UIImage.templateImageNamed(item.iconName) : nil
+            cell.leftIconView.tintColor = .iconColorActive
+            return cell
+        } else if item.cellType == OATitleSliderRoundCell.reuseIdentifier {
+            let cell = tableView.dequeueReusableCell(withIdentifier: OATitleSliderRoundCell.reuseIdentifier) as! OATitleSliderRoundCell
+            cell.selectionStyle = .none
+            cell.titleLabel.text = item.title
+            cell.sliderView.minimumValue = 0
+            cell.sliderView.maximumValue = 100
+            cell.sliderView.value = Float(distanceThreshold)
+            cell.valueLabel.text = OAOsmAndFormatter.getFormattedDistance(Float(distanceThreshold))
+            cell.sliderView.tag = indexPath.section << 10 | indexPath.row
+            cell.sliderView.removeTarget(self, action: nil, for: .allEvents)
+            cell.sliderView.addTarget(self, action: #selector(sliderValueChanged(sender:)), for: .allEvents)
+            return cell
         }
         
         return nil
+    }
+    
+    override func onRowSelected(_ indexPath: IndexPath) {
+        guard let tableData else { return }
+        let item = tableData.item(for: indexPath)
+        changedTrackGuidance = item.key == Self.askRowKey ? .manual : .automatic
+        generateData()
+        tableView.reloadSections(IndexSet(integer: 1), with: .automatic)
+    }
+    
+    override func onRightNavbarButtonPressed() {
+        if let trackGuidance = changedTrackGuidance {
+            OAAppSettings.sharedManager().detailedTrackGuidance.set(Int32(trackGuidance.rawValue), mode: appMode)
+        }
+        
+        OAAppSettings.sharedManager().gpxApproximationDistance.set(Int32(distanceThreshold), mode: self.appMode)
+        delegate.onSettingsChanged()
+        super.onLeftNavbarButtonPressed()
+    }
+    
+    @objc private func sliderValueChanged(sender: UISlider) {
+        let indexPath = IndexPath(row: sender.tag & 0x3FF, section: sender.tag >> 10)
+        if let cell = tableView.cellForRow(at: indexPath) as? OATitleSliderRoundCell {
+            cell.sliderView = sender
+            distanceThreshold = Int(cell.sliderView.value)
+            cell.valueLabel.text = OAOsmAndFormatter.getFormattedDistance(Float(distanceThreshold))
+        }
     }
 }

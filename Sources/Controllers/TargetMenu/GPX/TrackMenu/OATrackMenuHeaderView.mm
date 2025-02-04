c616cad990da74e9643074322c891166578d5154
alex-osm <alex.morozov.osm@gmail.com>
2025-02-04 17:06:37 +0300

    Revert unneeded changes

--- Sources/Controllers/TargetMenu/GPX/TrackMenu/OATrackMenuHeaderView.mm
+++ Sources/Controllers/TargetMenu/GPX/TrackMenu/OATrackMenuHeaderView.mm
@@ -264,7 +264,7 @@
     {
         if (analysis.totalDistance != 0)
         {
-            float totalDistance = analysis.totalDistance;
+            float totalDistance = withoutGaps ? analysis.totalDistanceWithoutGaps : analysis.totalDistance;
             [statisticCells addObject:[OAGPXTableCellData withData:@{
                     kTableValues: @{
                             @"string_value": [OAOsmAndFormatter getFormattedDistance:totalDistance],

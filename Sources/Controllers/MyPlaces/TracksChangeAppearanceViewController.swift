84a7f9ef646028d21c8263a5352faf87d5617d4d
Dmitry Svetlichny <svetlichny1988@icloud.com>
2025-02-20 17:38:48 +0200

    Add Logics for Change appearance

--- Sources/Controllers/MyPlaces/TracksChangeAppearanceViewController.swift
+++ Sources/Controllers/MyPlaces/TracksChangeAppearanceViewController.swift
@@ -8,8 +8,7 @@
 
 import UIKit
 
-final class TracksChangeAppearanceViewController: OABaseNavbarViewController {
-    private static let buttonTitleKey = "buttonTitleKey"
+final class TracksChangeAppearanceViewController: OABaseNavbarViewController, AppearanceChangedDelegate {
     private static let directionArrowsRowKey = "directionArrowsRowKey"
     private static let startFinishIconsRowKey = "startFinishIconsRowKey"
     private static let coloringRowKey = "coloringRowKey"
@@ -19,10 +18,14 @@ final class TracksChangeAppearanceViewController: OABaseNavbarViewController {
     private static let splitIntervalRow = "splitIntervalRow"
     private static let splitIntervalDescrRow = "splitIntervalDescrRow"
     
-    private var tracks: [TrackItem]
+    private var tracks: Set<TrackItem>
+    private var initialData: AppearanceData
+    private var data: AppearanceData
     
-    init(tracks: [TrackItem]) {
+    init(tracks: Set<TrackItem>) {
         self.tracks = tracks
+        self.initialData = Self.buildAppearanceData()
+        self.data = AppearanceData(data: self.initialData)
         super.init(nibName: "OABaseNavbarViewController", bundle: nil)
         initTableData()
     }
@@ -34,6 +37,11 @@ final class TracksChangeAppearanceViewController: OABaseNavbarViewController {
     override func registerCells() {
         addCell(OASimpleTableViewCell.reuseIdentifier)
         addCell(OAButtonTableViewCell.reuseIdentifier)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.data.delegate = self
     }
     
     override func getTitle() -> String? {
@@ -60,19 +68,16 @@ final class TracksChangeAppearanceViewController: OABaseNavbarViewController {
         directionArrowsRow.cellType = OAButtonTableViewCell.reuseIdentifier
         directionArrowsRow.key = Self.directionArrowsRowKey
         directionArrowsRow.title = localizedString("gpx_direction_arrows")
-        directionArrowsRow.setObj(localizedString("shared_string_unchanged"), forKey: Self.buttonTitleKey)
         let startFinishIconsRow = directionSection.createNewRow()
         startFinishIconsRow.cellType = OAButtonTableViewCell.reuseIdentifier
         startFinishIconsRow.key = Self.startFinishIconsRowKey
         startFinishIconsRow.title = localizedString("track_show_start_finish_icons")
-        startFinishIconsRow.setObj(localizedString("shared_string_unchanged"), forKey: Self.buttonTitleKey)
         
         let coloringSection = tableData.createNewSection()
         let coloringRow = coloringSection.createNewRow()
         coloringRow.cellType = OAButtonTableViewCell.reuseIdentifier
         coloringRow.key = Self.coloringRowKey
         coloringRow.title = localizedString("shared_string_coloring")
-        coloringRow.setObj(localizedString("shared_string_unchanged"), forKey: Self.buttonTitleKey)
         let coloringDescrRow = coloringSection.createNewRow()
         coloringDescrRow.cellType = OASimpleTableViewCell.reuseIdentifier
         coloringDescrRow.key = Self.coloringDescRowKey
@@ -83,7 +88,6 @@ final class TracksChangeAppearanceViewController: OABaseNavbarViewController {
         widthRow.cellType = OAButtonTableViewCell.reuseIdentifier
         widthRow.key = Self.widthRowKey
         widthRow.title = localizedString("routing_attr_width_name")
-        widthRow.setObj(localizedString("shared_string_unchanged"), forKey: Self.buttonTitleKey)
         let widthDescrRow = widthSection.createNewRow()
         widthDescrRow.cellType = OASimpleTableViewCell.reuseIdentifier
         widthDescrRow.key = Self.widthDescrRow
@@ -94,7 +98,6 @@ final class TracksChangeAppearanceViewController: OABaseNavbarViewController {
         splitIntervalRow.cellType = OAButtonTableViewCell.reuseIdentifier
         splitIntervalRow.key = Self.splitIntervalRow
         splitIntervalRow.title = localizedString("gpx_split_interval")
-        splitIntervalRow.setObj(localizedString("shared_string_unchanged"), forKey: Self.buttonTitleKey)
         let splitIntervalDescrRow = splitIntervalSection.createNewRow()
         splitIntervalDescrRow.cellType = OASimpleTableViewCell.reuseIdentifier
         splitIntervalDescrRow.key = Self.splitIntervalDescrRow
@@ -110,6 +113,14 @@ final class TracksChangeAppearanceViewController: OABaseNavbarViewController {
             cell.leftIconVisibility(false)
             cell.descriptionVisibility(false)
             cell.titleLabel.text = item.title
+            let config = UIButton.Configuration.plain()
+            cell.button.configuration = config
+            if let key = item.key {
+                cell.button.menu = createStateSelectionMenu(for: key)
+            }
+            cell.button.showsMenuAsPrimaryAction = true
+            cell.button.changesSelectionAsPrimaryAction = true
+            cell.button.contentHorizontalAlignment = .right
             return cell
         } else if item.cellType == OASimpleTableViewCell.reuseIdentifier {
             let cell = tableView.dequeueReusableCell(withIdentifier: OASimpleTableViewCell.reuseIdentifier) as! OASimpleTableViewCell
@@ -121,5 +132,142 @@ final class TracksChangeAppearanceViewController: OABaseNavbarViewController {
         }
         
         return nil
+    }
+    
+    override func onRightNavbarButtonPressed() {
+        let task = ChangeTracksAppearanceTask(data: self.data, items: self.tracks) { [weak self] in
+            guard let self = self else { return }
+            self.dismiss(animated: true) {
+                OsmAndApp.swiftInstance().updateGpxTracksOnMapObservable.notifyEvent()
+            }
+        }
+        
+        task.execute()
+    }
+    
+    private static func buildAppearanceData() -> AppearanceData {
+        let data = AppearanceData()
+        for parameter in GpxParameter.companion.getAppearanceParameters() {
+            data.setParameter(parameter, value: nil)
+        }
+        
+        return data
+    }
+    
+    private func createStateSelectionMenu(for key: String) -> UIMenu {
+        if key == Self.directionArrowsRowKey {
+            return createArrowsMenu()
+        } else if key == Self.startFinishIconsRowKey {
+            return createStartFinishMenu()
+        } else if key == Self.coloringRowKey {
+            return createColoringMenu()
+        } else if key == Self.widthRowKey {
+            return createWidthMenu()
+        } else if key == Self.splitIntervalRow {
+            return createStartFinishMenu()
+        } else {
+            return UIMenu()
+        }
+    }
+    
+    private func createArrowsMenu() -> UIMenu {
+        let unchangedAction = UIAction(title: localizedString("shared_string_unchanged"), state: .on) { _ in }
+        let originalAction = UIAction(title: localizedString("simulate_location_movement_speed_original"), state: .off) { [weak self] _ in
+            guard let self else { return }
+            self.data.resetParameter(GpxParameter.showArrows)
+        }
+        let unchangedOriginalMenu = inlineMenu(withActions: [unchangedAction, originalAction])
+        
+        let onAction = UIAction(title: localizedString("shared_string_on"), state: .off) { [weak self] _ in
+            guard let self else { return }
+            self.data.setParameter(GpxParameter.showArrows, value: true)
+        }
+        let offAction = UIAction(title: localizedString("shared_string_off"), state: .off) { [weak self] _ in
+            guard let self else { return }
+            self.data.setParameter(GpxParameter.showArrows, value: false)
+        }
+        let onOffMenu = inlineMenu(withActions: [onAction, offAction])
+        
+        return UIMenu(title: "", options: .singleSelection, children: [unchangedOriginalMenu, onOffMenu])
+    }
+    
+    private func createStartFinishMenu() -> UIMenu {
+        let unchangedAction = UIAction(title: localizedString("shared_string_unchanged"), state: .on) { _ in }
+        let originalAction = UIAction(title: localizedString("simulate_location_movement_speed_original"), state: .off) { [weak self] _ in
+            guard let self else { return }
+            self.data.resetParameter(GpxParameter.showStartFinish)
+        }
+        let unchangedOriginalMenu = inlineMenu(withActions: [unchangedAction, originalAction])
+        
+        let onAction = UIAction(title: localizedString("shared_string_on"), state: .off) { [weak self] _ in
+            guard let self else { return }
+            self.data.setParameter(GpxParameter.showStartFinish, value: true)
+        }
+        let offAction = UIAction(title: localizedString("shared_string_off"), state: .off) { [weak self] _ in
+            guard let self else { return }
+            self.data.setParameter(GpxParameter.showStartFinish, value: false)
+        }
+        let onOffMenu = inlineMenu(withActions: [onAction, offAction])
+        
+        return UIMenu(title: "", options: .singleSelection, children: [unchangedOriginalMenu, onOffMenu])
+    }
+    
+    private func createColoringMenu() -> UIMenu {
+        let unchangedAction = UIAction(title: localizedString("shared_string_unchanged"), state: .on) { _ in }
+        let originalAction = UIAction(title: localizedString("simulate_location_movement_speed_original"), state: .off) { _ in }
+        let unchangedOriginalMenu = inlineMenu(withActions: [unchangedAction, originalAction])
+        
+        let solidColorAction = UIAction(title: localizedString("track_coloring_solid"), state: .off) { _ in }
+        let solidColorMenu = inlineMenu(withActions: [solidColorAction])
+        
+        let altitudeAction = UIAction(title: localizedString("altitude"), state: .off) { _ in }
+        let speedAction = UIAction(title: localizedString("shared_string_speed"), state: .off) { _ in }
+        let slopeAction = UIAction(title: localizedString("shared_string_slope"), state: .off) { _ in }
+        let gradientColorMenu = inlineMenu(withActions: [altitudeAction, speedAction, slopeAction])
+        
+        let roadTypeAction = UIAction(title: localizedString("routeInfo_roadClass_name"), image: UIImage(named: "ic_payment_label_pro"), state: .off) { _ in }
+        let surfaceAction = UIAction(title: localizedString("routeInfo_surface_name"), image: UIImage(named: "ic_payment_label_pro"), state: .off) { _ in }
+        let smoothhnessAction = UIAction(title: localizedString("routeInfo_smoothness_name"), image: UIImage(named: "ic_payment_label_pro"), state: .off) { _ in }
+        let winterRoadsAction = UIAction(title: localizedString("routeInfo_winter_ice_road_name"), image: UIImage(named: "ic_payment_label_pro"), state: .off) { _ in }
+        let thicknessRoadsAction = UIAction(title: localizedString("routeInfo_tracktype_name"), image: UIImage(named: "ic_payment_label_pro"), state: .off) { _ in }
+        let horseRoadsAction = UIAction(title: localizedString("routeInfo_horse_scale_name"), image: UIImage(named: "ic_payment_label_pro"), state: .off) { _ in }
+        let proColorMenu = inlineMenu(withActions: [roadTypeAction, surfaceAction, smoothhnessAction, winterRoadsAction, thicknessRoadsAction, horseRoadsAction])
+        
+        return UIMenu(title: "", options: .singleSelection, children: [unchangedOriginalMenu, solidColorMenu, gradientColorMenu, proColorMenu])
+    }
+    
+    private func createWidthMenu() -> UIMenu {
+        let unchangedAction = UIAction(title: localizedString("shared_string_unchanged"), state: .on) { _ in }
+        let originalAction = UIAction(title: localizedString("simulate_location_movement_speed_original"), state: .off) { _ in }
+        let unchangedOriginalMenu = inlineMenu(withActions: [unchangedAction, originalAction])
+        
+        let thinAction = UIAction(title: localizedString("rendering_value_thin_name"), state: .off) { _ in }
+        let mediumAction = UIAction(title: localizedString("rendering_value_medium_w_name"), state: .off) { _ in }
+        let boldAction = UIAction(title: localizedString("rendering_value_bold_name"), state: .off) { _ in }
+        let widthMenu = inlineMenu(withActions: [thinAction, mediumAction, boldAction])
+        
+        let customAction = UIAction(title: localizedString("shared_string_custom"), state: .off) { _ in }
+        let customWidthMenu = inlineMenu(withActions: [customAction])
+        
+        return UIMenu(title: "", options: .singleSelection, children: [unchangedOriginalMenu, widthMenu, customWidthMenu])
+    }
+    
+    private func createSplitIntervalMenu() -> UIMenu {
+        let unchangedAction = UIAction(title: localizedString("shared_string_unchanged"), state: .on) { _ in }
+        let originalAction = UIAction(title: localizedString("simulate_location_movement_speed_original"), state: .off) { _ in }
+        let unchangedOriginalMenu = inlineMenu(withActions: [unchangedAction, originalAction])
+        
+        let onAction = UIAction(title: localizedString("shared_string_on"), state: .off) { _ in }
+        let offAction = UIAction(title: localizedString("shared_string_off"), state: .off) { _ in }
+        let onOffMenu = inlineMenu(withActions: [onAction, offAction])
+        
+        return UIMenu(title: "", options: .singleSelection, children: [unchangedOriginalMenu, onOffMenu])
+    }
+    
+    private func inlineMenu(withActions actions: [UIAction]) -> UIMenu {
+        UIMenu(title: "", options: .displayInline, children: actions)
+    }
+    
+    func onAppearanceChanged() {
     }
 }

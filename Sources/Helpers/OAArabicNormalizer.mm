49bfe89b5b84e3622e8c978f1a116b7725b072da
Ivan Pyrohivskyi <ivan@Mac-mini-Ivan.local>
2025-04-10 18:47:47 +0300

    ArabicNormalizer expand

--- Sources/Helpers/OAArabicNormalizer.mm
+++ Sources/Helpers/OAArabicNormalizer.mm
@@ -2,19 +2,29 @@
 
 @implementation OAArabicNormalizer
 
-static NSRegularExpression *diacriticRegex;
 static NSString *const kArabicDigits = @"٠١٢٣٤٥٦٧٨٩";
 static NSString *const kDigitsReplacement = @"0123456789";
-static NSString *const kKashida = @"\u0640";
+static NSArray *DIACRITIC_REGEX;
+static NSDictionary *DIACRITIC_REPLACE;
 
 + (void)initialize {
-    NSError *error = nil;
-    diacriticRegex = [NSRegularExpression regularExpressionWithPattern:@"[\u064B-\u0652]"
-                                                               options:0
-                                                                 error:&error];
-    if (error) {
-        NSLog(@"Error initializing regex: %@", error.localizedDescription);
-    }
+    DIACRITIC_REGEX = @[
+                @"[\u064B-\u065F]",
+                @"[\u0610-\u061A]",
+                @"[\u06D6-\u06ED]",
+                @"\u0640",
+                @"\u0670"
+    ];
+    
+    DIACRITIC_REPLACE = @{
+                @"\u0624": @"\u0648", // Replace Waw Hamza Above by Waw
+                @"\u0629": @"\u0647", // Replace Ta Marbuta by Ha
+                @"\u064A": @"\u0649", // Replace Ya by Alif Maksura
+                @"\u0626": @"\u0649", // Replace Ya Hamza Above by Alif Maksura
+                @"\u0622": @"\u0627", // Replace Alifs with Hamza Above
+                @"\u0623": @"\u0627", // Replace Alifs with Hamza Below
+                @"\u0625": @"\u0627"  // Replace with Madda Above by Alif
+    };
 }
 
 + (BOOL)isSpecialArabic:(NSString *)text {
@@ -26,7 +36,7 @@ static NSString *const kKashida = @"\u0640";
     if ([self isArabicCharacter:firstChar]) {
         for (NSUInteger i = 0; i < text.length; i++) {
             unichar c = [text characterAtIndex:i];
-            if ([self isDiacritic:c] || [self isArabicDigit:c] || [self isKashida:c]) {
+            if ([self isDiacritic:c] || [self isArabicDigit:c] || [self isNeedReplace:c]) {
                 return YES;
             }
         }
@@ -41,18 +51,17 @@ static NSString *const kKashida = @"\u0640";
     }
     
     // Remove diacritics
-    NSMutableString *result = [NSMutableString stringWithString:text];
-    result = [[diacriticRegex stringByReplacingMatchesInString:result
-                                                       options:0
-                                                         range:NSMakeRange(0, result.length)
-                                                  withTemplate:@""] mutableCopy];
-    
-    // Remove Kashida
-    [result replaceOccurrencesOfString:kKashida
-                            withString:@""
-                               options:0
-                                 range:NSMakeRange(0, result.length)];
-    
+    NSString *result = text;
+    for (NSString *regex in DIACRITIC_REGEX) {
+        NSRegularExpression *regExp = [NSRegularExpression regularExpressionWithPattern:regex options:0 error:nil];
+        result = [regExp stringByReplacingMatchesInString:result options:0 range:NSMakeRange(0, result.length) withTemplate:@""];
+    }
+        
+    // Replace characters
+    for (NSString *key in DIACRITIC_REPLACE) {
+        result = [result stringByReplacingOccurrencesOfString:key withString:DIACRITIC_REPLACE[key]];
+    }
+        
     return [self replaceDigits:result];
 }
 
@@ -82,19 +91,23 @@ static NSString *const kKashida = @"\u0640";
 }
 
 + (BOOL)isDiacritic:(unichar)c {
-    return (c >= 0x064B && c <= 0x0652);
+    return (c >= 0x064B && c <= 0x065F) ||
+            (c >= 0x0610 && c <= 0x061A) ||
+            (c >= 0x06D6 && c <= 0x06ED) ||
+            c == 0x0640 || c == 0x0670;
 }
 
 + (BOOL)isArabicDigit:(unichar)c {
     return (c >= 0x0660 && c <= 0x0669);
 }
 
-+ (BOOL)isKashida:(unichar)c {
-    return (c == 0x0640);
-}
-
 + (BOOL)isArabicCharacter:(unichar)c {
     return (c >= 0x0600 && c <= 0x06FF);
+}
+
++ (BOOL)isNeedReplace:(unichar)c {
+    NSString *charAsString = [NSString stringWithCharacters:&c length:1];
+    return DIACRITIC_REPLACE[charAsString] != nil;
 }
 
 @end

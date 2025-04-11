c47f4cdbbfeda45fa9a82bed648f06ef4d005ccf
Vladyslav Lysenko <vladyslav.l1988@gmail.com>
2025-04-11 18:49:48 +0300

    switch cycle routes defaults to previous selected

--- Sources/QuickAction/Actions/ShowHideCycleRoutesAction.swift
+++ Sources/QuickAction/Actions/ShowHideCycleRoutesAction.swift
@@ -40,17 +40,18 @@ final class ShowHideCycleRoutesAction: BaseRouteQuickAction {
     }
     
     override func execute() {
-        let newValue = isEnabled() ? "false" : "true"
+        let isEnabled = isEnabled()
         let styleSettings = OAMapStyleSettings.sharedInstance()
         let routesParameter = styleSettings?.getParameter(SHOW_CYCLE_ROUTES_ATTR)
         let nodeParameter = styleSettings?.getParameter(CYCLE_NODE_NETWORK_ROUTES_ATTR)
+        let cycleRoutesParameter = OAAppSettings.sharedManager().cycleRoutesParameter.get()
         
         if let nodeParameter {
-            nodeParameter.value = newValue
+            nodeParameter.value = isEnabled ? "false" : cycleRoutesParameter
             styleSettings?.save(nodeParameter)
         }
         if let routesParameter {
-            routesParameter.value = newValue
+            routesParameter.value = isEnabled ? "false" : "true"
             styleSettings?.save(routesParameter)
         }
     }

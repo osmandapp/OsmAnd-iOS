d13eff5ee55dd447c73ccf9c16eb1c8d75ee457f
Max Kojin <nnngrach@gmail.com>
2025-04-24 21:58:13 +0500

    Fix plan route several segments calculating
    sync with android: public final CommonPreference<RouteService> ROUTE_SERVICE

--- Sources/OAApplicationMode.m
+++ Sources/OAApplicationMode.m
@@ -488,6 +488,8 @@ static int PROFILE_TRUCK = 1000;
 
 - (NSInteger) getRouterService
 {
+    if (self == _DEFAULT)
+        return STRAIGHT;
     return [OAAppSettings.sharedManager.routerService get:self];
 }
 

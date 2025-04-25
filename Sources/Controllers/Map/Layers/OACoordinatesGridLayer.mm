b86b1e48d8d01c29c7323ad7183a23719ca45c94
Dmitry Svetlichny <svetlichny1988@icloud.com>
2025-04-25 10:53:14 +0300

    Add OACoordinatesGridLayer

---
+++ Sources/Controllers/Map/Layers/OACoordinatesGridLayer.mm
@@ -0,0 +1,13 @@
+//
+//  OACoordinatesGridLayer.m
+//  OsmAnd Maps
+//
+//  Created by Dmitry Svetlichny on 25.04.2025.
+//  Copyright © 2025 OsmAnd. All rights reserved.
+//
+
+#import "OACoordinatesGridLayer.h"
+
+@implementation OACoordinatesGridLayer
+
+@end

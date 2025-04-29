46e5ba1bae7e9b818f12a323446392a833cb62c7
Dmitry Svetlichny <svetlichny1988@icloud.com>
2025-04-29 23:52:37 +0300

    implementation OACoordinatesGridLayer

--- Sources/Controllers/Map/Layers/OACoordinatesGridLayer.mm
+++ Sources/Controllers/Map/Layers/OACoordinatesGridLayer.mm
@@ -7,7 +7,293 @@
 //
 
 #import "OACoordinatesGridLayer.h"
+#import "OAAppSettings.h"
+#import "OACoordinatesGridSettings.h"
+#import "OsmAnd_Maps-Swift.h"
+#import <OsmAndCore/Map/GridMarksProvider.h>
+#import "OANativeUtilities.h"
+#import "OAColors.h"
+#import <OsmAndCore/TextRasterizer.h>
+#import "OAMapRendererView.h"
+
+#include <OsmAndCore/Map/MapRendererState.h>
+
+static const CGFloat kDefaultMarginFactor = 8.0f;
+static const OsmAnd::TextRasterizer::Style::TextAlignment kNoTextAlignment = static_cast<OsmAnd::TextRasterizer::Style::TextAlignment>(-1);
 
 @implementation OACoordinatesGridLayer
+{
+    OsmAndAppInstance _app;
+    OAAppSettings *_settings;
+    OACoordinatesGridSettings *_gridSettings;
+    OAMapRendererView *_mapView;
+    
+    std::shared_ptr<OsmAnd::GridMarksProvider> _marksProvider;
+    std::shared_ptr<OsmAnd::GridConfiguration> _gridConfiguration;
+    
+    GridFormat _cachedGridFormat;
+    ZoomRange _cachedZoomLimits;
+    GridLabelsPosition _cachedLabelsPosition;
+    CGFloat _cachedTextScale;
+    int _cachedGridColorDay;
+    int _cachedGridColorNight;
+    BOOL _cachedGridEnabled;
+    BOOL _cachedNightMode;
+    
+    OAAutoObserverProxy *_coordinatesGridSettingsObservable;
+    OAAutoObserverProxy *_mapSettingsChangeObserver;
+    OAAutoObserverProxy *_dayNightModeObserver;
+}
+
+- (NSString *) layerId
+{
+    return kCoordinatesGridLayerId;
+}
+
+- (void)initLayer
+{
+    [super initLayer];
+    
+    _app = [OsmAndApp instance];
+    _settings = [OAAppSettings sharedManager];
+    _gridSettings = [[OACoordinatesGridSettings alloc] init];
+    _mapView = OARootViewController.instance.mapPanel.mapViewController.mapView;
+    
+    _coordinatesGridSettingsObservable = [[OAAutoObserverProxy alloc] initWith:self withHandler:@selector(onPreferenceChange) andObserve:[OsmAndApp instance].coordinatesGridSettingsObservable];
+    _mapSettingsChangeObserver = [[OAAutoObserverProxy alloc] initWith:self withHandler:@selector(onPreferenceChange) andObserve:_app.mapSettingsChangeObservable];
+    _dayNightModeObserver = [[OAAutoObserverProxy alloc] initWith:self withHandler:@selector(onPreferenceChange) andObserve:OsmAndApp.instance.dayNightModeObservable];
+}
+
+- (BOOL)updateLayer
+{
+    if (![super updateLayer])
+        return NO;
+    
+    [self updateGridSettings];
+    return YES;
+}
+
+- (void)resetLayer
+{
+    [super resetLayer];
+    
+    [_mapView removeKeyedSymbolsProvider:_marksProvider];
+    _marksProvider.reset();
+}
+
+- (void)deinitLayer
+{
+    [super deinitLayer];
+    
+    if (_coordinatesGridSettingsObservable)
+    {
+        [_coordinatesGridSettingsObservable detach];
+        _coordinatesGridSettingsObservable = nil;
+    }
+    if (_mapSettingsChangeObserver)
+    {
+        [_mapSettingsChangeObserver detach];
+        _mapSettingsChangeObserver = nil;
+    }
+    if (_dayNightModeObserver)
+    {
+        [_dayNightModeObserver detach];
+        _dayNightModeObserver = nil;
+    }
+}
+
+- (void)onPreferenceChange
+{
+    [self updateGridSettings];
+}
+
+- (void)updateGridSettings
+{
+    OAApplicationMode *appMode = [_settings.applicationMode get];
+    BOOL updateAppearance = NO;
+    BOOL zoomLevelsUpdated = NO;
+    BOOL marginFactorUpdated = NO;
+    BOOL show = [_gridSettings isEnabled];
+    if (show)
+    {
+        if (_gridConfiguration == nullptr || !_marksProvider)
+        {
+            _gridConfiguration = std::make_shared<OsmAnd::GridConfiguration>();
+            [self initVariablesWithAppMode:appMode];
+            updateAppearance = YES;
+        }
+        else
+        {
+            updateAppearance = [self updateVariablesWithAppMode:appMode];
+            zoomLevelsUpdated = [self updateZoomLevelsWithAppMode:appMode];
+        }
+        if (updateAppearance)
+        {
+            [self cleanupMarksProvider];
+            [self updateGridAppearance];
+        }
+    }
+    
+    BOOL updated = updateAppearance || zoomLevelsUpdated || marginFactorUpdated;
+    if (_gridConfiguration && (_cachedGridEnabled != show || updated))
+    {
+        _cachedGridEnabled = show;
+        [self updateGridVisibility:_cachedGridEnabled];
+    }
+}
+
+- (void)initVariablesWithAppMode:(OAApplicationMode *)appMode
+{
+    _cachedGridFormat = (GridFormat)[_gridSettings getGridFormatForAppMode:appMode];
+    _cachedLabelsPosition = (GridLabelsPosition)[_gridSettings getGridLabelsPositionForAppMode:appMode];
+    _cachedGridColorDay = [_gridSettings getDayGridColor];
+    _cachedGridColorNight = [_gridSettings getNightGridColor];
+    _cachedTextScale = [_gridSettings getTextScaleForAppMode:appMode];
+    _cachedGridEnabled = [_gridSettings isEnabled];
+    _cachedZoomLimits = [_gridSettings getZoomLevelsWithRestrictionsForAppMode:appMode];
+    _cachedNightMode = OADayNightHelper.instance.isNightMode;
+}
+
+- (BOOL)updateVariablesWithAppMode:(OAApplicationMode *)appMode
+{
+    BOOL updated = NO;
+    GridFormat newGridFormat =(GridFormat)[_gridSettings getGridFormatForAppMode:appMode];
+    if (_cachedGridFormat != newGridFormat)
+    {
+        _cachedGridFormat = newGridFormat;
+        updated = YES;
+    }
+    
+    int newGridColorDay = [_gridSettings getDayGridColor];
+    if (_cachedGridColorDay != newGridColorDay)
+    {
+        _cachedGridColorDay = newGridColorDay;
+        updated = YES;
+    }
+    
+    int newGridColorNight = [_gridSettings getNightGridColor];
+    if (_cachedGridColorNight != newGridColorNight)
+    {
+        _cachedGridColorNight = newGridColorNight;
+        updated = YES;
+    }
+    
+    CGFloat newTextScale = [_gridSettings getTextScaleForAppMode:appMode];
+    if (fabs(_cachedTextScale - newTextScale) >= 0.0001f)
+    {
+        _cachedTextScale = newTextScale;
+        updated = YES;
+    }
+    
+    GridLabelsPosition newLabelsPosition = (GridLabelsPosition)[_gridSettings getGridLabelsPositionForAppMode:appMode];
+    if (_cachedLabelsPosition != newLabelsPosition)
+    {
+        _cachedLabelsPosition = newLabelsPosition;
+        updated = YES;
+    }
+    
+    BOOL newNightMode = OADayNightHelper.instance.isNightMode;
+    if (_cachedNightMode != newNightMode)
+    {
+        _cachedNightMode = newNightMode;
+        updated = YES;
+    }
+    
+    return updated;
+}
+
+- (void)updateGridAppearance
+{
+    OAFormat oaFmt = [GridFormatWrapper getFormatFor:_cachedGridFormat];
+    OAProjection oaProj = [GridFormatWrapper projectionFor:_cachedGridFormat];
+    auto format = static_cast<OsmAnd::GridConfiguration::Format>(oaFmt);
+    auto projection = static_cast<OsmAnd::GridConfiguration::Projection>(oaProj);
+    OsmAnd::ZoomLevel minZoom = static_cast<OsmAnd::ZoomLevel>(_cachedZoomLimits.min);
+    OsmAnd::ZoomLevel maxZoom = static_cast<OsmAnd::ZoomLevel>(_cachedZoomLimits.max);
+    int colorInt = _cachedNightMode ? _cachedGridColorNight : _cachedGridColorDay;
+    OsmAnd::FColorARGB color = [colorFromARGB(colorInt) toFColorARGB];
+    UIColor *haloUIColor = [OAUtilities isColorBright:colorFromARGB(colorInt)] ? [UIColor colorWithWhite:0 alpha:0.5] : [UIColor whiteColor];
+    OsmAnd::FColorARGB haloColor = [haloUIColor toFColorARGB];
+    
+    _gridConfiguration->setPrimaryProjection(projection);
+    _gridConfiguration->setPrimaryFormat(format);
+    _gridConfiguration->setPrimaryColor(color);
+    _gridConfiguration->setPrimaryMinZoomLevel(minZoom);
+    _gridConfiguration->setPrimaryMaxZoomLevel(maxZoom);
+    _gridConfiguration->setSecondaryProjection(projection);
+    _gridConfiguration->setSecondaryFormat(format);
+    _gridConfiguration->setSecondaryColor(color);
+    _gridConfiguration->setSecondaryMinZoomLevel(minZoom);
+    _gridConfiguration->setSecondaryMaxZoomLevel(maxZoom);
+    
+    _marksProvider = std::make_shared<OsmAnd::GridMarksProvider>();
+    auto primaryStyle = [self createMarksStyleWithColor:color haloColor:haloColor textAlignment:OsmAnd::TextRasterizer::Style::TextAlignment::Under];
+    auto secondaryStyle = [self createMarksStyleWithColor:color haloColor:haloColor textAlignment:kNoTextAlignment];
+    _marksProvider->setPrimaryStyle(primaryStyle,   2.0f * _cachedTextScale, true);
+    NSString *equator = OALocalizedString(@"equator");
+    NSString *primeMeridian = OALocalizedString(@"prime_meridian");
+    NSString *meridian180 = OALocalizedString(@"meridian_180");
+    _marksProvider->setPrimary(false, equator.UTF8String,  "", primeMeridian.UTF8String, meridian180.UTF8String);
+    _marksProvider->setSecondaryStyle(secondaryStyle, 2.0f * _cachedTextScale, _cachedLabelsPosition == GridLabelsPositionCenter);
+    if ([GridFormatWrapper needSuffixesForFormat:_cachedGridFormat])
+        _marksProvider->setSecondary(true, "N", "S", "E", "W");
+    else
+        _marksProvider->setSecondary(true, "", "", "", "");
+}
+
+- (BOOL)updateZoomLevelsWithAppMode:(OAApplicationMode *)appMode
+{
+    ZoomRange newZoomLimits = [_gridSettings getZoomLevelsWithRestrictionsForAppMode:appMode];
+    if (_cachedZoomLimits.min != newZoomLimits.min || _cachedZoomLimits.max != newZoomLimits.max)
+    {
+        _cachedZoomLimits = newZoomLimits;
+        if (_gridConfiguration)
+        {
+            OsmAnd::ZoomLevel minZoom = static_cast<OsmAnd::ZoomLevel>(_cachedZoomLimits.min);
+            OsmAnd::ZoomLevel maxZoom = static_cast<OsmAnd::ZoomLevel>(_cachedZoomLimits.max);
+            _gridConfiguration->setPrimaryMinZoomLevel(minZoom);
+            _gridConfiguration->setPrimaryMaxZoomLevel(maxZoom);
+            _gridConfiguration->setSecondaryMinZoomLevel(minZoom);
+            _gridConfiguration->setSecondaryMaxZoomLevel(maxZoom);
+            return YES;
+        }
+    }
+    
+    return NO;
+}
+
+- (OsmAnd::TextRasterizer::Style)createMarksStyleWithColor:(const OsmAnd::FColorARGB &)color haloColor:(const OsmAnd::FColorARGB &)haloColor textAlignment:(OsmAnd::TextRasterizer::Style::TextAlignment)textAlignment
+{
+    OsmAnd::TextRasterizer::Style style;
+    style.setColor(OsmAnd::ColorARGB(color));
+    style.setHaloColor(OsmAnd::ColorARGB(haloColor));
+    style.setHaloRadius((int)(3.0f * _cachedTextScale));
+    style.setSize(16.0f * _cachedTextScale);
+    style.setBold(true);
+    if (textAlignment != kNoTextAlignment)
+        style.setTextAlignment(textAlignment);
+    
+    return style;
+}
+
+- (void)updateGridVisibility:(BOOL)visible
+{
+    _gridConfiguration->setPrimaryGrid(visible);
+    _gridConfiguration->setSecondaryGrid(visible);
+    _mapView.renderer->setGridConfiguration(*_gridConfiguration);
+    if (visible)
+        _mapView.renderer->addSymbolsProvider(_marksProvider);
+    else
+        _mapView.renderer->removeSymbolsProvider(_marksProvider);
+}
+
+- (void)cleanupMarksProvider
+{
+    if (_marksProvider)
+    {
+        [self.mapView removeKeyedSymbolsProvider:_marksProvider];
+        _marksProvider = nullptr;
+    }
+}
 
 @end

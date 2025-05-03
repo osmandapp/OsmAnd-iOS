5ea913f6d84164ae02d07a24d833f88dccf4c357
Aleksandr Panchenko <alex.panchenko.osmand@gmail.com>
2025-05-03 10:31:10 +0300

    fix search _gpxFilesRec crash (#4571)

--- Sources/Controllers/Map/OAMapViewController.mm
+++ Sources/Controllers/Map/OAMapViewController.mm
@@ -2996,9 +2996,11 @@ static const NSInteger kReplaceLocalNamesMaxZoom = 6;
     NSMutableArray *paths = [NSMutableArray array];
     NSMutableArray *gpxDocs = [NSMutableArray array];
 
-    for (NSString *key in _selectedGpxHelper.activeGpx.allKeys) {
+    for (NSString *key in _selectedGpxHelper.activeGpx.allKeys)
+    {
         id value = _selectedGpxHelper.activeGpx[key];
-        if (value == nil) {
+        if (value == nil)
+        {
             continue;
         }
         
@@ -3007,13 +3009,13 @@ static const NSInteger kReplaceLocalNamesMaxZoom = 6;
         [gpxDocs addObject:value];
     }
     
-    if (_gpxFilesRec) {
-        [gpxDocs addObject:_gpxFilesRec];
+    if (_gpxFilesRec && _gpxFilesRec.count > 0)
+    {
+        [gpxDocs addObjectsFromArray:_gpxFilesRec];
     }
     
-    [wptApi setWptData:gpxDocs paths:paths];
+    [wptApi setWptData:[gpxDocs copy] paths:paths];
 }
-
 
 - (BOOL)hasWptAt:(CLLocationCoordinate2D)location
 {

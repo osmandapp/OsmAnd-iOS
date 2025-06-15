3fc6cc1a2cbe19467537a3995041365bdaceb540
Dmitry Svetlichny <svetlichny1988@icloud.com>
2025-06-15 21:52:57 +0300

    Impl OBDTextWidget

--- Sources/Plugins/VehicleMetricsPlugin/OBDTextWidget.swift
+++ Sources/Plugins/VehicleMetricsPlugin/OBDTextWidget.swift
@@ -10,14 +10,48 @@ import Foundation
 
 @objcMembers
 final class OBDTextWidget: OASimpleWidget {
+    private static let measuredIntervalPrefKey = "average_obd_measured_interval_millis"
+    private static let averageModePrefKey = "average_obd_mode"
+    private static let defaultIntervalMillis: Int = 30 * 60 * 1000
+    
     private var plugin: VehicleMetricsPlugin?
+    private var widgetComputer: OBDDataComputer.OBDComputerWidget?
+    private var cacheTextData: String?
+    private var cacheSubTextData: String?
+    private var measuredIntervalPref: OACommonLong?
+    private var averageModePref: OACommonBoolean?
+    private var fieldType: OBDDataComputer.OBDTypeWidget?
     
     convenience init(customId: String?, widgetType: WidgetType, appMode: OAApplicationMode, widgetParams: ([String: Any])? = nil) {
         self.init(frame: .zero)
+        self.fieldType = computeFieldType(for: widgetType)
         self.widgetType = widgetType
-        plugin = OAPluginsHelper.getPlugin(VehicleMetricsPlugin.self) as? VehicleMetricsPlugin
+        self.plugin = OAPluginsHelper.getPlugin(VehicleMetricsPlugin.self) as? VehicleMetricsPlugin
         configurePrefs(withId: customId, appMode: appMode, widgetParams: widgetParams)
-        updateInfo()
+        var averageTimeSeconds = 0
+        if supportsAverageMode() {
+            measuredIntervalPref = registerMeasuredIntervalPref(customId)
+            averageModePref = registerAverageModePref(customId)
+            if averageModePref?.get() == true {
+                averageTimeSeconds = (measuredIntervalPref?.get() ?? 0) / 1000
+            }
+        } else if fieldType == .fuelConsumptionRatePercentHour || fieldType == .fuelConsumptionRateLiterHour || fieldType == .fuelConsumptionRateLiterKm {
+            averageTimeSeconds = Int(fieldType?.defaultAverageTime ?? 0)
+        }
+        
+        if let fieldType {
+            widgetComputer = OBDDataComputer.shared.registerWidget(type: fieldType, averageTimeSeconds: Int32(averageTimeSeconds))
+        }
+        
+        _ = updateInfo()
+        setIconFor(widgetType)
+        onClickFunction = { [weak self] _ in
+            guard let self, self.supportsAverageMode(), let avgPref = self.averageModePref else { return }
+            let newValue = !avgPref.get()
+            avgPref.set(newValue)
+            self.updatePrefs(prefsChanged: true)
+            _ = self.updateInfo()
+        }
     }
     
     override init(frame: CGRect) {
@@ -26,5 +60,173 @@ final class OBDTextWidget: OASimpleWidget {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func updateInfo() -> Bool {
+        guard let wt = widgetType, wt.isPurchased(), let plug = plugin, let ft = fieldType, let comp = widgetComputer else { return false }
+        let subtext = plug.getWidgetUnit(ft)
+        let textData = plug.getWidgetValue(computerWidget: comp)
+        if textData != cacheTextData || subtext != cacheSubTextData {
+            setText(textData, subtext: subtext)
+            cacheTextData = textData
+            cacheSubTextData = subtext
+        }
+        
+        return false
+    }
+    
+    override func configureContextWidgetMenu() -> UIMenu? {
+        guard let baseMenu = super.configureContextWidgetMenu() else { return nil }
+        let newChildren: [UIMenuElement] = baseMenu.children.map { element in
+            guard let submenu = element as? UIMenu, let first = submenu.children.first as? UIAction, first.title == localizedString("shared_string_settings"), supportsAverageMode(), averageModePref?.get() == true else { return element }
+            let resetAction = UIAction(title: localizedString("reset_average_value"), image: .icCustomReset) { [weak self] _ in
+                guard let self else { return }
+                self.resetAverageValue()
+            }
+            
+            let reorderedChildren = [resetAction] + submenu.children
+            return UIMenu(title: submenu.title, image: submenu.image, identifier: submenu.identifier, options: submenu.options, children: reorderedChildren)
+        }
+        
+        return UIMenu(title: baseMenu.title, image: baseMenu.image, identifier: baseMenu.identifier, options: baseMenu.options, children: newChildren)
+    }
+    
+    override func isMetricSystemDepended() -> Bool {
+        true
+    }
+    
+    private func computeFieldType(for widgetType: WidgetType) -> OBDDataComputer.OBDTypeWidget {
+        switch widgetType {
+        case .OBDSpeed:
+            return .speed
+        case .OBDRpm:
+            return .rpm
+        case .OBDEngineRuntime:
+            return .engineRuntime
+        case .OBDFuelPressure:
+            return .fuelPressure
+        case .OBDAirIntakeTemp:
+            return .temperatureIntake
+        case .engineOilTemperature:
+            return .engineOilTemperature
+        case .OBDAmbientAirTemp:
+            return .temperatureAmbient
+        case .OBDBatteryVoltage:
+            return .batteryVoltage
+        case .OBDEngineCoolantTemp:
+            return .temperatureCoolant
+        case .OBDRemainingFuel:
+            return .fuelLeftLiter
+        case .OBDCalculatedEngineLoad:
+            return .calculatedEngineLoad
+        case .OBDThrottlePosition:
+            return .throttlePosition
+        case .OBDFuelConsumption:
+            return .fuelConsumptionRateLiterHour
+        default:
+            return .speed
+        }
+    }
+    
+    private func updateWidgetName() {
+        guard let widgetName = widgetType?.title, !widgetName.isEmpty else { return }
+        let finalName: String
+        if supportsAverageMode(), averageModePref?.get() == true {
+            let ms: Int
+            if let rawMs = measuredIntervalPref?.get() {
+                ms = Int(rawMs)
+            } else {
+                ms = Self.defaultIntervalMillis
+            }
+            let formattedInterval = OBDTextWidget.formatIntervals(interval: ms)
+            let format = localizedString("ltr_or_rtl_combine_via_colon")
+            finalName = String(format: format, widgetName, formattedInterval).uppercased()
+        } else {
+            finalName = widgetName.uppercased()
+        }
+        
+        setContentTitle(finalName)
+    }
+    
+    private func registerAverageModePref(_ customId: String?) -> OACommonBoolean {
+        let prefId: String
+        if let id = customId, !id.isEmpty {
+            prefId = Self.averageModePrefKey + id
+        } else {
+            prefId = Self.averageModePrefKey
+        }
+        
+        return OAAppSettings.sharedManager().registerBooleanPreference(prefId, defValue: false).makeProfile()
+    }
+    
+    private func registerMeasuredIntervalPref(_ customId: String?) -> OACommonLong {
+        let prefId: String
+        if let id = customId, !id.isEmpty {
+            prefId = Self.measuredIntervalPrefKey + id
+        } else {
+            prefId = Self.measuredIntervalPrefKey
+        }
+        
+        return OAAppSettings.sharedManager().registerLongPreference(prefId, defValue: Self.defaultIntervalMillis).makeProfile()
+    }
+    
+    private func resetAverageValue() {
+        widgetComputer?.resetLocations()
+        setText("-", subtext: nil)
+    }
+    
+    func updatePrefs(prefsChanged: Bool) {
+        guard supportsAverageMode() else { return }
+        let newTimeSeconds: Int = {
+            if averageModePref?.get() == true {
+                return Int((measuredIntervalPref?.get() ?? 0) / 1000)
+            } else {
+                return 0
+            }
+        }()
+        
+        if prefsChanged {
+            if let wc = widgetComputer {
+                widgetComputer = OBDDataComputer.shared.registerWidget(type: wc.type, averageTimeSeconds: Int32(newTimeSeconds))
+            }
+        } else {
+            widgetComputer?.averageTimeSeconds = Int32(newTimeSeconds)
+        }
+        
+        updateWidgetName()
+    }
+    
+    func supportsAverageMode() -> Bool {
+        switch widgetType {
+        case .OBDSpeed, .OBDCalculatedEngineLoad, .OBDFuelPressure, .OBDThrottlePosition, .OBDBatteryVoltage, .OBDAirIntakeTemp, .engineOilTemperature, .OBDAmbientAirTemp, .OBDEngineCoolantTemp:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    func isTemperatureWidget() -> Bool {
+        switch widgetType {
+        case .OBDAirIntakeTemp, .engineOilTemperature, .OBDAmbientAirTemp, .OBDEngineCoolantTemp:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    func getWidgetOBDCommand() -> OBDCommand? {
+        fieldType?.requiredCommand
+    }
+}
+
+extension OBDTextWidget {
+    static func formatIntervals(interval: Int) -> String {
+        let isSeconds = interval < 60 * 1000
+        let count = isSeconds ? interval / 1000 : interval / (60 * 1000)
+        let timeInterval = "\(count)"
+        let timeUnitKey = isSeconds ? "shared_string_sec" : "shared_string_minute_lowercase"
+        let timeUnit = localizedString(timeUnitKey)
+        let format = localizedString("ltr_or_rtl_combine_via_space")
+        return String(format: format, timeInterval, timeUnit)
     }
 }

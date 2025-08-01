b84b72b35c502b204a81ed2fba585eedb7042c39
Vladyslav Lysenko <vladyslav.l1988@gmail.com>
2025-08-01 12:42:31 +0300

    [WIP] header image and description

--- Sources/Controllers/ConfigureScreen/DistanceByTapViewController.swift
+++ Sources/Controllers/ConfigureScreen/DistanceByTapViewController.swift
@@ -9,9 +9,45 @@
 import Foundation
 
 final class DistanceByTapViewController: OABaseNavbarViewController {
+    private static let imgRowKey = "imgRowKey"
+    
     weak var delegate: WidgetStateDelegate?
     
     override func getTitle() -> String {
         localizedString("map_widget_distance_by_tap")
+    }
+    
+    override func hideFirstHeader() -> Bool {
+        true
+    }
+    
+    override func registerCells() {
+        addCell(ImageHeaderCell.reuseIdentifier)
+    }
+    
+    override func generateData() {
+        tableData.clearAllData()
+        
+        let distanceByTapImgSection = tableData.createNewSection()
+        distanceByTapImgSection.footerText = localizedString("distance_by_tap_use_description")
+        
+        let imgRow = distanceByTapImgSection.createNewRow()
+        imgRow.cellType = ImageHeaderCell.reuseIdentifier
+        imgRow.key = Self.imgRowKey
+        imgRow.iconName = "img_distance_by_tap"
+    }
+    
+    override func getRow(_ indexPath: IndexPath?) -> UITableViewCell? {
+        guard let indexPath else { return nil }
+        let item = tableData.item(for: indexPath)
+        if item.cellType == ImageHeaderCell.reuseIdentifier {
+            let cell = tableView.dequeueReusableCell(withIdentifier: ImageHeaderCell.reuseIdentifier) as! ImageHeaderCell
+            cell.selectionStyle = .none
+            cell.backgroundImageView.image = UIImage(named: item.iconName ?? "")
+            cell.backgroundImageView.layer.cornerRadius = 6
+            cell.configure(verticalSpace: 16, horizontalSpace: 16)
+            return cell
+        }
+        return nil
     }
 }

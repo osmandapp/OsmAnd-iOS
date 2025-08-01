8c288aa140e7c9da3a806e1f3a2f6e1049244979
Vladyslav Lysenko <vladyslav.l1988@gmail.com>
2025-08-01 14:51:52 +0300

    [WIP] distance by tap switch

--- Sources/Controllers/ConfigureScreen/DistanceByTapViewController.swift
+++ Sources/Controllers/ConfigureScreen/DistanceByTapViewController.swift
@@ -10,8 +10,17 @@ import Foundation
 
 final class DistanceByTapViewController: OABaseNavbarViewController {
     private static let imgRowKey = "imgRowKey"
+    private static let selectedKey = "isSelected"
+    private static let distanceByTapKey = "distanceByTapKey"
     
     weak var delegate: WidgetStateDelegate?
+    // swiftlint:disable force_unwrapping
+    private lazy var settings = OAAppSettings.sharedManager()!
+    // swiftlint:enable force_unwrapping
+    
+    override func commonInit() {
+        settings = OAAppSettings.sharedManager()
+    }
     
     override func getTitle() -> String {
         localizedString("map_widget_distance_by_tap")
@@ -23,6 +32,7 @@ final class DistanceByTapViewController: OABaseNavbarViewController {
     
     override func registerCells() {
         addCell(ImageHeaderCell.reuseIdentifier)
+        addCell(OASwitchTableViewCell.reuseIdentifier)
     }
     
     override func generateData() {
@@ -35,6 +45,17 @@ final class DistanceByTapViewController: OABaseNavbarViewController {
         imgRow.cellType = ImageHeaderCell.reuseIdentifier
         imgRow.key = Self.imgRowKey
         imgRow.iconName = "img_distance_by_tap"
+        
+        let switchCellSection = tableData.createNewSection()
+        let showDistanceRuler = settings.showDistanceRuler.get()
+        
+        let distanceByTapRow = switchCellSection.createNewRow()
+        distanceByTapRow.cellType = OASwitchTableViewCell.reuseIdentifier
+        distanceByTapRow.key = Self.distanceByTapKey
+        distanceByTapRow.title = localizedString("map_widget_distance_by_tap")
+        distanceByTapRow.accessibilityLabel = distanceByTapRow.title
+        distanceByTapRow.accessibilityValue = localizedString(showDistanceRuler ? "shared_string_on" : "shared_string_off")
+        distanceByTapRow.setObj(showDistanceRuler, forKey: Self.selectedKey)
     }
     
     override func getRow(_ indexPath: IndexPath?) -> UITableViewCell? {
@@ -47,7 +68,35 @@ final class DistanceByTapViewController: OABaseNavbarViewController {
             cell.backgroundImageView.layer.cornerRadius = 6
             cell.configure(verticalSpace: 16, horizontalSpace: 16)
             return cell
+        } else if item.cellType == OASwitchTableViewCell.reuseIdentifier {
+            let cell = tableView.dequeueReusableCell(withIdentifier: OASwitchTableViewCell.reuseIdentifier) as! OASwitchTableViewCell
+            cell.descriptionVisibility(false)
+            cell.leftIconVisibility(false)
+            let selected = item.bool(forKey: Self.selectedKey)
+            cell.leftIconView.tintColor = selected ? UIColor(rgb: item.iconTint) : .iconColorDefault
+            cell.titleLabel.text = item.title
+            cell.accessibilityLabel = item.accessibilityLabel
+            cell.accessibilityValue = item.accessibilityValue
+            cell.switchView.removeTarget(nil, action: nil, for: .allEvents)
+            cell.switchView.isOn = selected
+            cell.switchView.tag = indexPath.section << 10 | indexPath.row
+            cell.switchView.addTarget(self, action: #selector(onSwitchClick(_:)), for: .valueChanged)
+            return cell
         }
         return nil
+    }
+    
+    @objc private func onSwitchClick(_ sender: Any) -> Bool {
+        guard let tableData, let sw = sender as? UISwitch else { return false }
+        
+        let indexPath = IndexPath(row: sw.tag & 0x3FF, section: sw.tag >> 10)
+        let data = tableData.item(for: indexPath)
+        
+        if data.key == Self.distanceByTapKey {
+            settings.showDistanceRuler.set(sw.isOn)
+            reloadDataWith(animated: true, completion: nil)
+            delegate?.onWidgetStateChanged()
+        }
+        return false
     }
 }

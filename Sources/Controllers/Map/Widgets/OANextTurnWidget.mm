1d7980559893e66652ce17b8d89ee335b76e4e57
Vladyslav-lys <66596645+Vladyslav-lys@users.noreply.github.com>
2025-08-07 10:15:46 +0300

    fixed no comma in distance (#4794)

--- Sources/Controllers/Map/Widgets/OANextTurnWidget.mm
+++ Sources/Controllers/Map/Widgets/OANextTurnWidget.mm
@@ -606,8 +606,8 @@
             NSString *text = [_distanceLabel.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
             if (self.widgetSizeStyle == EOAWidgetSizeStyleSmall && (!_exitView.hidden || _streetLabel.text.length != 0 || !_shieldStackView.hidden))
                 _distanceLabel.text = [text stringByAppendingString:@","];
-            else
-                _distanceLabel.text = [text stringByReplacingOccurrencesOfString:@"," withString:@""];
+            else if ([text hasSuffix:@","])
+                _distanceLabel.text = [text substringToIndex:text.length - 1];
         }
     }
     else

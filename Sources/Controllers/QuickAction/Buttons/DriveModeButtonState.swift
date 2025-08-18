bfdc2593dea3e609c5bd31540e9bebc449f76dfe
Dmitry Svetlichny <svetlichny1988@icloud.com>
2025-08-18 15:29:54 +0300

    Add DriveModeButtonState

---
+++ Sources/Controllers/QuickAction/Buttons/DriveModeButtonState.swift
@@ -0,0 +1,26 @@
+//
+//  DriveModeButtonState.swift
+//  OsmAnd Maps
+//
+//  Created by Dmitry Svetlichny on 18.08.2025.
+//  Copyright © 2025 OsmAnd. All rights reserved.
+//
+
+import Foundation
+
+@objcMembers
+final class DriveModeButtonState: MapButtonState {
+    private static let hudId = "map.view.route_planning"
+    
+    init() {
+        super.init(withId: Self.hudId)
+    }
+    
+    override func getName() -> String {
+        localizedString("shared_string_navigation")
+    }
+    
+    @discardableResult override func setupButtonPosition(_ position: ButtonPositionSize) -> ButtonPositionSize {
+        setupButtonPosition(position, posH: ButtonPositionSize.Companion().POS_LEFT, posV: ButtonPositionSize.Companion().POS_BOTTOM, xMove: true, yMove: false)
+    }
+}

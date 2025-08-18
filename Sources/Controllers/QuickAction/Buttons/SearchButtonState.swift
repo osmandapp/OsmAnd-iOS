de8ff179fcabc3962cdaa9505516d1bb7dc94645
Dmitry Svetlichny <svetlichny1988@icloud.com>
2025-08-18 09:45:45 +0300

    Add SearchButtonState

---
+++ Sources/Controllers/QuickAction/Buttons/SearchButtonState.swift
@@ -0,0 +1,26 @@
+//
+//  SearchButtonState.swift
+//  OsmAnd Maps
+//
+//  Created by Dmitry Svetlichny on 15.08.2025.
+//  Copyright © 2025 OsmAnd. All rights reserved.
+//
+
+import Foundation
+
+@objcMembers
+final class SearchButtonState: MapButtonState {
+    private static let hudId = "map.view.quick_search"
+    
+    init() {
+        super.init(withId: Self.hudId)
+    }
+    
+    override func getName() -> String {
+        localizedString("shared_string_search")
+    }
+    
+    @discardableResult override func setupButtonPosition(_ position: ButtonPositionSize) -> ButtonPositionSize {
+        setupButtonPosition(position, posH: ButtonPositionSize.Companion().POS_LEFT, posV: ButtonPositionSize.Companion().POS_TOP, xMove: true, yMove: false)
+    }
+}

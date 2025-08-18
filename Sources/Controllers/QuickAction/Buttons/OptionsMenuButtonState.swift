17e15ac6bc8da60ea150dce3c4cec9d08ddd45af
Dmitry Svetlichny <svetlichny1988@icloud.com>
2025-08-18 15:12:17 +0300

    Add OptionsMenuButtonState

---
+++ Sources/Controllers/QuickAction/Buttons/OptionsMenuButtonState.swift
@@ -0,0 +1,26 @@
+//
+//  OptionsMenuButtonState.swift
+//  OsmAnd Maps
+//
+//  Created by Dmitry Svetlichny on 18.08.2025.
+//  Copyright © 2025 OsmAnd. All rights reserved.
+//
+
+import Foundation
+
+@objcMembers
+final class OptionsMenuButtonState: MapButtonState {
+    private static let hudId = "map.view.menu"
+    
+    init() {
+        super.init(withId: Self.hudId)
+    }
+    
+    override func getName() -> String {
+        localizedString("shared_string_menu")
+    }
+    
+    @discardableResult override func setupButtonPosition(_ position: ButtonPositionSize) -> ButtonPositionSize {
+        setupButtonPosition(position, posH: ButtonPositionSize.Companion().POS_LEFT, posV: ButtonPositionSize.Companion().POS_BOTTOM, xMove: true, yMove: false)
+    }
+}

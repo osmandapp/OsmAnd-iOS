d72ed9b803b3fc2425193a6b2828e262f522980d
Dmitry Svetlichny <svetlichny1988@icloud.com>
2025-09-30 12:29:31 +0300

    Add enableWidgetsV2

--- Sources/Controllers/Map/Widgets/Helpers/WidgetsAvailabilityHelper.swift
+++ Sources/Controllers/Map/Widgets/Helpers/WidgetsAvailabilityHelper.swift
@@ -36,6 +36,7 @@ class WidgetsAvailabilityHelper: NSObject {
     
     static func initRegVisibility() {
         let installDate = UserDefaults.standard.double(forKey: "kAppInstalledDate")
+        let enableWidgetsV2 = installDate >= Self.routeWidgetsV2IntroTimeInSeconds
         let exceptDefault: [OAApplicationMode] = [.car(), .bicycle(), .pedestrian(), .public_TRANSPORT(), .boat(), .aircraft(), .ski(), .truck(), .motorcycle(), .horse(), .moped(), .train()]
         
         // left
@@ -51,7 +52,14 @@ class WidgetsAvailabilityHelper: NSObject {
         regWidgetAvailability(widgetType: .secondNextTurn, appModes: exceptDefault)
         
         // right
-        if installDate >= Self.routeWidgetsV2IntroTimeInSeconds {
+        if !enableWidgetsV2 {
+            regWidgetVisibility(widgetType: .intermediateDestination)
+            regWidgetVisibility(widgetType: .distanceToDestination)
+            regWidgetVisibility(widgetType: .timeToIntermediate)
+            regWidgetVisibility(widgetType: .timeToDestination)
+        }
+
+        if enableWidgetsV2 {
             regWidgetVisibility(widgetType: .routeInfo, appModes: exceptDefault)
         }
         

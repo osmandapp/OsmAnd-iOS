4fa1b20f06382b492d87835877008d9504e6036a
Max Kojin <nnngrach@gmail.com>
2025-11-04 18:10:55 +0500

    sync GpxAppearanceInfo with android

--- Sources/GPX/GpxAppearanceInfo.swift
+++ Sources/GPX/GpxAppearanceInfo.swift
@@ -62,50 +62,62 @@ final class GpxAppearanceInfo: NSObject {
     ])
     
     var width: String?
-    // public GradientScaleType scaleType; // закомментировано как и в исходнике
-    var color: Int = 0
     var coloringType: String?
-    var gradientSpeedColor: Int = 0
-    var gradientSlopeColor: Int = 0
+    var gradientPaletteName: String?
+    var color: Int = 0
     var splitType: EOAGpxSplitType = .none
-    var splitInterval: CGFloat = 0.0
+    var splitInterval: Double = 0.0
     var showArrows: Bool = false
     var showStartFinish: Bool = false
-    var isJoinSegments: Bool = false
-    var verticalExaggerationScale: Double = 0.0
-    var elevationMeters: Int = 0
-    var visualization3dByType: EOAGPX3DLineVisualizationByType = .none
-    var visualization3dWallColorType: EOAGPX3DLineVisualizationWallColorType = .none
-    var visualization3dPositionType: EOAGPX3DLineVisualizationPositionType = .top
-
     var timeSpan: Int = 0
     var wptPoints: Int = 0
     var totalDistance: Float = 0.0
+    
+    var smoothingThreshold: Double = 0.0
+    var minFilterSpeed: Double = 0.0
+    var maxFilterSpeed: Double = 0.0
+    var minFilterAltitude: Double = 0.0
+    var maxFilterAltitude: Double = 0.0
+    var maxFilterHdop: Double = 0.0
+    
+    var trackVisualizationType: EOAGPX3DLineVisualizationByType = .none
+    var trackWallColorType: EOAGPX3DLineVisualizationWallColorType = .none
+    var trackLinePositionType: EOAGPX3DLineVisualizationPositionType = .top
+    
+    var verticalExaggeration: Double = 0.0
+    var elevationMeters: Int = 0
+    
+    var isJoinSegments: Bool = false // Don't exit in android
 
     convenience init(dataItem: GpxDataItem) {
         self.init()
         self.color = dataItem.color
-        self.coloringType = dataItem.coloringType
         self.width = dataItem.width
         self.showArrows = dataItem.showArrows
         self.showStartFinish = dataItem.showStartFinish
-        self.isJoinSegments = dataItem.joinSegments
-        self.verticalExaggerationScale = dataItem.verticalExaggerationScale
-        self.elevationMeters = Int(dataItem.elevationMeters)
-        self.visualization3dByType = dataItem.visualization3dByType
-        self.visualization3dWallColorType = dataItem.visualization3dWallColorType
-        self.visualization3dPositionType = dataItem.visualization3dPositionType
         self.splitType = dataItem.splitType
         self.splitInterval = dataItem.splitInterval
+        self.coloringType = dataItem.coloringType
+        self.gradientPaletteName = dataItem.gradientPaletteName
         
-        // self.scaleType = dataItem.scaleType
-        // self.gradientSpeedColor = dataItem.getGradientSpeedColor
-        // self.gradientSlopeColor = dataItem.getGradientSlopeColor
-        // self.gradientAltitudeColor = dataItem.getGradientAltitudeColor
-
+        self.trackVisualizationType = dataItem.visualization3dByType
+        self.trackWallColorType = dataItem.visualization3dWallColorType
+        self.trackLinePositionType = dataItem.visualization3dPositionType
+        self.verticalExaggeration = dataItem.verticalExaggerationScale
+        self.elevationMeters = Int(dataItem.elevationMeters)
+        
         self.timeSpan = dataItem.timeSpan
         self.wptPoints = Int(dataItem.wptPoints)
         self.totalDistance = dataItem.totalDistance
+        
+        self.smoothingThreshold = dataItem.getParameter(parameter: GpxParameter.smoothingThreshold) as? Double ?? 0
+        self.minFilterSpeed = dataItem.getParameter(parameter: GpxParameter.minFilterSpeed) as? Double ?? 0
+        self.maxFilterSpeed = dataItem.getParameter(parameter: GpxParameter.maxFilterSpeed) as? Double ?? 0
+        self.minFilterAltitude = dataItem.getParameter(parameter: GpxParameter.minFilterAltitude) as? Double ?? 0
+        self.maxFilterAltitude = dataItem.getParameter(parameter: GpxParameter.minFilterAltitude) as? Double ?? 0
+        self.maxFilterHdop = dataItem.getParameter(parameter: GpxParameter.maxFilterHdop) as? Double ?? 0
+        
+        self.isJoinSegments = dataItem.joinSegments
     }
     
     override init() {
@@ -116,38 +128,57 @@ final class GpxAppearanceInfo: NSObject {
         let gpxAppearanceInfo = GpxAppearanceInfo()
 
         gpxAppearanceInfo.color = Int(UIColor.toNumber(from: ""))
-        if let color = json["color"] {
+        if let color = json[TAG_COLOR] {
             if let number = color as? NSNumber {
                 gpxAppearanceInfo.color = number.intValue
             } else if let string = color as? String {
                 gpxAppearanceInfo.color = Int(UIColor.toNumber(from: string))
             }
         }
-
-        gpxAppearanceInfo.coloringType = json["coloring_type"] as? String
-        gpxAppearanceInfo.width = json["width"] as? String
-        gpxAppearanceInfo.showArrows = (json["show_arrows"] as? NSString)?.boolValue ?? false
-        gpxAppearanceInfo.showStartFinish = (json["show_start_finish"] as? NSString)?.boolValue ?? false
-        gpxAppearanceInfo.isJoinSegments = (json["is_join_segments"] as? NSString)?.boolValue ?? false
-        gpxAppearanceInfo.verticalExaggerationScale = (json["elevation_meters"] as? NSString)?.doubleValue ?? 0
-        gpxAppearanceInfo.elevationMeters = (json["elevation_meters"] as? NSString)?.integerValue ?? 0
         
-        gpxAppearanceInfo.visualization3dByType = OAGPXDatabase.lineVisualizationByType(forName: json["elevation_meters"] as? String ?? "")
-        gpxAppearanceInfo.visualization3dWallColorType = OAGPXDatabase.lineVisualizationWallColorType(forName: json["line_3d_visualization_wall_color_type"] as? String ?? "")
-        gpxAppearanceInfo.visualization3dPositionType = OAGPXDatabase.lineVisualizationPositionType(forName: json["line_3d_visualization_position_type"] as? String ?? "")
+        if let value = json[TAG_WIDTH] as? String {
+            gpxAppearanceInfo.width = value
+        }
+        gpxAppearanceInfo.showArrows = json[TAG_SHOW_ARROWS] as? Bool ?? false
+        gpxAppearanceInfo.showStartFinish = json[TAG_START_FINISH] as? Bool ?? false
+        gpxAppearanceInfo.splitType = OAGPXDatabase.splitType(byName: json[TAG_SPLIT_TYPE] as? String ?? "")
+        gpxAppearanceInfo.splitInterval = json[TAG_SPLIT_INTERVAL] as? Double ?? 0
         
-        gpxAppearanceInfo.splitType = OAGPXDatabase.splitType(byName: json["split_type"] as? String ?? "")
-        gpxAppearanceInfo.splitInterval = json["split_interval"] as? Double ?? 0
+        if let value = json[TAG_COLORING_TYPE] as? String {
+            gpxAppearanceInfo.coloringType = value
+        }
+        if gpxAppearanceInfo.coloringType == nil, let value = json[TAG_GRADIENT_SCALE_TYPE] as? String {
+            gpxAppearanceInfo.coloringType = value
+        }
         
-        // gpxAppearanceInfo.scaleType = [self getScaleType:json[@"gradient_scale_type"]];
-        // gpxAppearanceInfo.gradientSpeedColor = json.optInt(GradientScaleType.SPEED.getColorTypeName());
-        // gpxAppearanceInfo.gradientSlopeColor = json.optInt(GradientScaleType.SLOPE.getColorTypeName());
-        // gpxAppearanceInfo.gradientAltitudeColor = json.optInt(GradientScaleType.ALTITUDE.getColorTypeName());
+        if let value = json[TAG_COLOR_PALETTE] as? String {
+            gpxAppearanceInfo.gradientPaletteName = value
+        }
+        if let value = json[TAG_LINE_3D_VISUALIZATION_BY_TYPE] as? String {
+            gpxAppearanceInfo.trackVisualizationType = OAGPXDatabase.lineVisualizationByType(forName: value)
+        }
+        if let value = json[TAG_LINE_3D_VISUALIZATION_WALL_COLOR_TYPE] as? String {
+            gpxAppearanceInfo.trackWallColorType = OAGPXDatabase.lineVisualizationWallColorType(forName: value)
+        }
+        if let value = json[TAG_LINE_3D_VISUALIZATION_POSITION_TYPE] as? String {
+            gpxAppearanceInfo.trackLinePositionType = OAGPXDatabase.lineVisualizationPositionType(forName: value)
+        }
         
-        gpxAppearanceInfo.timeSpan = json["split_interval"] as? Int ?? 0
-        gpxAppearanceInfo.wptPoints = json["wpt_points"] as? Int ?? 0
-        gpxAppearanceInfo.totalDistance = json["total_distance"] as? Float ?? 0
-       
+        gpxAppearanceInfo.verticalExaggeration = json[TAG_VERTICAL_EXAGGERATION_SCALE] as? Double ?? 0
+        gpxAppearanceInfo.elevationMeters = json[TAG_ELEVATION_METERS] as? Int ?? 0
+        gpxAppearanceInfo.timeSpan = json[TAG_TIME_SPAN] as? Int ?? 0
+        gpxAppearanceInfo.wptPoints = json[TAG_WPT_POINTS] as? Int ?? 0
+        gpxAppearanceInfo.totalDistance = json[TAG_TOTAL_DISTANCE] as? Float ?? 0
+        gpxAppearanceInfo.smoothingThreshold = json[TAG_SMOOTHING_THRESHOLD] as? Double ?? 0
+        
+        gpxAppearanceInfo.minFilterSpeed = json[TAG_MIN_FILTER_SPEED] as? Double ?? 0
+        gpxAppearanceInfo.maxFilterSpeed = json[TAG_MAX_FILTER_SPEED] as? Double ?? 0
+        gpxAppearanceInfo.minFilterAltitude = json[TAG_MIN_FILTER_ALTITUDE] as? Double ?? 0
+        gpxAppearanceInfo.maxFilterAltitude = json[TAG_MAX_FILTER_ALTITUDE] as? Double ?? 0
+        gpxAppearanceInfo.maxFilterHdop = json[TAG_MAX_FILTER_HDOP] as? Double ?? 0
+        
+        gpxAppearanceInfo.isJoinSegments = json["is_join_segments"] as? Bool ?? false
+        
         return gpxAppearanceInfo
     }
     
@@ -155,39 +186,74 @@ final class GpxAppearanceInfo: NSObject {
         gpxAppearanceTags.contains(tag)
     }
     
-    func myToJson(json: inout [String: Any]) {
+    func toJson(_ json: inout [String: Any]) {
+        json[Self.TAG_COLOR] = UIColor(argb: color).toHexARGBString()
+        json[Self.TAG_WIDTH] = width
+        json[Self.TAG_SHOW_ARROWS] = showArrows ? "true" : "false"
+        json[Self.TAG_START_FINISH] = showStartFinish ? "true" : "false"
+        
+        json[Self.TAG_SPLIT_TYPE] = OAGPXDatabase.splitTypeName(byValue: splitType)
+        json[Self.TAG_SPLIT_INTERVAL] = String(format: "%f", splitInterval)
+        json[Self.TAG_COLORING_TYPE] = coloringType
+        if let gradientPaletteName {
+            json[Self.TAG_COLOR_PALETTE] = gradientPaletteName
+        }
+        
+        json[Self.TAG_LINE_3D_VISUALIZATION_BY_TYPE] = OAGPXDatabase.lineVisualizationByTypeName(for: trackVisualizationType)
+        json[Self.TAG_LINE_3D_VISUALIZATION_WALL_COLOR_TYPE] = OAGPXDatabase.lineVisualizationWallColorTypeName(for: trackWallColorType)
+        json[Self.TAG_LINE_3D_VISUALIZATION_POSITION_TYPE] = OAGPXDatabase.lineVisualizationPositionTypeName(for: trackLinePositionType)
+        
+        json[Self.TAG_VERTICAL_EXAGGERATION_SCALE] = String(format: "%f", verticalExaggeration)
+        json[Self.TAG_ELEVATION_METERS] = String(format: "%ld", elevationMeters)
+        
+        json[Self.TAG_TIME_SPAN] = String(format: "%ld", timeSpan)
+        json[Self.TAG_WPT_POINTS] = String(format: "%ld", wptPoints)
+        json[Self.TAG_TOTAL_DISTANCE] = String(format: "%f", totalDistance)
+        
+        json[Self.TAG_SMOOTHING_THRESHOLD] = String(format: "%f", smoothingThreshold)
+        json[Self.TAG_MIN_FILTER_SPEED] = String(format: "%f", minFilterSpeed)
+        json[Self.TAG_MAX_FILTER_SPEED] = String(format: "%f", maxFilterSpeed)
+        json[Self.TAG_MIN_FILTER_ALTITUDE] = String(format: "%f", minFilterAltitude)
+        json[Self.TAG_MAX_FILTER_ALTITUDE] = String(format: "%f", maxFilterSpeed)
+        json[Self.TAG_MAX_FILTER_HDOP] = String(format: "%f", maxFilterHdop)
+        
+        json["is_join_segments"] = isJoinSegments ? "true" : "false"
     }
     
+    // to run from obj-c
     func toJson(_ json: [String: Any]) -> [String: Any] {
         var newJson = json
         toJson(&newJson)
         return newJson
     }
     
-    func toJson(_ json: inout [String: Any]) {
-        json["color"] = UIColor(argb: color).toHexARGBString()
-        json["coloring_type"] = coloringType
-        json["width"] = width
-        json["show_arrows"] = showArrows ? "true" : "false"
-        json["show_start_finish"] = showStartFinish ? "true" : "false"
-        json["is_join_segments"] = isJoinSegments ? "true" : "false"
-        json["vertical_exaggeration_scale"] = String(format: "%f", verticalExaggerationScale)
-        json["elevation_meters"] = String(format: "%ld", elevationMeters)
+    private static func writeParam(_ json: inout [String: Any], name: String, value: Any?) {
+        guard let value = value else { return }
         
-        json["line_3d_visualization_by_type"] = OAGPXDatabase.lineVisualizationByTypeName(for: visualization3dByType)
-        json["line_3d_visualization_wall_color_type"] = OAGPXDatabase.lineVisualizationWallColorTypeName(for: visualization3dWallColorType)
-        json["line_3d_visualization_position_type"] = OAGPXDatabase.lineVisualizationPositionTypeName(for: visualization3dPositionType)
-        
-        json["split_type"] = OAGPXDatabase.splitTypeName(byValue: splitType)
-        json["split_interval"] = String(format: "%f", splitInterval)
-        
-//        json["gradient_scale_type"] = scaleType
-//        json[GradientScaleType.SPEED.getColorTypeName] = show_arrows
-//        json[GradientScaleType.SLOPE.getColorTypeName] = show_start_finish
-//        json[GradientScaleType.ALTITUDE.getColorTypeName] = color
-        
-        json["time_span"] = String(format: "%ld", timeSpan)
-        json["wpt_points"] = String(format: "%ld", wptPoints)
-        json["total_distance"] = String(format: "%f", totalDistance)
+        switch value {
+        case let v as Int:
+            if v != 0 { json[name] = v }
+        case let v as Int64:
+            if v != 0 { json[name] = v }
+        case let v as UInt:
+            if v != 0 { json[name] = v }
+        case let v as Double:
+            if v != 0.0, !v.isNaN { json[name] = v }
+        case let v as Float:
+            let d = Double(v)
+            if d != 0.0, !d.isNaN { json[name] = d }
+        case let v as Bool:
+            json[name] = v ? "true" : "false"
+        case let s as String:
+            if !s.isEmpty { json[name] = s }
+        default:
+            json[name] = value
+        }
+    }
+
+    private static func writeValidDouble(_ json: inout [String: Any], name: String, value: Double) {
+        if !value.isNaN {
+            json[name] = value
+        }
     }
 }

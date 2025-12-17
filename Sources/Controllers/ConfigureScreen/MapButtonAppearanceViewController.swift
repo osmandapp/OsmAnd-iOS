72271ec8be23bfd6fd20c6d64ca9d76a2c3b3503
Vladyslav Lysenko <vladyslav.l1988@gmail.com>
2025-12-17 16:22:28 +0200

    [WIP] appearance nav bar

--- Sources/Controllers/ConfigureScreen/MapButtonAppearanceViewController.swift
+++ Sources/Controllers/ConfigureScreen/MapButtonAppearanceViewController.swift
@@ -8,5 +8,60 @@
 
 @objc
 final class MapButtonAppearanceViewController: OABaseNavbarViewController {
-    // TODO
+    override func getTitle() -> String {
+        localizedString("shared_string_appearance")
+    }
+    
+    override func getLeftNavbarButtonTitle() -> String {
+        localizedString("shared_string_cancel")
+    }
+    
+    override func getRightNavbarButtons() -> [UIBarButtonItem] {
+        [createRightNavbarButton(nil, iconName: "ic_navbar_reset", action: #selector(onRightNavbarButtonPressed), menu: nil)]
+    }
+    
+    override func onLeftNavbarButtonPressed() {
+        // TODO
+        //showUnsavedChangesAlert()
+        super.onLeftNavbarButtonPressed()
+    }
+    
+    override func onRightNavbarButtonPressed() {
+        showResetToDefaultActionSheet()
+    }
+    
+    override func hideFirstHeader() -> Bool {
+        true
+    }
+    
+    private func showUnsavedChangesAlert() {
+        let alert: UIAlertController = UIAlertController(title: localizedString("unsaved_changes"),
+                                                         message: localizedString("unsaved_changes_will_be_lost_discard"),
+                                                         preferredStyle: .alert)
+        let discardAction = UIAlertAction(title: localizedString("shared_string_discard"), style: .default) { _ in
+            self.dismiss()
+        }
+
+        let cancelAction = UIAlertAction(title: localizedString("shared_string_cancel"), style: .default, handler: nil)
+
+        alert.addAction(cancelAction)
+        alert.addAction(discardAction)
+        alert.preferredAction = discardAction
+
+        present(alert, animated: true)
+    }
+    
+    private func showResetToDefaultActionSheet() {
+        let actionSheet = UIAlertController(title: localizedString("reset_to_default"),
+                                            message: localizedString("reset_all_settings_desc"),
+                                            preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: localizedString("shared_string_reset"), style: .destructive) { _ in
+            // TODO
+        })
+        actionSheet.addAction(UIAlertAction(title: localizedString("shared_string_cancel"), style: .cancel))
+        if let popoverController = actionSheet.popoverPresentationController {
+            popoverController.barButtonItem = navigationItem.rightBarButtonItem
+        }
+        present(actionSheet, animated: true)
+    }
 }

bbb84927366b8295db87ee5880facd3fd1b9fa0d
Vladyslav Lysenko <vladyslav.l1988@gmail.com>
2026-01-19 19:00:35 +0200

    absent waypoint colors fixed

--- Sources/Controllers/TargetMenu/GPX/TrackMenu/OATrackMenuHudViewController.mm
+++ Sources/Controllers/TargetMenu/GPX/TrackMenu/OATrackMenuHudViewController.mm
@@ -1229,6 +1229,7 @@
                 OASGpxUtilitiesPointsGroup *metaGroup = self.doc.pointsGroups[oldKey];
                 if (metaGroup)
                 {
+                    metaGroup.color = [_groupColor toRGBNumber];
                     [self.doc.pointsGroups removeObjectForKey:oldKey];
                     self.doc.pointsGroups[newKey] = metaGroup;
                 }

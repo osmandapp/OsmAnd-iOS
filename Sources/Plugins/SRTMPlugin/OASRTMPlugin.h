9b4e7ab4ada8aafb8091aad78e707b0b1e3be86f
Dmitry Svetlichny <svetlichny1988@icloud.com>
2026-03-20 12:26:51 +0200

    Add settings to OASRTMPlugin

--- Sources/Plugins/SRTMPlugin/OASRTMPlugin.h
+++ Sources/Plugins/SRTMPlugin/OASRTMPlugin.h
@@ -13,11 +13,20 @@ extern NSInteger const terrainMaxSupportedZoom;
 extern NSInteger const hillshadeDefaultTrasparency;
 extern NSInteger const defaultTrasparency;
 
-@class OACommonBoolean, TerrainMode;
+@class OACommonBoolean, OACommonDouble, OACommonInteger, OACommonString, TerrainMode;
 
 @interface OASRTMPlugin : OAPlugin
 
 @property (nonatomic) OACommonBoolean *enable3dMapsPref;
+@property (nonatomic) OACommonBoolean *enable3dMapObjectsPref;
+@property(nonatomic, strong) OACommonDouble *buildings3dAlphaPref;
+@property(nonatomic, strong) OACommonInteger *buildings3dViewDistancePref;
+@property(nonatomic, strong) OACommonInteger *buildings3dColorStylePref;
+@property(nonatomic, strong) OACommonInteger *buildings3dCustomNightColorPref;
+@property(nonatomic, strong) OACommonInteger *buildings3dCustomDayColorPref;
+@property(nonatomic, strong) OACommonBoolean *buildings3dDetailLevelPref;
+@property(nonatomic, strong) OACommonBoolean *buildings3dEnableColoringPref;
+@property(nonatomic, strong) OACommonString *buildings3dColorPref;
 @property (nonatomic) OACommonBoolean *terrainEnabledPref;
 @property (nonatomic) OACommonString *terrainModeTypePref;
 
@@ -25,6 +34,15 @@ extern NSInteger const defaultTrasparency;
 - (void)setTerrainMode:(TerrainMode *)mode;
 - (BOOL)isTerrainLayerEnabled;
 - (void)setTerrainLayerEnabled:(BOOL)enabled;
+- (BOOL)is3dMapObjectsEnabled;
+- (void)set3dMapObjectsEnabled:(BOOL)enabled;
+- (void)reset3DBuildingAlphaToDefault;
+- (void)apply3DBuildingsAlpha:(double)alpha;
+- (void)apply3DBuildingsDetalization;
+- (NSInteger)get3DBuildingsColorStyle;
+- (void)apply3DBuildingsColorStyle:(NSInteger)style;
+- (void)apply3DBuildingsColor:(int)color;
+- (int)getBuildings3dColor;
 - (NSInteger)getTerrainMinZoom;
 - (NSInteger)getTerrainMaxZoom;
 

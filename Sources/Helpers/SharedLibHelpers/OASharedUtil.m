e7cc907af9d564e47f0aca65765cac7dd91ecb7e
alex-dev <alex.dev@osmand.net>
2026-05-02 13:21:33 +0300

    Drop NetworkAPIImpl after shared refactoring

--- Sources/Helpers/SharedLibHelpers/OASharedUtil.m
+++ Sources/Helpers/SharedLibHelpers/OASharedUtil.m
@@ -18,7 +18,7 @@
 + (void)initSharedLib:(NSString *)documentsPath gpxPath:(NSString *)gpxPath
 {
     [OASPlatformUtil.shared initializeOsmAndContext:[[OAOsmAndContextImpl alloc] init]
-                                      xmlFactoryApi:[[OAXmlFactory alloc] init] networkAPI:[SharedLibNetworkAPI new]];
+                                      xmlFactoryApi:[[OAXmlFactory alloc] init]];
 }
 
 // Temporary test code

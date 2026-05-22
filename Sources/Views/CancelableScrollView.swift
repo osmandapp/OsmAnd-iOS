37e8e24b51c1e4e194c3327b633889022bf4a9c2
vitaliy-sova-ios <vitaliy.sova.ios@gmail.com>
2026-05-22 12:57:02 +0300

    Update CancelableScrollView.swift

--- Sources/Views/CancelableScrollView.swift
+++ Sources/Views/CancelableScrollView.swift
@@ -8,10 +8,8 @@
 
 import UIKit
 
-/// Custom UIScrollView subclass used to fine-tune touch handling behavior for interactive controls inside scrollable menus.
-/// It disables touch delays and adjusts content touch cancellation rules to ensure that scrolling gestures take priority
-/// over embedded UI elements such as buttons. This provides a more responsive and predictable interaction experience,
-/// especially in dense UI layouts like side panels and CarPlay-style interfaces where both scrolling and tapping coexist.
+/// A UIScrollView subclass that disables touch delays
+/// and prioritizes scroll gestures over embedded controls.
 final class CancelableScrollView: UIScrollView {
     
     override init(frame: CGRect) {

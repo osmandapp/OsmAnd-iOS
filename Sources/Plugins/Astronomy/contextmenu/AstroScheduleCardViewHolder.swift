6a0bb20f7a2426d8b7675360b3c312530bac6927
vitaliy-sova-ios <vitaliy.sova.ios@gmail.com>
2026-06-24 19:02:17 +0300

    removed redeclaration - extension Array

--- Sources/Plugins/Astronomy/contextmenu/AstroScheduleCardViewHolder.swift
+++ Sources/Plugins/Astronomy/contextmenu/AstroScheduleCardViewHolder.swift
@@ -386,9 +386,3 @@ enum AstroScheduleCardViewHolder {
         dayOffset > 0 ? "+\(dayOffset)" : nil
     }
 }
-
-private extension Array {
-    subscript(safe index: Int) -> Element? {
-        indices.contains(index) ? self[index] : nil
-    }
-}

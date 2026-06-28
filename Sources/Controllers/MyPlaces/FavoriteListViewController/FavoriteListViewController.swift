dab44639ffb8ac258aa89e6eddbb3d6b58893fe1
Vladyslav Lysenko <vladyslav.l1988@gmail.com>
2026-06-28 15:44:48 +0300

    updated sort header

--- Sources/Controllers/MyPlaces/FavoriteListViewController/FavoriteListViewController.swift
+++ Sources/Controllers/MyPlaces/FavoriteListViewController/FavoriteListViewController.swift
@@ -15,7 +15,7 @@ final class FavoriteListViewController: UIViewController {
     static let imageSize: CGFloat = 30.0
     static let favoriteIconSize: CGFloat = 36.0
     static let sortHeaderHeight: CGFloat = 44.0
-    static let sortHeaderLeadingInset: CGFloat = 16.0
+    static let sortHeaderLeadingInset: CGFloat = 24.0
     static let navigationTitleFontSize: CGFloat = 17.0
     static let navigationTitleMaximumSize: CGFloat = 22.0
     static let navigationSubtitleFontSize: CGFloat = 12.0

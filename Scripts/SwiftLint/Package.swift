fbd25ae51804dfd6993cdb3b4ae4f54ddd1fe557
Aleksandr Panchenko <alex.panchenko.osmand@gmail.com>
2026-07-09 15:13:46 +0300

    Update SwiftLint binary package to 0.65.0

--- Scripts/SwiftLint/Package.swift
+++ Scripts/SwiftLint/Package.swift
@@ -9,8 +9,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SwiftLintBinary",
-            url: "https://github.com/realm/SwiftLint/releases/download/0.63.0/SwiftLintBinary.artifactbundle.zip",
-            checksum: "b51ca39ffe2331fe0337f9267d4b0dea2c182791a2fd0f3b961d7cbfb6d488d7"
+            url: "https://github.com/realm/SwiftLint/releases/download/0.65.0/SwiftLintBinary.artifactbundle.zip",
+            checksum: "eb333bd76dfb5f46d21fdf3615fe39bb938956ca0b8e94c241c4b2db6e696b90"
         )
     ]
 )

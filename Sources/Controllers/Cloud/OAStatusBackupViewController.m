261b9abb859ca8f9c073bce93c92b8e4fa58a9c2
Vladyslav Lysenko <vladyslav.l1988@gmail.com>
2026-08-03 17:46:33 +0300

    Update OAStatusBackupViewController.m

--- Sources/Controllers/Cloud/OAStatusBackupViewController.m
+++ Sources/Controllers/Cloud/OAStatusBackupViewController.m
@@ -277,7 +277,6 @@
 - (void)onBackupFinished:(NSNotification *)notification
 {
     NSString *error = notification.userInfo[@"error"];
-    
     if (error != nil)
     {
         [OAUtilities showToast:nil details:[[OABackupError alloc] initWithError:error].getLocalizedError duration:.4 inView:self.view];

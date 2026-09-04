1cec5dd1db4eda74a18f21f15b51dcc744cd60ee
Aleksandr Panchenko <alex.panchenko.osmand@gmail.com>
2026-09-04 13:38:55 +0300

    fix self.unitLabel.textColor

--- Sources/Controllers/Map/Widgets/OATextInfoWidget.mm
+++ Sources/Controllers/Map/Widgets/OATextInfoWidget.mm
@@ -697,11 +697,6 @@ NSString * const kSizeStylePref = @"simple_widget_size";
         self.valueLabel.textAlignment = NSTextAlignmentNatural;
     }
     
-    if (![[self getWidgetPanel] isPanelVertical])
-    {
-        self.unitLabel.textColor = [UIColor colorNamed:ACColorNameWidgetUnitsColor];
-        [self updatesSeparatorsColor:[UIColor colorNamed:ACColorNameWidgetSeparatorColor].appMapThemeColor];
-    }
 }
 
 - (BOOL)isEnabledTextInfoComponents

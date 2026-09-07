aafc0996b008b9f414066bfd5fcb15eeb0879060
aleksandr-tata <aleksandr.tata.osmand@gmail.com>
2026-09-07 09:02:22 +0200

    Fix Plan Route side panel layout on iPad and iPhone landscape (#5698)
    * Fix Plan Route side panel layout on iPad and iPhone landscape
    
    * Fixed rotation issues
    
    * Restore Plan Route status bar style and clean up constants
    
    * Preserve viewport X scale in Plan Route bottom-sheet mode
    
    * Restore Plan Route viewport after dismissal animation
    
    * Use bottom sheet for compact Plan Route windows
    
    * Keep map HUD visible in Plan Route side panel
    
    * Exclude Plan Route toolbars from map viewport bounds

--- Sources/Controllers/Map/OAMapViewController.mm
+++ Sources/Controllers/Map/OAMapViewController.mm
@@ -824,6 +824,16 @@ static char kMapSourceUpdateQueueKey;
     [self setViewportScaleY:y];
 }
 
+- (double)viewportXScale
+{
+    return _mapView.viewportXScale;
+}
+
+- (void)setViewportXScale:(double)viewportXScale
+{
+    [self setViewportScaleX:viewportXScale];
+}
+
 - (double)viewportYScale
 {
     return _mapView.viewportYScale;

1cbfcfdb4ea9f8d4745fd95a2cf435a359310598
Aleksandr <aleksandr.tata.osmand@gmail.com>
2026-09-10 19:24:42 +0200

    Fix Plan Route tests target membership

--- Sources/Controllers/PlanRoute/PlanRouteModels.swift
+++ Sources/Controllers/PlanRoute/PlanRouteModels.swift
@@ -30,22 +30,6 @@ enum PlanRouteMode {
     }
 }
 
-struct PlanRoutePresentationContext {
-    static let standard = PlanRoutePresentationContext(followTrackMode: false,
-                                                        showSnapWarning: false,
-                                                        appliesApproximationToNavigation: false)
-
-    let followTrackMode: Bool
-    let showSnapWarning: Bool
-    let appliesApproximationToNavigation: Bool
-
-    static func followTrack(attachToRoads: Bool) -> PlanRoutePresentationContext {
-        PlanRoutePresentationContext(followTrackMode: true,
-                                     showSnapWarning: attachToRoads,
-                                     appliesApproximationToNavigation: attachToRoads)
-    }
-}
-
 enum PlanRouteTab: Int, CaseIterable {
     case poi
     case analyze

ca27a9a1b1fa4dbee00d6784d12d249179ead109
vitaliy-sova-ios <vitaliy.sova.ios@gmail.com>
2026-09-11 19:28:51 +0300

    CarPlay Map mode to Vehicle appearance by default

--- Sources/Helpers/MigrationManager.swift
+++ Sources/Helpers/MigrationManager.swift
@@ -23,7 +23,6 @@ final class MigrationManager: NSObject {
         case migrateRouteRecalculationValues
         case migrateLocationIconSizeAndCourseIconSize
         case migrateAstronomyPreferences
-        case migrateCarPlayMapAppearanceMode
         case migrateTracksSortModeKeysAndFormat
     }
     
@@ -110,10 +109,6 @@ final class MigrationManager: NSObject {
             if !defaults.bool(forKey: MigrationKey.migrateAstronomyPreferences.rawValue) {
                 migrateAstronomyPreferences()
                 defaults.set(true, forKey: MigrationKey.migrateAstronomyPreferences.rawValue)
-            }
-            if !defaults.bool(forKey: MigrationKey.migrateCarPlayMapAppearanceMode.rawValue) {
-                migrateCarPlayMapAppearanceMode()
-                defaults.set(true, forKey: MigrationKey.migrateCarPlayMapAppearanceMode.rawValue)
             }
             if !defaults.bool(forKey: MigrationKey.migrateTracksSortModeKeysAndFormat.rawValue) {
                 migrateTracksSortModeKeysAndFormat()
@@ -621,14 +616,6 @@ final class MigrationManager: NSObject {
         }
     }
     
-    private func migrateCarPlayMapAppearanceMode() {
-        let current = settings.applicationMode.get()
-        let firstCar = CarPlayService.shared.firstCarMode()
-        let resolved = settings.isCarPlayModeDefault.get() ? (current.isDerivedRouting(from: .car()) ? current : firstCar) : settings.carPlayMode.get()
-
-        settings.carPlayMapAppearanceMode.set(settings.appearanceMode.get(resolved))
-    }
-
     private func migrateTracksSortModeKeysAndFormat() {
         let validValues = Set(TracksSortMode.allCases.map(\.value))
         var tracksSortModes = settings.getTracksSortModes()

3214e85b4979be8ecbcfe63698c621aa8ac88494
RZR-UA <youonly@ukr.net>
2026-09-17 19:58:30 +0200

    Fix startup crash in OARootViewController (LLM)

--- Sources/Controllers/OARootViewController.m
+++ Sources/Controllers/OARootViewController.m
@@ -109,7 +109,7 @@ typedef enum : NSUInteger {
 
 - (void) loadView
 {
-    self.view = [[UIView alloc] initWithFrame:[UIScreen mainScreen].applicationFrame];
+    self.view = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.view.backgroundColor = UIColor.whiteColor;
 }
 

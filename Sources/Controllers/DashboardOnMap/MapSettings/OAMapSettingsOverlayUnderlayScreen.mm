6c67a49378c29a76e0582b9a9731c4e2f178dc36
vitaliy-sova-ios <vitaliy.sova.ios@gmail.com>
2026-09-17 15:04:07 +0300

    Fixed visibility eye icon on Overlay and Underlay map menu

--- Sources/Controllers/DashboardOnMap/MapSettings/OAMapSettingsOverlayUnderlayScreen.mm
+++ Sources/Controllers/DashboardOnMap/MapSettings/OAMapSettingsOverlayUnderlayScreen.mm
@@ -324,6 +324,7 @@ static NSInteger kButtonsSection;
             NSString *imgName = _isEnabled ? @"ic_custom_show.png" : @"ic_custom_hide.png";
             cell.leftIconView.image = [UIImage templateImageNamed:imgName];
             cell.leftIconView.tintColor = _isEnabled ? [UIColor colorNamed:ACColorNameIconColorSelected]: [UIColor colorNamed:ACColorNameIconColorDisabled];
+            [cell leftIconVisibility:YES];
 
             [cell.switchView removeTarget:self action:NULL forControlEvents:UIControlEventValueChanged];
             [cell.switchView setOn:_isEnabled];
@@ -401,11 +402,11 @@ static NSInteger kButtonsSection;
         {
             NSArray *nib = [[NSBundle mainBundle] loadNibNamed:[OASwitchTableViewCell getCellIdentifier] owner:self options:nil];
             cell = (OASwitchTableViewCell *) nib[0];
-            [cell leftIconVisibility:NO];
             [cell descriptionVisibility:NO];
         }
         if (cell)
         {
+            [cell leftIconVisibility:NO];
             cell.titleLabel.text = item[@"title"];
             [cell.switchView removeTarget:self action:NULL forControlEvents:UIControlEventValueChanged];
 

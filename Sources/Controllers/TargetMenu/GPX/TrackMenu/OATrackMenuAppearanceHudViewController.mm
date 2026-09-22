0410c5b5b76ea9d3ed507087aa42dd9279121afc
Aleksandr Panchenko <alex.panchenko.osmand@gmail.com>
2026-09-22 11:41:59 +0300

    Address transport and stale control review findings

--- Sources/Controllers/TargetMenu/GPX/TrackMenu/OATrackMenuAppearanceHudViewController.mm
+++ Sources/Controllers/TargetMenu/GPX/TrackMenu/OATrackMenuAppearanceHudViewController.mm
@@ -1406,7 +1406,14 @@ static const NSInteger kColorsSection = 1;
 
 - (OAGPXTableCellData *)getCellData:(NSIndexPath *)indexPath
 {
-    return _tableData[indexPath.section].subjects[indexPath.row];
+    if (indexPath.section >= _tableData.count)
+        return nil;
+
+    OAGPXTableSectionData *sectionData = _tableData[indexPath.section];
+    if (indexPath.row >= sectionData.subjects.count)
+        return nil;
+
+    return sectionData.subjects[indexPath.row];
 }
 
 - (void)doAdditionalLayout
@@ -2144,6 +2151,8 @@ static const NSInteger kColorsSection = 1;
     UISwitch *switchView = (UISwitch *) sender;
     NSIndexPath *indexPath = [NSIndexPath indexPathForRow:switchView.tag & 0x3FF inSection:switchView.tag >> 10];
     OAGPXTableCellData *cellData = [self getCellData:indexPath];
+    if (!cellData)
+        return;
 
     [self onSwitch:switchView.isOn tableData:cellData];
 
@@ -2159,6 +2168,8 @@ static const NSInteger kColorsSection = 1;
     {
         NSIndexPath *indexPath = [NSIndexPath indexPathForRow:segment.tag & 0x3FF inSection:segment.tag >> 10];
         OAGPXTableCellData *cellData = [self getCellData:indexPath];
+        if (!cellData)
+            return;
 
         [self updateProperty:@(segment.selectedSegmentIndex) tableData:cellData];
 
@@ -2176,24 +2187,19 @@ static const NSInteger kColorsSection = 1;
 
 - (void)sliderChanged:(id)sender
 {
-    UISlider *slider = (UISlider *) sender;
+    OASegmentedSlider *slider = (OASegmentedSlider *) sender;
     if (sender)
     {
         NSIndexPath *indexPath = [NSIndexPath indexPathForRow:slider.tag & 0x3FF inSection:slider.tag >> 10];
-        if (indexPath.section >= _tableData.count)
-            return;
-
-        OAGPXTableSectionData *sectionData = _tableData[indexPath.section];
-        if (indexPath.row >= sectionData.subjects.count)
+        OAGPXTableCellData *cellData = [self getCellData:indexPath];
+        if (!cellData)
             return;
 
         OASegmentSliderTableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
         if (![cell isKindOfClass:OASegmentSliderTableViewCell.class] || cell.sliderView != slider)
             return;
 
-        OAGPXTableCellData *cellData = sectionData.subjects[indexPath.row];
-
-        [self updateProperty:@(cell.sliderView.selectedMark) tableData:cellData];
+        [self updateProperty:@(slider.selectedMark) tableData:cellData];
 
         [self updateData:cellData];
 
@@ -2768,8 +2774,12 @@ static const NSInteger kColorsSection = 1;
         [indexPaths addObject:_colorsCollectionIndexPath];
     if (_paletteNameIndexPath)
     {
-        [self updateData:_tableData[_paletteNameIndexPath.section].subjects[_paletteNameIndexPath.row]];
-        [indexPaths addObject:_paletteNameIndexPath];
+        OAGPXTableCellData *paletteNameData = [self getCellData:_paletteNameIndexPath];
+        if (paletteNameData)
+        {
+            [self updateData:paletteNameData];
+            [indexPaths addObject:_paletteNameIndexPath];
+        }
     }
     if (indexPaths.count > 0)
         [self.tableView reloadRowsAtIndexPaths:indexPaths withRowAnimation:UITableViewRowAnimationNone];
@@ -2905,8 +2915,12 @@ static const NSInteger kColorsSection = 1;
             NSMutableArray<NSIndexPath *> *indexPaths = [NSMutableArray array];
             if (_paletteNameIndexPath)
             {
-                [self updateData:_tableData[_paletteNameIndexPath.section].subjects[_paletteNameIndexPath.row]];
-                [indexPaths addObject:_paletteNameIndexPath];
+                OAGPXTableCellData *paletteNameData = [self getCellData:_paletteNameIndexPath];
+                if (paletteNameData)
+                {
+                    [self updateData:paletteNameData];
+                    [indexPaths addObject:_paletteNameIndexPath];
+                }
             }
             if (_paletteLegendIndexPath)
                 [indexPaths addObject:_paletteLegendIndexPath];

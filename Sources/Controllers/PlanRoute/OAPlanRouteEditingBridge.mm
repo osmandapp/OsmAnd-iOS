526b806b526de82e04c465a162d81fc5d254c7e3
DmitrySvetlichny <111898301+DmitrySvetlichny@users.noreply.github.com>
2026-09-23 13:52:44 +0300

    Add a tag with the OSM object URL to the favourite (#5811)
    * Add a tag with the OSM object URL to the favourite
    
    * Pass context menu target object to favorite editing handler explicitly

--- Sources/Controllers/PlanRoute/OAPlanRouteEditingBridge.mm
+++ Sources/Controllers/PlanRoute/OAPlanRouteEditingBridge.mm
@@ -1457,7 +1457,7 @@ static const NSTimeInterval kRouteInfoRefreshInterval = 0.25;
         return;
 
     OASGpxFile *gpxFile = filePath.length == 0 ? _draftGpxFile : [self editingContext].gpxData.gpxFile;
-    OAEditPointViewController *controller = [[OAEditPointViewController alloc] initWithLocation:location title:OALocalizedString(@"shared_string_waypoint") address:nil customParam:gpxFilePath pointType:EOAEditPointTypeWaypoint targetMenuState:nil poi:nil gpxFile:gpxFile];
+    OAEditPointViewController *controller = [[OAEditPointViewController alloc] initWithLocation:location title:OALocalizedString(@"shared_string_waypoint") address:nil customParam:gpxFilePath pointType:EOAEditPointTypeWaypoint targetMenuState:nil poi:nil gpxFile:gpxFile targetObject:nil];
     controller.gpxWptDelegate = self;
     UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:controller];
     [presentingViewController presentViewController:navigationController animated:YES completion:nil];
